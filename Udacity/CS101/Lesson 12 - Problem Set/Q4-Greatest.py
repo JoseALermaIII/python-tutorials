bdadd5c1e6ead213d9697ba9a60f2dc5e181988c
JoseALermaIII <jose@JoseALerma.com>
2017-11-01 03:46:59 -0500

    Initialize

---
+++ Udacity/CS101/Lesson 12 - Problem Set/Q4-Greatest.py
@@ -0,0 +1,16 @@
+# Define a procedure, greatest,
+# that takes as input a list
+# of positive numbers, and
+# returns the greatest number
+# in that list. If the input
+# list is empty, the output
+# should be 0.
+
+def greatest(list_of_numbers):
+
+# print greatest([4,23,1])
+# >>> 23
+# print greatest([])
+# >>> 0
+
+
