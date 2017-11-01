8c563243e8e077ecabec138356908b56e34263d9
JoseALermaIII <jose@JoseALerma.com>
2017-11-01 04:18:52 -0500

    Working solution

--- Udacity/CS101/Lesson 12 - Problem Set/Q6-Max Pages.py
+++ Udacity/CS101/Lesson 12 - Problem Set/Q6-Max Pages.py
@@ -71,22 +71,26 @@ def get_all_links(page):
 def crawl_web(seed, max_pages):
     tocrawl = [seed]
     crawled = []
+
     while tocrawl:
         page = tocrawl.pop()
         if page not in crawled:
             union(tocrawl, get_all_links(get_page(page)))
             crawled.append(page)
+            if len(crawled) >= max_pages:
+                break
+
     return crawled
 
-#print crawl_web("http://www.udacity.com/cs101x/index.html",1)
+print crawl_web("http://www.udacity.com/cs101x/index.html",1)
 #>>> ['http://www.udacity.com/cs101x/index.html']
 
-# print crawl_web("http://www.udacity.com/cs101x/index.html",3)
+print crawl_web("http://www.udacity.com/cs101x/index.html",3)
 #>>> ['http://www.udacity.com/cs101x/index.html',
 #>>> 'http://www.udacity.com/cs101x/flying.html',
 #>>> 'http://www.udacity.com/cs101x/walking.html']
 
-#print crawl_web("http://www.udacity.com/cs101x/index.html",500)
+print crawl_web("http://www.udacity.com/cs101x/index.html",500)
 #>>> ['http://www.udacity.com/cs101x/index.html',
 #>>> 'http://www.udacity.com/cs101x/flying.html',
 #>>> 'http://www.udacity.com/cs101x/walking.html',
