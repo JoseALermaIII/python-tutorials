a7e1b180cd415c3a78a84536d2836f72434bd105
JoseALermaIII <jose@JoseALerma.com>
2018-02-06 03:46:06 -0600

    Working solution

--- Udacity/CS101/Lesson 14 - Problem Set Optional 2/Q1-Antisymmetric Square.py
+++ Udacity/CS101/Lesson 14 - Problem Set Optional 2/Q1-Antisymmetric Square.py
@@ -4,8 +4,33 @@
 # An nxn square is called antisymmetric if A[i][j]=-A[j][i]
 # for each i=0,1,...,n-1 and for each j=0,1,...,n-1.
 
-def antisymmetric(A):
-    #Write your code here
+def antisymmetric(list_):
+    new_list = zip(*list_) # Converts rows to columns in new list
+    length = len(list_)
+    i = 0
+    # Debug list and new_list conversion
+    #print("list: ", list_, "new_list: ", new_list)
+
+    # Filters non-square lists
+    while i < length:
+        if len(list_[i]) != len(new_list[i]):
+            return False
+        i += 1
+
+    # Compares lists
+    i = 0
+    while i < length:
+        j = 0
+        while j < len(list_[i]):
+            # Debug list_[i][j], new_list[i][j], and comparison
+            #print("list_[i][j]: ", list_[i][j],
+            #      "new_list[i][j]: ", new_list[i][j],
+            #      "result: ", list_[i][j] != new_list[i][j])
+            if list_[i][j] != -new_list[i][j]:
+                return False
+            j += 1
+        i += 1
+    return True
 
 
 # Test Cases:
