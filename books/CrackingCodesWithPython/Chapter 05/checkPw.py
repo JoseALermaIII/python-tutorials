58736f9256dbb419094fb3692ab9ec17c5c3a9e8
JoseALermaIII <jose@JoseALerma.com>
2018-04-26 00:27:20 -0500

    Modified checkPw: added missing colon

--- books/CrackingCodesWithPython/Chapter 05/checkPw.py
+++ books/CrackingCodesWithPython/Chapter 05/checkPw.py
@@ -3,5 +3,5 @@ typedPassword = input("Enter your password.\n")
 if typedPassword == 'hunter2':
     print("You entered: *******")
 else:
-    print("You entered " + typedPassword)
+    print("You entered: " + typedPassword)
 print("Done")
