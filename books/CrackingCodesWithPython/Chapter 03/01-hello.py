d56e6b0582e12da46a7f00ce9c9638431b34bccc
JoseALermaIII <jose@JoseALerma.com>
2018-04-25 19:19:19 -0500

    Initialize chapter 03

---
+++ books/CrackingCodesWithPython/Chapter 03/01-hello.py
@@ -0,0 +1,9 @@
+# This program says hello and asks for my name.
+# Note: using double quotes for strings because I'm a nitpicker -
+# author admits that he uses single quotes because it is easier to type
+# and it technically doesn't matter
+
+print("Hello, world!")
+print("What is your name?")
+myName = input()
+print("It is good to meet you, " + myName)
