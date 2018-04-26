b3462001fec693e4d6a4e74f0570af993330a01c
JoseALermaIII <jose@JoseALerma.com>
2018-04-25 20:22:16 -0500

    Modified reverseCipher: readability, simplify while loop, new operators, included decryption

--- books/CrackingCodesWithPython/Chapter 04/01-reverseCipher.py
+++ books/CrackingCodesWithPython/Chapter 04/01-reverseCipher.py
@@ -1,12 +1,20 @@
 # Reverse Cipher
-# https://www.nostartch.com/crackingcodes/ (BSD Licensed)
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+# Note: pretty much the same, except I use double quotes,
+# expand variable names for readability, simplify the while loop,
+# and use fancier operators
 
-message = 'Three can keep a secret, if two of them are dead.'
-translated = ''
+message = "Three can keep a secret, if two of them are dead."
+translated = ""
 
-i = len(message) - 1
-while i >= 0:
-    trasnlated = translated + message[i]
-    i = i - 1
+index = len(message)
+while index:
+    index -= 1  # equivalent to index = index - 1
+    translated += message[index]
 
 print(translated)
+
+# Decrypt example
+# Was going to make a function out of it so it's easier, but I
+# didn't want to change it more than I already did. (^_^;)
+print(translated[::-1])  # a step of -1 reverses the string
