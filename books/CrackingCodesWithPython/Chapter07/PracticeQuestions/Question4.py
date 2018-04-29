b6bdd864f4146c0fb832f2ba2a8512707f2bfcb8
JoseALermaIII <jose@JoseALerma.com>
2018-04-29 04:24:39 -0500

    Initialize Chapter07 Question4

---
+++ books/CrackingCodesWithPython/Chapter07/PracticeQuestions/Question4.py
@@ -0,0 +1,9 @@
+# What value does each of the following expressions evaluate to?
+# aka "Lists are OP"
+
+print(len([2, 4]))
+print(len([]))
+print(len(['', '', '']))
+print([4, 5, 6] + [1, 2, 3])
+print(3 * [1, 2, 3] + [0])  # I hate you
+print(42 in [41, 42, 42, 42])
