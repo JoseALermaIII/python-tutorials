ba745222873dbc9122b4fa6722984d059c55b4b3
JoseALermaIII <jose@JoseALerma.com>
2018-04-29 04:35:41 -0500

    Initialize

---
+++ books/CrackingCodesWithPython/Chapter07/PracticeQuestions/Question5.py
@@ -0,0 +1,17 @@
+# What are the four augmented assignment operators?
+# Hint: Table 7-1 on pg 92
+
+word = "Bo"
+number = 42
+
+word += "Bo"
+print(word)
+
+number -= 2
+print(number)
+
+word *= 10
+print(word)
+
+number /= 4
+print(number)
