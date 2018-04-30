ccd0620ea904adec853017fbfefaeccff38ab714
JoseALermaIII <jose@JoseALerma.com>
2018-04-30 02:07:15 -0500

    Initialize

---
+++ books/CrackingCodesWithPython/Chapter05/PracticeQuestions/Question2.py
@@ -0,0 +1,11 @@
+# Using caesarCipher.py, decrypt the following ciphertexts with the given keys
+# SPOILERS for chapter 7 (functions)
+
+from books.CrackingCodesWithPython.Chapter05.PracticeQuestions.Question1 import caesarCipher
+
+ciphertexts = ["Kv?uqwpfu?rncwukdng?gpqwijB",
+               "XCBSw88S18A1S 2SB41SE .8zSEwAS50D5A5x81V"]
+keys = [2, 22]
+
+for index in range(len(keys)):
+    print(caesarCipher(keys[index], ciphertexts[index], "decrypt"))
