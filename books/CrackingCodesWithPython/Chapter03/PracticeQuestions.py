38b0032faa8493667bf7ecd88de0c2eb858885f3
JoseALermaIII <jose@JoseALerma.com>
2018-04-30 03:06:21 -0500

    Initialize

---
+++ books/CrackingCodesWithPython/Chapter03/PracticeQuestions.py
@@ -0,0 +1,28 @@
+# Chapter 3 Practice Questions
+
+spam = "Cats"
+# 1. If you assign spam = "Cats", what do the following lines print?
+print(spam + spam + spam)
+print(spam * 3)
+
+# 2. What do the following lines print?
+print("Dear Alice, \nHow are you?\nSincerely,\nBob")
+print("Hello" + "Hello")
+
+spam = "Four score and seven years is eighty seven years."
+# 3. If you assign spam = "Four score and seven years is eighty seven years.",
+#    what would each of the following lines print?
+print(spam[5])
+print(spam[-3])
+print(spam[0:4] + spam[5])
+print(spam[-3:-1])
+print(spam[:10])
+print(spam[-5:])
+print(spam[:])
+
+# 4. Which window displays the >>> prompt, the interactive shell or the file editor?
+# Hint: We're in the file editor
+
+# 5. What does the following line print?
+
+#print("Hello, world!")
