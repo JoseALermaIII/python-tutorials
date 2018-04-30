0ab30289161621533b860db2993540dcc3bd315f
JoseALermaIII <jose@JoseALerma.com>
2018-04-30 04:46:36 -0500

    Initialize

---
+++ books/CrackingCodesWithPython/Chapter08/Practice Questions/Question4.py
@@ -0,0 +1,11 @@
+# Which of the following is correct?
+
+"""
+if __name__ == '__main__':
+if __main__ == '__name__':
+if _name_ == '_main_':
+if _main_ == '_name_':
+"""
+
+# Answer with key = 9:
+answer = "Tr_=_h n _eia'' sm_:a:e_n _msi_awf ie_=n"
