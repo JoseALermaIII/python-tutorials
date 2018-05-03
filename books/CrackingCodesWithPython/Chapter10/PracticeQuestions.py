ae3b4758dcdafe2f5be55013892af75bae9ddfd0
JoseALermaIII <jose@JoseALerma.com>
2018-05-03 03:05:05 -0500

    Initialize Chapter 10 PracticeQuestions

---
+++ books/CrackingCodesWithPython/Chapter10/PracticeQuestions.py
@@ -0,0 +1,17 @@
+# Chapter 10 Practice Questions
+
+# 1. Which is correct: os.exists() or os.path.exists()?
+import os
+print(os.exists("PracticeQuestions.py"))
+print(os.path.exists("PracticeQuestions.py"))
+
+# 2. When is the Unix Epoch?
+# Hint: check page 136
+
+# 3. What do the following expressions evaluate to?
+print('Foobar'.startswith('Foo'))
+print('Foo'.startswith('Foobar'))
+print('Foobar'.startswith('foo'))
+print('bar'.endswith('Foobar'))
+print('Foobar'.endswith('bar'))
+print('The quick brown fox jumped over the yellow lazy dog.'.title())
