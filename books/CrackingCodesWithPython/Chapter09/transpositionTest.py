5174fe64a076de6ef5afc73278d457157eca8d69
JoseALermaIII <jose@JoseALerma.com>
2018-05-02 23:21:12 -0500

    Update transpositionTest: fixed imports and PEP8 formatting

--- books/CrackingCodesWithPython/Chapter09/transpositionTest.py
+++ books/CrackingCodesWithPython/Chapter09/transpositionTest.py
@@ -1,10 +1,13 @@
 # Transposition Cipher Test
 # https://www.nostarch.com/crackingcodes/ (BSD Licensed)
 
-import random, sys, transpositionEncrypt, transpositionDecrypt
+import random, sys
+from books.CrackingCodesWithPython.Chapter07.transpositionEncrypt import encryptMessage
+from books.CrackingCodesWithPython.Chapter08.transpositionDecrypt import decryptMessage
+
 
 def main():
-    random.seed(42)  #  Set the random "seed" to a static value.
+    random.seed(42)  # Set the random "seed" to a static value.
 
     for i in range(20):  # Run 20 tests.
         # Generate random messages to test.
@@ -21,8 +24,8 @@ def main():
 
         # Check all possible keys for each message:
         for key in range(1, int(len(message)/2)):
-            encrypted = transpositionEncrypt.encryptMessage(key, message)
-            decrypted = transpositionDecrypt.decryptMessage(key, encrypted)
+            encrypted = encryptMessage(key, message)
+            decrypted = decryptMessage(key, encrypted)
 
             # If the decryption doesn't match the original message, display
             # an error message and quit:
