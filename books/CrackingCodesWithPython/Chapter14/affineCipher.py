6929c978b7f15b2375fce004df494c62ecadd3b0
JoseALermaIII <jose@JoseALerma.com>
2018-05-07 05:40:11 -0500

    Update affineCipher: fixed PEP8 issues, imports, and spacing

--- books/CrackingCodesWithPython/Chapter14/affineCipher.py
+++ books/CrackingCodesWithPython/Chapter14/affineCipher.py
@@ -1,7 +1,10 @@
 # Affine Cipher
 # https://www.nostarch.com/crackingcodes/ (BSD Licensed)
 
-import sys, pyperclip, cryptomath, random
+import sys
+import random
+from books.CrackingCodesWithPython.pyperclip import copy
+from books.CrackingCodesWithPython.Chapter13.cryptomath import gcd, findModInverse
 SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
 
 
@@ -15,17 +18,17 @@ def main():
         translated = encryptMessage(myKey, myMessage)
     elif myMode == 'decrypt':
         translated = decryptMessage(myKey, myMessage)
-    print('Key: %s' % (myKey))
+    print('Key: %s' % myKey)
     print('%sed text:' % (myMode.title()))
     print(translated)
-    pyperclip.copy(translated)
+    copy(translated)
     print('Full %sed text copied to clipboard.' % myMode)
 
 
 def getKeyParts(key):
     keyA = key // len(SYMBOLS)
     keyB = key % len(SYMBOLS)
-    return (keyA, keyB)
+    return keyA, keyB
 
 
 def checkKeys(keyA, keyB, mode):
@@ -35,30 +38,30 @@ def checkKeys(keyA, keyB, mode):
         sys.exit('Cipher is weak if key B is 0. Choose a different key.')
     if keyA < 0 or keyB < 0 or keyB > len(SYMBOLS) - 1:
         sys.exit('Key A must be greater than 0 and Key B must be between 0 and %s ' % (len(SYMBOLS) - 1))
-    if cryptomath.gcd(keyA, len(SYMBOLS)) != 1:
+    if gcd(keyA, len(SYMBOLS)) != 1:
         sys.exit('Key A (%s) and the symbol set size (%s) are not relatively prime. Choose a different key.'
                  % (keyA, len(SYMBOLS)))
 
 
-    def encryptMessage(key, message):
-        keyA, keyB = getKeyParts(key)
-        checkKeys(keyA, keyB, 'encrypt')
-        ciphertext = ''
-        for symbol in message:
-            if symbol in SYMBOLS:
-                # Encrypt the symbol:
-                symbolIndex = SYMBOLS.find(symbol)
-                ciphertext += SYMBOLS[(symbolIndex * keyA + keyB) % len(SYMBOLS)]
-            else:
-                ciphertext += symbol # Append the symbol without encrypting.
-        return ciphertext
+def encryptMessage(key, message):
+    keyA, keyB = getKeyParts(key)
+    checkKeys(keyA, keyB, 'encrypt')
+    ciphertext = ''
+    for symbol in message:
+        if symbol in SYMBOLS:
+            # Encrypt the symbol:
+            symbolIndex = SYMBOLS.find(symbol)
+            ciphertext += SYMBOLS[(symbolIndex * keyA + keyB) % len(SYMBOLS)]
+        else:
+            ciphertext += symbol  # Append the symbol without encrypting.
+    return ciphertext
 
 
 def decryptMessage(key, message):
     keyA, keyB = getKeyParts(key)
     checkKeys(keyA, keyB, 'decrypt')
     plaintext = ''
-    modInverseOfKeyA = cryptomath.findModInverse(keyA, len(SYMBOLS))
+    modInverseOfKeyA = findModInverse(keyA, len(SYMBOLS))
 
     for symbol in message:
         if symbol in SYMBOLS:
@@ -66,7 +69,7 @@ def decryptMessage(key, message):
             symbolIndex = SYMBOLS.find(symbol)
             plaintext += SYMBOLS[(symbolIndex - keyB) * modInverseOfKeyA % len(SYMBOLS)]
         else:
-            plaintext += symbol # Append the symbol without decrypting.
+            plaintext += symbol  # Append the symbol without decrypting.
     return plaintext
 
 
@@ -74,7 +77,7 @@ def getRandomKey():
     while True:
         keyA = random.randint(2, len(SYMBOLS))
         keyB = random.randint(2, len(SYMBOLS))
-        if cryptomath.gcd(keyA, len(SYMBOLS)) == 1:
+        if gcd(keyA, len(SYMBOLS)) == 1:
             return keyA * len(SYMBOLS) + keyB
 
 
