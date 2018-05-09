342b2cf92eefcbbcd91366c64ea6c04b1c1d3b15
JoseALermaIII <jose@JoseALerma.com>
2018-05-09 04:50:14 -0500

    Initialize Ch. 15 PracticeQuestions

---
+++ books/CrackingCodesWithPython/Chapter15/PracticeQuestions.py
@@ -0,0 +1,19 @@
+# Chapter 15 Practice Questions
+
+# 1. What does 2 ** 5 evaluate to?
+print(2 ** 5)
+
+# 2. What does 6 ** 2 evaluate to?
+print(6 ** 2)
+
+# 3. What does the following code print?
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+
+# 4. Does the main() function of affineHacker.py get called if another program
+#    runs import affineHacker?
+# Hint: check page 204
+from books.CrackingCodesWithPython.Chapter15.affineHacker import hackAffine
+hackAffine("Well, I can't figure out just two! So let's pretend you opened 200.")
