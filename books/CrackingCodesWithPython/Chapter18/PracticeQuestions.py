3b8001d62d241fdc57d18334e329fb7cf2fbd6c2
JoseALermaIII <jose@JoseALerma.com>
2018-05-14 03:45:03 -0500

    Update Ch. 18 PracticeQuestions: added è

--- books/CrackingCodesWithPython/Chapter18/PracticeQuestions.py
+++ books/CrackingCodesWithPython/Chapter18/PracticeQuestions.py
@@ -1,6 +1,6 @@
 # Chapter 18 Practice Questions
 
-# 1. Which cipher is the Vigenere cipher similar to, except that the Vigenere
+# 1. Which cipher is the Vigenère cipher similar to, except that the Vigenere
 #    cipher uses multiple keys instead of just one key?
 # Hint: Check page 248
 from books.CrackingCodesWithPython.Chapter18.vigenereCipher import decryptMessage
@@ -8,7 +8,7 @@ from books.CrackingCodesWithPython.Chapter18.vigenereCipher import decryptMessag
 message = "Tuw Rmxeawni Ticzav zs faimcae lk xye Psawrr pallvr."  # Encrypted with ANSWER
 #print(decryptMessage(blank, blank))  # Fill in the blanks
 
-# 2. How many possible keys are there for a Vigenere key with a key length
+# 2. How many possible keys are there for a Vigenère key with a key length
 #    of 10?
 # Hint: Check page 250
 #
@@ -32,7 +32,7 @@ else:
 
 #print("%s: %s" % (answer, solution))
 
-# 3. What kind of cipher is the Vigenere cipher?
+# 3. What kind of cipher is the Vigenère cipher?
 # Hint: Check page 248
 
 message = "Mft Zbetrxpt Gbnwik gh e imactjeltztxba hyuqimmsimhl rmiftv."  # Encrypted with TYPE
