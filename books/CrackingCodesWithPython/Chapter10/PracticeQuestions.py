e4c0b517f746475a351bf07e7b545daf421cad32
JoseALermaIII <jose@JoseALerma.com>
2018-05-18 06:06:43 -0500

    Update Ch. 10 PracticeQuestions: added answer for question 2

--- books/CrackingCodesWithPython/Chapter10/PracticeQuestions.py
+++ books/CrackingCodesWithPython/Chapter10/PracticeQuestions.py
@@ -7,6 +7,15 @@ print(os.path.exists("PracticeQuestions.py"))
 
 # 2. When is the Unix Epoch?
 # Hint: check page 136
+months = ["January", "February", "March", "April", "May",
+          "June", "July", "August", "September", "October",
+          "November", "December"]
+
+month = 278 + 42 * 7 - 39 + 53 * 0 - 533
+day = 153 * 7 - 1070
+year = 394 * 5
+
+print("The Unix Epoch is on %s %s, %s" % (months[month], day, year))
 
 # 3. What do the following expressions evaluate to?
 print('Foobar'.startswith('Foo'))
