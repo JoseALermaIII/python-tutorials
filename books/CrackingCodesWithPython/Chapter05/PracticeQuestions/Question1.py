c7af505e25a6c29cf88611bdd6decb6298caf671
JoseALermaIII <jose@JoseALerma.com>
2018-05-18 05:46:48 -0500

    Update Ch. 5 Question1: changed function to module

--- books/CrackingCodesWithPython/Chapter05/PracticeQuestions/Question1.py
+++ books/CrackingCodesWithPython/Chapter05/PracticeQuestions/Question1.py
@@ -2,37 +2,7 @@
 # SPOILER for Chapter 7 (functions)
 
 
-def caesarCipher(key, message, mode):
-
-    # Every possible symbol that can be encrypted:
-    SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
-
-    # Store the encrypted/decrypted form of the message:
-    translated = ''
-
-    for symbol in message:
-        # Note: Only symbols in the SYMBOLS string can be encrypted/decrypted.
-        if symbol in SYMBOLS:
-            symbolIndex = SYMBOLS.find(symbol)
-
-            # Perform encryption/decryption:
-            if mode == 'encrypt':
-                translatedIndex = symbolIndex + key
-            elif mode == 'decrypt':
-                translatedIndex = symbolIndex - key
-
-            # Handle wraparound, if needed:
-            if translatedIndex >= len(SYMBOLS):
-                translatedIndex -= len(SYMBOLS)
-            elif translatedIndex < 0:
-                translatedIndex += len(SYMBOLS)
-
-            translated += SYMBOLS[translatedIndex]
-        else:
-            # Append the symbol without encrypting/decrypting:
-            translated += symbol
-
-    return translated
+from books.CrackingCodesWithPython.Chapter01.caesarCipher import caesarCipher
 
 
 def main():
