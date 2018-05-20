bc354e0db52d2946e929c95ed264a48a4caeb7cd
JoseALermaIII <jose@JoseALerma.com>
2018-05-20 02:33:49 -0500

    Initialize Ch. 22 PracticeQuestions

---
+++ books/CrackingCodesWithPython/Chapter22/PracticeQuestions.py
@@ -0,0 +1,19 @@
+# Chapter 22 Practice Questions
+
+from books.CrackingCodesWithPython.Chapter18.vigenereCipher import decryptMessage
+
+# 1. How many prime numbers are there?
+# Hint: Check page 322
+message = "Iymdi ah rv urxxeqfi fjdjqv gu gzuqw clunijh."  # Encrypted with key "PRIMES"
+#print(decryptMessage(blank, blank))  # Fill in the blanks
+
+# 2. What are integers that are not prime called?
+# Hint: Check page 323
+message = "Vbmggpcw wlvx njr bhv pctqh emi psyzxf czxtrwdxr fhaugrd."  # Encrypted with key "NOTCALLEDEVENS"
+#print(decryptMessage(blank, blank))  # Fill in the blanks
+
+# 3. What are two algorithms for finding prime numbers?
+# Hint: Check page 323
+# Encrypted with key "ALGORITHMS"
+message = "Tsk hyzxl mdgzxwkpfz gkeo ob kpbz ngov gfv: bkpmd dtbwjqhu, eaegk cw Mkhfgsenseml, hzv Rlhwe-Ubsxwr."
+#print(decryptMessage(blank, blank))  # Fill in the blanks
