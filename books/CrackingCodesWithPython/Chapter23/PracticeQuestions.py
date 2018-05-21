2d740353037db271136b7a365558a342612069f6
JoseALermaIII <jose@JoseALerma.com>
2018-05-20 19:24:32 -0500

    Initialize Ch. 23 PracticeQuestions

---
+++ books/CrackingCodesWithPython/Chapter23/PracticeQuestions.py
@@ -0,0 +1,23 @@
+# Chapter 23 Practice Questions
+
+# 1. What is the difference between a symmetric cipher and an asymmetric
+#    cipher?
+# Hint: Check page 336
+
+
+# 2. Alice generates a public key and a private key. Unfortunately, she later
+#    loses her private key.
+#    a.  Will other people be able to send her encrypted messages?
+#    b.  Will she be able to decrypt messages previously sent to her?
+#    c.  Will she be able to digitally sign documents?
+#    d.  Will other people be able to verify her previously signed documents?
+# Hint: Check page 336
+
+
+# 3. What are authentication and confidentiality? How are they different?
+# Hint: Check page 338
+
+
+# 4. What is non-repudiation?
+# Hint: Check page 339
+
