e52688a7bd1fae08683e98d7f501214f24b4ccdc
JoseALermaIII <jose@JoseALerma.com>
2018-05-22 01:00:59 -0500

    Update Q5-Add Page to Index: working solution

--- Udacity/CS101/Lesson 15 - Responding to Queries/Q5-Add Page to Index.py
+++ Udacity/CS101/Lesson 15 - Responding to Queries/Q5-Add Page to Index.py
@@ -6,29 +6,29 @@
 #   - content (String)
 
 # It should update the index to include
-# all of the word occurences found in the
+# all of the word occurrences found in the
 # page content by adding the url to the
 # word's associated url list.
 
 index = []
 
 
-def add_to_index(index,keyword,url):
+def add_to_index(index, keyword, url):
     for entry in index:
         if entry[0] == keyword:
             entry[1].append(url)
             return
-    index.append([keyword,[url]])
-
-def add_page_to_index(index,url,content):
+    index.append([keyword, [url]])
 
 
+def add_page_to_index(index, url, content):
+    words = content.split()
+    for word in words:
+        add_to_index(index, word, url)
+    return None
 
 
-
-#add_page_to_index(index,'fake.text',"This is a test")
-#print index
-#>>> [['This', ['fake.text']], ['is', ['fake.text']], ['a', ['fake.text']],
-#>>> ['test',['fake.text']]]
-
-
+add_page_to_index(index, 'fake.text', "This is a test")
+print(index)
+# >>> [['This', ['fake.text']], ['is', ['fake.text']], ['a', ['fake.text']],
+# >>> ['test',['fake.text']]]
