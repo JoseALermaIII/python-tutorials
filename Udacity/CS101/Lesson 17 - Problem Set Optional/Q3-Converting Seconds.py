a166b0f2b8b2f05d713e29f5997c301ba52bc38b
JoseALermaIII <jose@JoseALerma.com>
2018-05-22 20:59:23 -0500

    Update Q3-Converting Seconds: working solution

--- Udacity/CS101/Lesson 17 - Problem Set Optional/Q3-Converting Seconds.py
+++ Udacity/CS101/Lesson 17 - Problem Set Optional/Q3-Converting Seconds.py
@@ -11,15 +11,41 @@
 # it should be "0 minutes".
 #
 
-def convert_seconds():
+
+def convert_seconds(seconds):
+    output = []
+
+    hours = seconds // 3600
+    if hours:
+        seconds = seconds % 3600
+
+    minutes = seconds // 60
+    if minutes:
+        seconds = seconds % 60
+
+    if hours == 1:
+        output.append("1 hour, ")
+    else:
+        output.append(str(int(hours)) + " hours, ")
+
+    if minutes == 1:
+        output.append("1 minute, ")
+    else:
+        output.append(str(int(minutes)) + " minutes, ")
+
+    if seconds == 1:
+        output.append("1 second")
+    else:
+        output.append(str(seconds) + " seconds")
+
+    return ''.join(output)
 
 
+print(convert_seconds(3661))
+# >>> 1 hour, 1 minute, 1 second
 
-print convert_seconds(3661)
-#>>> 1 hour, 1 minute, 1 second
+print(convert_seconds(7325))
+# >>> 2 hours, 2 minutes, 5 seconds
 
-print convert_seconds(7325)
-#>>> 2 hours, 2 minutes, 5 seconds
-
-print convert_seconds(7261.7)
-#>>> 2 hours, 1 minute, 1.7 seconds
+print(convert_seconds(7261.7))
+# >>> 2 hours, 1 minute, 1.7 seconds
