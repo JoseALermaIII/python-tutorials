7ae093a3624764ca9fcfed28852a6889a4bb018c
JoseALermaIII <jose@JoseALerma.com>
2018-05-23 22:06:42 -0500

    Update Q34-Changing Lookup: working solution

--- Udacity/CS101/Lesson 18 - How Programs Run/Q34-Changing Lookup.py
+++ Udacity/CS101/Lesson 18 - How Programs Run/Q34-Changing Lookup.py
@@ -1,8 +1,9 @@
 # Change the lookup procedure
 # to now work with dictionaries.
 
+
 def lookup(index, keyword):
-    for entry in index:
-        if entry[0] == keyword:
-            return entry[1]
-    return None
+    if keyword in index:
+        return index[keyword]
+    else:
+        return None
