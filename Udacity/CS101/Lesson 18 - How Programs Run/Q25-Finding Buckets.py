8b4dcb2ca51c129d637d5a07b684bc6dfc186d91
JoseALermaIII <jose@JoseALerma.com>
2018-05-23 20:58:28 -0500

    Update Q25-Finding Buckets: working solution

--- Udacity/CS101/Lesson 18 - How Programs Run/Q25-Finding Buckets.py
+++ Udacity/CS101/Lesson 18 - How Programs Run/Q25-Finding Buckets.py
@@ -3,15 +3,17 @@
 # a keyword, and returns the bucket where the
 # keyword could occur.
 
-def hashtable_get_bucket(htable,keyword):
+
+def hashtable_get_bucket(htable, keyword):
+    return htable[hash_string(keyword, len(htable))]
 
 
-
-def hash_string(keyword,buckets):
+def hash_string(keyword, buckets):
     out = 0
     for s in keyword:
         out = (out + ord(s)) % buckets
     return out
+
 
 def make_hashtable(nbuckets):
     table = []
@@ -20,15 +22,14 @@ def make_hashtable(nbuckets):
     return table
 
 
-#table = [[['Francis', 13], ['Ellis', 11]], [], [['Bill', 17],
-#['Zoe', 14]], [['Coach', 4]], [['Louis', 29], ['Rochelle', 4], ['Nick', 2]]]
+table = [[['Francis', 13], ['Ellis', 11]], [], [['Bill', 17],
+         ['Zoe', 14]], [['Coach', 4]], [['Louis', 29], ['Rochelle', 4], ['Nick', 2]]]
 
-#print hashtable_get_bucket(table, "Zoe")
-#>>> [['Bill', 17], ['Zoe', 14]]
+print(hashtable_get_bucket(table, "Zoe"))
+# >>> [['Bill', 17], ['Zoe', 14]]
 
-#print hashtable_get_bucket(table, "Brick")
-#>>> []
+print(hashtable_get_bucket(table, "Brick"))
+# >>> []
 
-#print hashtable_get_bucket(table, "Lilith")
-#>>> [['Louis', 29], ['Rochelle', 4], ['Nick', 2]]
-
+print(hashtable_get_bucket(table, "Lilith"))
+# >>> [['Louis', 29], ['Rochelle', 4], ['Nick', 2]]
