6665d92e29bd0d864757a693559b7508d176d33f
JoseALermaIII <jose@JoseALerma.com>
2018-05-23 23:34:12 -0500

    Update Q6-Refactoring: working solution

--- Udacity/CS101/Lesson 19 - Problem Set/Q6-Refactoring.py
+++ Udacity/CS101/Lesson 19 - Problem Set/Q6-Refactoring.py
@@ -5,20 +5,19 @@
 
 # Here are the original procedures:
 
+
 def hashtable_update(htable, key, value):
     bucket = hashtable_get_bucket(htable, key)
-    for entry in bucket:
-        if entry[0] == key:
+    entry = bucket_find(bucket, key)
+    if entry is not None:
             entry[1] = value
             return
     bucket.append([key, value])
 
+
 def hashtable_lookup(htable, key):
-    bucket = hashtable_get_bucket(htable, key)
-    for entry in bucket:
-        if entry[0] == key:
-            return entry[1]
-    return None
+    return bucket_find(hashtable_get_bucket(htable, key), key)[1]
+
 
 def make_hashtable(size):
     table = []
@@ -26,11 +25,13 @@ def make_hashtable(size):
         table.append([])
     return table
 
+
 def hash_string(s, size):
     h = 0
     for c in s:
          h = h + ord(c)
     return h % size
+
 
 def hashtable_get_bucket(htable, key):
     return htable[hash_string(key, len(htable))]
@@ -48,13 +49,21 @@ def hashtable_get_bucket(htable, key):
 # hashtable_update or hashtable_lookup should include any for or while loop, and
 # the block of each procedure should be no more than 6 lines long.
 
+
+# New procedure:
+def bucket_find(bucket, key):
+    for entry in bucket:
+        if entry[0] == key:
+            return entry
+    return None
+
 # Your procedures should have the same behavior as the originals.  For example,
+
 
 table = make_hashtable(10)
 hashtable_update(table, 'Python', 'Monty')
 hashtable_update(table, 'CLU', 'Barbara Liskov')
 hashtable_update(table, 'JavaScript', 'Brendan Eich')
 hashtable_update(table, 'Python', 'Guido van Rossum')
-print hashtable_lookup(table, 'Python')
-#>>> Guido van Rossum
-
+print(hashtable_lookup(table, 'Python'))
+# >>> Guido van Rossum
