14fae26efe51ae7c29696e23bebef78db112353a
JoseALermaIII <jose@JoseALerma.com>
2018-05-24 00:07:59 -0500

    Update Q1-Shift a Letter: working solution

--- Udacity/CS101/Lesson 20 - Problem Set Optional/Q1-Shift a Letter.py
+++ Udacity/CS101/Lesson 20 - Problem Set Optional/Q1-Shift a Letter.py
@@ -2,13 +2,16 @@
 # a-z and returns the next letter in the alphabet after it, with 'a'
 # following 'z'.
 
+
 def shift(letter):
+    if letter < 'z':
+        return chr((ord(letter) + 1))
+    return 'a'
 
 
-
-print shift('a')
-#>>> b
-print shift('n')
-#>>> o
-print shift('z')
-#>>> a
+print(shift('a'))
+# >>> b
+print(shift('n'))
+# >>> o
+print(shift('z'))
+# >>> a
