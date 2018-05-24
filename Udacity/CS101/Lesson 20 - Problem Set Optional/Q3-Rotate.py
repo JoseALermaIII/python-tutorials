c3bb7c7177d27f4d0a031bdd3457cd0e7762ed96
JoseALermaIII <jose@JoseALerma.com>
2018-05-24 00:31:24 -0500

    Update Q3-Rotate: working solution

--- Udacity/CS101/Lesson 20 - Problem Set Optional/Q3-Rotate.py
+++ Udacity/CS101/Lesson 20 - Problem Set Optional/Q3-Rotate.py
@@ -5,17 +5,35 @@
 # choose to as long as rotate returns the correct string.
 # Note that n can be positive, negative or zero.
 
-def rotate():
-    # Your code here
 
-print rotate ('sarah', 13)
-#>>> 'fnenu'
-print rotate('fnenu',13)
-#>>> 'sarah'
-print rotate('dave',5)
-#>>>'ifaj'
-print rotate('ifaj',-5)
-#>>>'dave'
-print rotate(("zw pfli tfuv nfibj tfiivtkcp pfl jyflcu "
-                "sv rscv kf ivru kyzj"),-17)
-#>>> ???
+def rotate(string_, shift):
+    output = []
+    for letter in string_:
+        if letter == ' ':
+            output.append(' ')
+        else:
+            output.append(shift_n_letters(letter, shift))
+    return ''.join(output)
+
+
+def shift_n_letters(letter, n):
+    result = ord(letter) + n
+    if result < ord('a'):
+        return chr(ord('z') - (ord('a') - result) + 1)
+    elif result > ord('z'):
+        return chr(ord('a') + (result - ord('z')) - 1)
+    else:
+        return chr(result)
+
+
+print(rotate ('sarah', 13))
+# >>> 'fnenu'
+print(rotate('fnenu', 13))
+# >>> 'sarah'
+print(rotate('dave', 5))
+# >>>'ifaj'
+print(rotate('ifaj', -5))
+# >>>'dave'
+print(rotate(("zw pfli tfuv nfibj tfiivtkcp pfl jyflcu "
+              "sv rscv kf ivru kyzj"), -17))
+# >>> ???
