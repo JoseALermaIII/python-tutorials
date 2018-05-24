e0dcdef388d045a7ffbd362b5109a5af43142f2a
JoseALermaIII <jose@JoseALerma.com>
2018-05-23 21:21:55 -0500

    Update Q27-Lookup: working solution

--- Udacity/CS101/Lesson 18 - How Programs Run/Q27-Lookup.py
+++ Udacity/CS101/Lesson 18 - How Programs Run/Q27-Lookup.py
@@ -7,22 +7,30 @@
 # and returns the value associated
 # with that key.
 
-def hashtable_lookup(htable,key):
+
+def hashtable_lookup(htable, key):
+    bucket = hashtable_get_bucket(htable, key)
+    for index in range(0, len(bucket)):
+        if key in bucket[index]:
+            return bucket[index][1]
+    return None
 
 
-def hashtable_add(htable,key,value):
-    bucket = hashtable_get_bucket(htable,key)
-    bucket.append([key,value])
+def hashtable_add(htable, key, value):
+    bucket = hashtable_get_bucket(htable, key)
+    bucket.append([key, value])
 
 
-def hashtable_get_bucket(htable,keyword):
-    return htable[hash_string(keyword,len(htable))]
+def hashtable_get_bucket(htable, keyword):
+    return htable[hash_string(keyword, len(htable))]
 
-def hash_string(keyword,buckets):
+
+def hash_string(keyword, buckets):
     out = 0
     for s in keyword:
         out = (out + ord(s)) % buckets
     return out
+
 
 def make_hashtable(nbuckets):
     table = []
@@ -31,15 +39,14 @@ def make_hashtable(nbuckets):
     return table
 
 
-#table = [[['Ellis', 11], ['Francis', 13]], [], [['Bill', 17], ['Zoe', 14]],
-#[['Coach', 4]], [['Louis', 29], ['Nick', 2], ['Rochelle', 4]]]
+table = [[['Ellis', 11], ['Francis', 13]], [], [['Bill', 17], ['Zoe', 14]],
+        [['Coach', 4]], [['Louis', 29], ['Nick', 2], ['Rochelle', 4]]]
 
-#print hashtable_lookup(table, 'Francis')
-#>>> 13
+print(hashtable_lookup(table, 'Francis'))
+# >>> 13
 
-#print hashtable_lookup(table, 'Louis')
-#>>> 29
+print(hashtable_lookup(table, 'Louis'))
+# >>> 29
 
-#print hashtable_lookup(table, 'Zoe')
-#>>> 14
-
+print(hashtable_lookup(table, 'Zoe'))
+# >>> 14
