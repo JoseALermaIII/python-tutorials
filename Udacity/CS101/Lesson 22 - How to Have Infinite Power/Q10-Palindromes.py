1b696942d779d5abe51ca65c75606c58f4006a4f
JoseALermaIII <jose@JoseALerma.com>
2018-05-25 12:37:50 -0500

    Update Q10-Palindromes: working solution

--- Udacity/CS101/Lesson 22 - How to Have Infinite Power/Q10-Palindromes.py
+++ Udacity/CS101/Lesson 22 - How to Have Infinite Power/Q10-Palindromes.py
@@ -5,11 +5,21 @@
 # Recursive Case: if first and last characters don't match => False
 # if they do match, is the middle a palindrome?
 
-def is_palindrome(s):
 
-# print is_palindrome('')
+def is_palindrome(s):
+    if s == '':
+        return True
+    else:
+        if s[0] != s[-1]:
+            return False
+        return is_palindrome(s[1:-1])
+
+
+print(is_palindrome(''))
 # >>> True
-# print is_palindrome('abab')
+print(is_palindrome('abab'))
 # >>> False
-# print is_palindrome('abba')
+print(is_palindrome('abba'))
+# >>> True
+print(is_palindrome('abaaba'))
 # >>> True
