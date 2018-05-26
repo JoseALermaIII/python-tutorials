781220062c06a6f7cdf330d82994a6d8ec47e20f
JoseALermaIII <jose@JoseALerma.com>
2018-05-26 15:56:15 -0500

    Update Q15-Faster Fibonacci: fixed for smaller fibonacci numbers

--- Udacity/CS101/Lesson 22 - How to Have Infinite Power/Q15-Faster Fibonacci.py
+++ Udacity/CS101/Lesson 22 - How to Have Infinite Power/Q15-Faster Fibonacci.py
@@ -4,12 +4,19 @@
 
 def fibonacci(n):
     i, j = 0, 1
-    n -= 2
+    n -= 1
     while n >= 0:
         i, j = j, j + i
         n -= 1
-    return j
+    return i
 
-
+print(fibonacci(0))
+# >>> 0
+print(fibonacci(1))
+# >>> 1
+print(fibonacci(2))
+# >>> 1
+print(fibonacci(3))
+# >>> 2
 print(fibonacci(36))
 # >>> 14930352
