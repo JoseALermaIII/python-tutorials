fc7272937ab05fbca11c3fdba4804f9fbb0926fc
JoseALermaIII <jose@JoseALerma.com>
2018-05-26 15:50:37 -0500

    Update Q15-Faster Fibonacci: working solution

--- Udacity/CS101/Lesson 22 - How to Have Infinite Power/Q15-Faster Fibonacci.py
+++ Udacity/CS101/Lesson 22 - How to Have Infinite Power/Q15-Faster Fibonacci.py
@@ -1,11 +1,15 @@
-#Define a faster fibonacci procedure that will enable us to computer
-#fibonacci(36).
+# Define a faster fibonacci procedure that will enable us to compute
+# fibonacci(36).
+
 
 def fibonacci(n):
+    i, j = 0, 1
+    n -= 2
+    while n >= 0:
+        i, j = j, j + i
+        n -= 1
+    return j
 
 
-
-
-
-#print fibonacci(36)
-#>>> 14930352
+print(fibonacci(36))
+# >>> 14930352
