8e82824b2bd5abc573b415798febfd9aa620548c
JoseALermaIII <jose@JoseALerma.com>
2018-05-29 17:20:39 -0500

    Update Q3-Spreading Udaciousness: working solution

--- Udacity/CS101/Lesson 23 - Problem Set/Q3-Spreading Udaciousness.py
+++ Udacity/CS101/Lesson 23 - Problem Set/Q3-Spreading Udaciousness.py
@@ -18,26 +18,30 @@
 
 # For credit, your procedure must not use: while, for, or import math.
 
+
 def hexes_to_udaciousness(n, spread, target):
+    if n >= target:
+        return 0
+    else:
+        return 1 + hexes_to_udaciousness((n + (n * spread)), spread, target)
+
 
 # 0 more needed, since n already exceeds target
-# print hexes_to_udaciousness(100000, 2, 36230)
+print(hexes_to_udaciousness(100000, 2, 36230))
 # >>> 0
 
 # after 1 hexamester, there will be 50000 + (50000 * 2) Udacians
-# print hexes_to_udaciousness(50000, 2, 150000)
+print(hexes_to_udaciousness(50000, 2, 150000))
 # >>> 1
 
 # need to match or exceed the target
-# print hexes_to_udaciousness(50000, 2, 150001)
+print(hexes_to_udaciousness(50000, 2, 150001))
 # >>> 2
 
 # only 12 hexamesters (2 years) to world domination!
-# print hexes_to_udaciousness(20000, 2, 7 * 10 ** 9)
+print(hexes_to_udaciousness(20000, 2, 7 * 10 ** 9))
 # >>> 12
 
 # more friends means faster world domination!
-# print hexes_to_udaciousness(15000, 3, 7 * 10 ** 9)
+print(hexes_to_udaciousness(15000, 3, 7 * 10 ** 9))
 # >>> 10
-
-
