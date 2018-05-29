338ae4b766996f9155697ecaadce1902bbaac99f
JoseALermaIII <jose@JoseALerma.com>
2018-05-29 17:42:02 -0500

    Update Q4-Deep Count: working solution

--- Udacity/CS101/Lesson 23 - Problem Set/Q4-Deep Count.py
+++ Udacity/CS101/Lesson 23 - Problem Set/Q4-Deep Count.py
@@ -20,17 +20,24 @@ def is_list(p):
 # input is a List, and returns False otherwise.
 
 def deep_count(p):
+    count = 0
+    for element in p:
+        count += 1
+        if is_list(element):
+            count += deep_count(element)
+    return count
 
-# print deep_count([1, 2, 3])
+
+print(deep_count([1, 2, 3]))
 # >>> 3
 
 # The empty list still counts as an element of the outer list
-# print deep_count([1, [], 3])
+print(deep_count([1, [], 3]))
 # >>> 3
 
-# print deep_count([1, [1, 2, [3, 4]]])
+print(deep_count([1, [1, 2, [3, 4]]]))
 # >>> 7
 
-# print deep_count([[[[[[[[1, 2, 3]]]]]]]])
+print(deep_count([[[[[[[[1, 2, 3]]]]]]]]))
 # >>> 10
 
