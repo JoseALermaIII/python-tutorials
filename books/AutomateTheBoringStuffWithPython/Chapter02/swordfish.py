fc882ddca13a01bf6fa6f4232b1f88b6ae5906dd
JoseALermaIII <jose@JoseALerma.com>
2018-06-11 03:13:37 -0500

    Initialize swordfish

---
+++ books/AutomateTheBoringStuffWithPython/Chapter02/swordfish.py
@@ -0,0 +1,11 @@
+# This program asks for a name and password
+while True:
+    print('Who are you?')
+    name = input()
+    if name != 'Joe':
+        continue
+    print('Hello, Joe. What is the password? (It is a fish.)')
+    password = input()
+    if password == 'swordfish':
+        break
+print('Access granted.')
