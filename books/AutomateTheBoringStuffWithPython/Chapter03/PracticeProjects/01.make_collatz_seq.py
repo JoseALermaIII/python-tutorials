dcb1bb6320d4d527d6f2abf8213efd91483f3f8b
JoseALermaIII <jose@JoseALerma.com>
2018-06-12 18:42:47 -0500

    Initialize 01.make_collatz_seq

---
+++ books/AutomateTheBoringStuffWithPython/Chapter03/PracticeProjects/01.make_collatz_seq.py
@@ -0,0 +1,19 @@
+# This program makes a Collatz Sequence for a given number
+
+# Write a function named collatz() that has one parameter named number.
+# If number is even, then collatz() should print number // 2 and return this value.
+# If number is odd, then collatz() should print and return 3 * number + 1.
+
+# Then write a program that lets the user type in an integer and that keeps calling
+# collatz() on that number until the function returns the value 1.
+
+# Sample output:
+# Enter number:
+# 3
+# 10
+# 5
+# 16
+# 8
+# 4
+# 2
+# 1
