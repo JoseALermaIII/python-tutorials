edde7727f903e31857c4843c8fb57e60d1f1c07c
JoseALermaIII <jose@JoseALerma.com>
2018-06-12 22:22:52 -0500

    Initialize P04_magic8Ball2

---
+++ books/AutomateTheBoringStuffWithPython/Chapter04/P04_magic8Ball2.py
@@ -0,0 +1,14 @@
+# This program more efficiently answers your questions
+import random
+
+messages = ['It is certain',
+            'It is decidedly so',
+            'Yes definitely',
+            'Reply hazy try again',
+            'Ask again later',
+            'Concentrate and ask again',
+            'My reply is no',
+            'Outlook not so good',
+            'Very doubtful']
+
+print(messages[random.randint(0, len(messages) - 1)])
