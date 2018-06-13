be45d9dbf219becd7792a49f065d7c885aaeed27
JoseALermaIII <jose@JoseALerma.com>
2018-06-12 21:27:25 -0500

    Initialize P02_input_validation

---
+++ books/AutomateTheBoringStuffWithPython/Chapter03/PracticeProjects/P02_input_validation.py
@@ -0,0 +1,7 @@
+# This program adds input validation to make_collatz_seq.py
+
+# Add try and except statements to the previous project to detect whether
+# the user types in a noninteger string. Normally, the int() function will
+# raise a ValueError error if it is passed a noninteger string, as in int('puppy').
+# In the except clause, print a message to the user saying they must enter an integer.
+
