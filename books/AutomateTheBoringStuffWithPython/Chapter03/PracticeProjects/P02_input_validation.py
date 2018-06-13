c33d645b22e995f907b1b5358509bd000abf87f6
JoseALermaIII <jose@JoseALerma.com>
2018-06-12 21:35:52 -0500

    Update P02_input_validation: working solution

--- books/AutomateTheBoringStuffWithPython/Chapter03/PracticeProjects/P02_input_validation.py
+++ books/AutomateTheBoringStuffWithPython/Chapter03/PracticeProjects/P02_input_validation.py
@@ -5,3 +5,13 @@
 # raise a ValueError error if it is passed a noninteger string, as in int('puppy').
 # In the except clause, print a message to the user saying they must enter an integer.
 
+from P01_make_collatz_seq import collatz
+
+try:
+    n = int(input("Input a number: "))
+    while n != 1:
+        print(n)
+        n = collatz(n)
+    print(n)  # When n == 1
+except ValueError:
+    print("Error: Input must be an integer.")
