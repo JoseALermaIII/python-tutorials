9efcf5c098a354d227d14a467290837c67fbacea
JoseALermaIII <jose@JoseALerma.com>
2018-06-12 22:14:02 -0500

    Initialize P03_myPets

---
+++ books/AutomateTheBoringStuffWithPython/Chapter04/P03_myPets.py
@@ -0,0 +1,8 @@
+# This program checks if a given pet is in a list of pets
+myPets = ['Zophie', 'Pooka', 'Fat-tail']
+print('Enter a pet name:')
+name = input()
+if name not in myPets:
+    print('I do not have a pet named ' + name)
+else:
+    print(name + ' is my pet.')
