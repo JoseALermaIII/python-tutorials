37008b7a8ed030b9e37b40e73c98fbd98abfd954
JoseALermaIII <jose@JoseALerma.com>
2018-06-13 23:58:41 -0500

    Update P04_ticTacToe: added printBoard

--- books/AutomateTheBoringStuffWithPython/Chapter05/P04_ticTacToe.py
+++ books/AutomateTheBoringStuffWithPython/Chapter05/P04_ticTacToe.py
@@ -2,3 +2,14 @@
 theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
             'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+
+printBoard(theBoard)
