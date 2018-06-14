736f6f3a233c4fe9905870abe4e499d1bd7abc5f
JoseALermaIII <jose@JoseALerma.com>
2018-06-13 22:24:31 -0500

    Initialize P01_comma_code

---
+++ books/AutomateTheBoringStuffWithPython/Chapter04/PracticeProjects/P01_comma_code.py
@@ -0,0 +1,11 @@
+# This program converts a list to a comma separated string
+#
+# Write a function that takes a list value as an argument and returns a string
+# with all the items separated by a comma and a space, with and inserted before
+# the last item.
+#
+# Say you have a list value like this:
+# spam = ['apples', 'bananas', 'tofu', 'cats']
+# For example, passing the previous spam list to the function would return
+# 'apples, bananas, tofu, and cats'. But your function should be able to work
+# with any list value passed to it.
