80f9e7cde4c8d9d549417655bb6e3f0a69dafd01
JoseALermaIII <jose@JoseALerma.com>
2018-06-14 00:34:31 -0500

    Update P01_fantasy_game_inventory: working solution

--- books/AutomateTheBoringStuffWithPython/Chapter05/PracticeProjects/P01_fantasy_game_inventory.py
+++ books/AutomateTheBoringStuffWithPython/Chapter05/PracticeProjects/P01_fantasy_game_inventory.py
@@ -1,4 +1,5 @@
 # This program models a player's inventory from a fantasy game
+#
 # You are creating a fantasy video game. The data structure to model the player’s
 # inventory will be a dictionary where the keys are string values describing the item
 # in the inventory and the value is an integer value detailing how many of that item
@@ -20,11 +21,14 @@
 # Total number of items: 62
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
+
 def displayInventory(inventory):
     print("Inventory:")
     item_total = 0
     for k, v in inventory.items():
-        # FILL THIS PART IN
+        item_total += v
+        print(str(v) + " " + k)
     print("Total number of items: " + str(item_total))
+
 
 displayInventory(stuff)
