06df0baaddb9edc3533bf6d309a0600f00a7720c
JoseALermaIII <jose@JoseALerma.com>
2018-06-13 23:54:34 -0500

    Initialize P04_ticTacToe

---
+++ books/AutomateTheBoringStuffWithPython/Chapter05/P04_ticTacToe.py
@@ -0,0 +1,4 @@
+# This program emulates a tic-tac-toe board
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
