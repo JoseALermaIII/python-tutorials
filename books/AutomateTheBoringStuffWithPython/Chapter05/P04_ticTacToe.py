6a43319f86c5568a0100410e7b062bbd2f8bf5b6
JoseALermaIII <jose@JoseALerma.com>
2018-06-14 00:08:56 -0500

    Update P04_ticTacToe: added inputs for turns

--- books/AutomateTheBoringStuffWithPython/Chapter05/P04_ticTacToe.py
+++ books/AutomateTheBoringStuffWithPython/Chapter05/P04_ticTacToe.py
@@ -12,4 +12,14 @@ def printBoard(board):
     print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
 
+turn = 'X'
+for i in range(9):
+    printBoard(theBoard)
+    print('Turn for ' + turn + '. Move on which space?')
+    move = input()
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
 printBoard(theBoard)
