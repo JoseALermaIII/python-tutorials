2a39ee59ded4d322f221b9334a232e8aae8a2ff5
JoseALermaIII <jose@JoseALerma.com>
2018-06-15 03:55:18 -0500

    Update P01_tablePrinter: working solution

--- books/AutomateTheBoringStuffWithPython/Chapter06/PracticeProjects/P01_tablePrinter.py
+++ books/AutomateTheBoringStuffWithPython/Chapter06/PracticeProjects/P01_tablePrinter.py
@@ -14,3 +14,36 @@
 #  oranges   Bob  cats
 # cherries Carol moose
 #   banana David goose
+
+
+def printTable(matrix):
+    # Calculate length of longest word in each row
+    colWidths = []
+    for row in matrix:
+        maxWidth = 0
+        for item in row:
+            if len(item) > maxWidth:
+                maxWidth = len(item)
+        colWidths.append(maxWidth)
+
+    # Sort lengths of words in each row and find longest word in matrix
+    colWidths = sorted(colWidths)
+    maxWidth = colWidths[-1]
+
+    # Print each column with right-justification of longest word in matrix
+    for index in range(len(matrix[0])):
+        output = ""
+        for row in matrix:
+            output += row[index].rjust(maxWidth)
+        print(output)
+
+
+def main():
+    tableData = [['apples', 'oranges', 'cherries', 'banana'],
+                 ['Alice', 'Bob', 'Carol', 'David'],
+                 ['dogs', 'cats', 'moose', 'goose']]
+    printTable(tableData)
+
+
+if __name__ == "__main__":
+    main()
