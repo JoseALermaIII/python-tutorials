a244ffe1814e8ad7966f90b212faebfaf35424be
JoseALermaIII <jose@JoseALerma.com>
2018-06-15 03:06:07 -0500

    Initialize P01_tablePrinter

---
+++ books/AutomateTheBoringStuffWithPython/Chapter06/PracticeProjects/P01_tablePrinter.py
@@ -0,0 +1,16 @@
+# This program displays a list of strings in a table
+#
+# Write a function named printTable() that takes a list of lists of strings and
+# displays it in a well-organized table with each column right-justified. Assume
+# that all the inner lists will contain the same number of strings.
+#
+# For example, the value could look like this:
+# tableData = [['apples', 'oranges', 'cherries', 'banana'],
+#              ['Alice', 'Bob', 'Carol', 'David'],
+#              ['dogs', 'cats', 'moose', 'goose']]
+#
+# Your printTable() function would print the following:
+#   apples Alice  dogs
+#  oranges   Bob  cats
+# cherries Carol moose
+#   banana David goose
