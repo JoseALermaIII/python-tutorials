d01c269e7f3e80c210fc9197576f50bd2401e312
JoseALermaIII <jose@JoseALerma.com>
2018-06-16 04:33:02 -0500

    Initialize P01_strong_pw_detect

---
+++ books/AutomateTheBoringStuffWithPython/Chapter07/PracticeProjects/P01_strong_pw_detect.py
@@ -0,0 +1,7 @@
+# Strong Password Detection - this program ensures passwords entered are "strong"
+#
+# Write a function that uses regular expressions to make sure the password string
+# it is passed is strong. A strong password is defined as one that is at least eight
+# characters long, contains both uppercase and lowercase characters, and has at least
+# one digit. You may need to test the string against multiple regex patterns to validate
+# its strength.
