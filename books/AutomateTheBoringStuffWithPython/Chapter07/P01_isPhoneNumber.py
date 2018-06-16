2295b7a69e88aa77792b3ed5601af031fdb19dae
JoseALermaIII <jose@JoseALerma.com>
2018-06-16 03:25:08 -0500

    Update P01_isPhoneNumber: adds sample string parser

--- books/AutomateTheBoringStuffWithPython/Chapter07/P01_isPhoneNumber.py
+++ books/AutomateTheBoringStuffWithPython/Chapter07/P01_isPhoneNumber.py
@@ -25,3 +25,10 @@ print('415-555-4242 is a phone number:')
 print(isPhoneNumber('415-555-4242'))
 print('Moshi moshi is a phone number:')
 print(isPhoneNumber('Moshi moshi'))
+
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+for i in range(len(message)):
+    chunk = message[i:i + 12]
+    if isPhoneNumber(chunk):
+        print('Phone number found: ' + chunk)
+print('Done')
