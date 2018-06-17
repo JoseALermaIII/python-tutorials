ba9175d8d411463039369047e07ceeb54f539514
JoseALermaIII <jose@JoseALerma.com>
2018-06-17 02:48:44 -0500

    Initialize P02_regex_strip

---
+++ books/AutomateTheBoringStuffWithPython/Chapter07/PracticeProjects/P02_regex_strip.py
@@ -0,0 +1,8 @@
+# Regex Version of strip() - this program acts like the strip() method but using regex
+#
+# Write a function that takes a string and does the same thing as the strip() string
+# method. If no other arguments are passed other than the string to strip, then
+# whitespace characters will be removed from the beginning and end of the string.
+# Otherwise, the characters specified in the second argument to the function will be
+# removed from the string.
+
