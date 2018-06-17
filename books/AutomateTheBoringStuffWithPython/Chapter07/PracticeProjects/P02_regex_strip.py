d31519f359ccd442e5da4780334f2e6cc189bd16
JoseALermaIII <jose@JoseALerma.com>
2018-06-17 03:08:54 -0500

    Update P02_regex_strip: working solution

--- books/AutomateTheBoringStuffWithPython/Chapter07/PracticeProjects/P02_regex_strip.py
+++ books/AutomateTheBoringStuffWithPython/Chapter07/PracticeProjects/P02_regex_strip.py
@@ -5,4 +5,21 @@
 # whitespace characters will be removed from the beginning and end of the string.
 # Otherwise, the characters specified in the second argument to the function will be
 # removed from the string.
+import re
 
+
+def regex_strip(text, replace=""):
+    if replace == "":
+        whitespaceRegex = re.compile(r"^\s+(\w+)\s+$")  # group characters between whitespace
+        match = whitespaceRegex.search(text)
+        return match.group(1)
+    else:
+        subRegex = re.compile(replace)  # match replacement text
+        return subRegex.sub("", text)
+
+
+message = "    something     "
+print(regex_strip(message))
+print(regex_strip(message, "some"))
+message = "Supercalifragilisticexpialidocious"
+print(regex_strip(message, "cali"))
