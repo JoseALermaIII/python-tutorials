815313d5034da8fad8eb22c1c7b2a026aa731222
JoseALermaIII <jose@JoseALerma.com>
2018-06-20 05:19:42 -0500

    Update P03_regex_search: working solution

--- books/AutomateTheBoringStuffWithPython/Chapter08/PracticeProjects/P03_regex_search.py
+++ books/AutomateTheBoringStuffWithPython/Chapter08/PracticeProjects/P03_regex_search.py
@@ -2,3 +2,29 @@
 # that matches a user-supplied regular expression.
 #
 # The results should be printed to the screen.
+import os, re
+
+# Get list of all .txt files
+allfiles = os.listdir("./")  # use current working directory
+textfiles = []
+for file in allfiles:
+    if file.endswith(".txt"):
+        textfiles.append(file)
+
+# Get regular expression
+regex = input("Enter regular expression to search for: ")
+searchregex = re.compile(regex)
+
+# Open .txt file
+for file in textfiles:
+    inputfile = open(file)
+    inputcontent = inputfile.readlines()
+    inputfile.close()
+
+    # Search for regex in file
+    for line in inputcontent:
+        matchobjects = searchregex.findall(line)
+        if matchobjects is not None:
+            # Print result
+            for match in matchobjects:
+                print(match)
