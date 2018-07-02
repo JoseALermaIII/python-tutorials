1d273380f1e853c39648e004f11a802bcb949806
JoseALermaIII <jose@JoseALerma.com>
2018-07-02 03:17:23 -0500

    Update P03_zipfile: added extracting from zip files

--- books/AutomateTheBoringStuffWithPython/Chapter09/P03_zipfile.py
+++ books/AutomateTheBoringStuffWithPython/Chapter09/P03_zipfile.py
@@ -2,6 +2,8 @@
 import zipfile, os
 os.chdir('./')  # move to the folder with example.zip
 exampleZip = zipfile.ZipFile('example.zip')
+
+# Reading ZIP Files
 print(exampleZip.namelist())
 # ['spam.txt', 'cats/', 'cats/catnames.txt', 'cats/zophie.jpg']
 spamInfo = exampleZip.getinfo('spam.txt')
@@ -11,4 +13,8 @@ print(spamInfo.compress_size)  # in bytes
 # 25
 print('Compressed file is %sx smaller!' % (round(spamInfo.file_size / spamInfo.compress_size, 2)))
 # Compressed file is 1.0x smaller!
+
+# Extracting from ZIP Files
+#exampleZip.extractall()  # extracts all files to current working directory
+exampleZip.extractall('./testdir')  # auto-creates folder
 exampleZip.close()
