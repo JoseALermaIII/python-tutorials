6962805936192b77c2a91e3c383de811b7eb5642
JoseALermaIII <jose@JoseALerma.com>
2018-07-03 03:41:57 -0500

    Initialize P01_selectiveCopy

---
+++ books/AutomateTheBoringStuffWithPython/Chapter09/PracticeProjects/P01_selectiveCopy.py
@@ -0,0 +1,3 @@
+# Write a program that walks through a folder tree and searches for files with a
+# certain file extension (such as .pdf or .jpg). Copy these files from whatever
+# location they are in to a new folder.
