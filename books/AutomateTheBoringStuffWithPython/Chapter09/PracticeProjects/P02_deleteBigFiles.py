12d35b2177eb604d0a3b5efb6dfe4ea11ddf6a4a
JoseALermaIII <jose@JoseALerma.com>
2018-07-04 02:40:19 -0500

    Update P02_deleteBigFiles Working solution

--- books/AutomateTheBoringStuffWithPython/Chapter09/PracticeProjects/P02_deleteBigFiles.py
+++ books/AutomateTheBoringStuffWithPython/Chapter09/PracticeProjects/P02_deleteBigFiles.py
@@ -7,3 +7,17 @@
 # large files or folders—say, ones that have a file size of more than 100MB.
 # (Remember, to get a file’s size, you can use os.path.getsize() from the os module.)
 # Print these files with their absolute path to the screen.
+#
+# Note:
+# - testfile.txt was created by typing truncate -s 101M testfile.txt in a terminal
+
+import os
+
+FOLDER = "./"
+FILESIZE = 100 * (1024 ** 2)  # 1024 ** 2 == 1 MB
+
+for foldername, subfolders, filenames in os.walk(FOLDER):
+    for filename in filenames:
+        if os.path.getsize(filename) > FILESIZE:
+            #os.unlink(filename)
+            print(filename)  # DEBUG
