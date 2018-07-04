750cdd40fc00050eac160f5feb59f32a91a9b3df
JoseALermaIII <jose@JoseALerma.com>
2018-07-04 02:16:14 -0500

    Update P01_delete Safe to run so long as os.unlink stays commented out

--- books/AutomateTheBoringStuffWithPython/Chapter09/P01_delete.py
+++ books/AutomateTheBoringStuffWithPython/Chapter09/P01_delete.py
@@ -1,7 +1,6 @@
 # This program permanently deletes files ending with a .txt extension
 #
 # Note:
-# - Do not run this program for numerous reasons.
 # - Demonstrates testing with delete functions
 import os
 for filename in os.listdir('./'):
