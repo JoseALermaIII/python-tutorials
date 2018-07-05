269c5b69887cc07492baf8374dc4bf62a743b51b
JoseALermaIII <jose@JoseALerma.com>
2018-07-05 03:44:56 -0500

    Initialize P03_fillGaps

---
+++ books/AutomateTheBoringStuffWithPython/Chapter09/PracticeProjects/P03_fillGaps.py
@@ -0,0 +1,5 @@
+# Write a program that finds all files with a given prefix, such as spam001.txt,
+# spam002.txt, and so on, in a single folder and locates any gaps in the numbering
+# (such as if there is a spam001.txt and spam003.txt but no spam002.txt).
+#
+# Have the program rename all the later files to close this gap.
