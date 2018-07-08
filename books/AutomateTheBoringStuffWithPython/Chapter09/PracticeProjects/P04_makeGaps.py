25a8b61b2aa9324eed269edd0c46e9ec6740e54d
JoseALermaIII <jose@JoseALerma.com>
2018-07-08 18:35:45 -0500

    Update P04_makeGaps Working solution

--- books/AutomateTheBoringStuffWithPython/Chapter09/PracticeProjects/P04_makeGaps.py
+++ books/AutomateTheBoringStuffWithPython/Chapter09/PracticeProjects/P04_makeGaps.py
@@ -6,3 +6,52 @@
 #
 # As an added challenge, write another program that can insert gaps into numbered
 # files so that a new file can be added.
+
+from books.AutomateTheBoringStuffWithPython.Chapter09.PracticeProjects.P03_fillGaps \
+    import is_sequence, get_filenames, get_numbers
+
+import os
+
+
+def make_gaps(folder, gap):
+    # Get names of all files
+    files = get_filenames(folder)
+
+    # Get list of numbers used in file names
+    numlist = get_numbers(files)
+
+    # Check if there is a sequence
+    if not is_sequence(numlist):
+        print("There is already a gap in the files.")
+        return None
+
+    # Rename files after gap point to create gap
+    files.sort(reverse=True)  # Start from end to prevent overwrites
+    for file in files:
+        prefix, number, suffix = file[0], file[1], file[2]
+        if gap <= int(number):
+            # Get file names
+            numlen = len(number)
+            oldfilename = ''.join(file)
+            newfilename = prefix + str(int(number) + 1).zfill(numlen) + suffix
+
+            # Get absolute paths
+            workingdir = os.path.abspath(folder)
+            oldfilepath = os.path.join(workingdir, oldfilename)
+            newfilepath = os.path.join(workingdir, newfilename)
+
+            # Rename file
+            print("Renaming '%s' to '%s'..." % (oldfilename, newfilename))  # DEBUG
+            #os.rename(oldfilepath, newfilepath)  # Uncomment after testing
+
+    return None
+
+
+def main():
+    make_gaps("./testdir", 2)
+    return None
+
+
+# If P04_makeGaps.py is run instead of imported, run main():
+if __name__ == "__main__":
+    main()
