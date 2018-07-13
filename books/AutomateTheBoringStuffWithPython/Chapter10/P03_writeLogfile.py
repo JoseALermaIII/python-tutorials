176c6a349018c979a77e7dbd14fc666dbd67baa3
JoseALermaIII <jose@JoseALerma.com>
2018-07-13 09:42:49 -0500

    Initialize P03_writeLogfile

---
+++ books/AutomateTheBoringStuffWithPython/Chapter10/P03_writeLogfile.py
@@ -0,0 +1,11 @@
+# This program raises an exception but outputs traceback to a logfile
+
+import traceback
+
+try:
+    raise Exception('This is the error message.')
+except:
+    errorFile = open('errorInfo.txt', 'w')
+    errorFile.write(traceback.format_exc())
+    errorFile.close()
+    print('The traceback info was written to errorInfo.txt.')
