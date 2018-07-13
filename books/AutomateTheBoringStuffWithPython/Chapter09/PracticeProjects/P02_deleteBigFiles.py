3d32e9268f1232bd3f7a3f1de6c3e0a871ca2ea6
JoseALermaIII <jose@JoseALerma.com>
2018-07-13 09:10:39 -0500

    Update P02_deleteBigFiles fixed typo

--- books/AutomateTheBoringStuffWithPython/Chapter09/PracticeProjects/P02_deleteBigFiles.py
+++ books/AutomateTheBoringStuffWithPython/Chapter09/PracticeProjects/P02_deleteBigFiles.py
@@ -1,4 +1,4 @@
-# t’s not uncommon for a few unneeded but humongous files or folders to take up the
+# It’s not uncommon for a few unneeded but humongous files or folders to take up the
 # bulk of the space on your hard drive. If you’re trying to free up room on your
 # computer, you’ll get the most bang for your buck by deleting the most massive of
 # the unwanted files. But first you have to find them.
