a8046c509037e2116b960b3d9af9ee504292ab25
JoseALermaIII <jose@JoseALerma.com>
2018-07-20 12:14:42 -0500

    Initialize P07_buggyAddingProgram

---
+++ books/AutomateTheBoringStuffWithPython/Chapter10/P07_buggyAddingProgram.py
@@ -0,0 +1,9 @@
+# This program adds three numbers and displays the sum
+
+print("Enter the first number to add:")
+first = input()
+print("Enter the second number to add:")
+second = input()
+print("Enter the third number to add:")
+third = input()
+print("The sum is " + first + second + third)
