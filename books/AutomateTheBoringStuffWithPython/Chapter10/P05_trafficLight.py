ece94bc0beac559b601c990cc0a54a8bc93963db
JoseALermaIII <jose@JoseALerma.com>
2018-07-19 20:14:33 -0500

    Initialize P05_trafficLight

---
+++ books/AutomateTheBoringStuffWithPython/Chapter10/P05_trafficLight.py
@@ -0,0 +1,17 @@
+# This program emulates traffic lights at intersections with assertions
+
+market_2nd = {"ns": "green", "ew": "red"}
+mission_16th = {"ns": "red", "ew": "green"}
+
+
+def switchLights(stoplight):
+    for key in stoplight.keys():
+        if stoplight[key] == "green":
+            stoplight[key] = "yellow"
+        elif stoplight[key] == "yellow":
+            stoplight[key] = "red"
+        elif stoplight[key] == "red":
+            stoplight[key] = "green"
+
+
+switchLights(market_2nd)
