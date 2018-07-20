118869f815da46abf841f858129324ad97ecbf45
JoseALermaIII <jose@JoseALerma.com>
2018-07-19 20:27:24 -0500

    Update P06_factorialLog fixed starting factorial at zero

--- books/AutomateTheBoringStuffWithPython/Chapter10/P06_factorialLog.py
+++ books/AutomateTheBoringStuffWithPython/Chapter10/P06_factorialLog.py
@@ -8,7 +8,7 @@ logging.debug("Start of program")
 def factorial(n):
     logging.debug("Start of factorial(%s%%)" % n)
     total = 1
-    for i in range(n + 1):
+    for i in range(1, n + 1):
         total *= i
         logging.debug("i is " + str(i) + ", total is " + str(total))
     logging.debug("End of factorial(%s%%)" % n)
