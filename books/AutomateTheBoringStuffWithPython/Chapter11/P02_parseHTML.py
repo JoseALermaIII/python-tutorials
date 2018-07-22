4eabe926a072bcdf914757650d42dc0546a31946
JoseALermaIII <jose@JoseALerma.com>
2018-07-22 04:53:38 -0500

    Update P02_parseHTML added BeautifulSoup object example

--- books/AutomateTheBoringStuffWithPython/Chapter11/P02_parseHTML.py
+++ books/AutomateTheBoringStuffWithPython/Chapter11/P02_parseHTML.py
@@ -1,3 +1,13 @@
 # This program uses requests to fetch an HTML page and BeautifulSoup to parse it
 
 import requests, bs4
+
+# Creating a BeautifulSoup Object from HTML
+res = requests.get("http://nostarch.com")
+res.raise_for_status()  # Raise error if nothing fetched
+noStarchSoup = bs4.BeautifulSoup(res.text, "lxml")  # Specify parser to avoid warning
+print(type(noStarchSoup))
+
+exampleFile = open("example.html")
+exampleSoup = bs4.BeautifulSoup(exampleFile, "lxml")
+print(type(exampleSoup))
