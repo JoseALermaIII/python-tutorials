13b27d782752ab47ec2abadf02a35ce96b8c7748
JoseALermaIII <jose@JoseALerma.com>
2018-07-22 05:32:46 -0500

    Update debugCoinToss ensure input is always lowercase

--- books/AutomateTheBoringStuffWithPython/Chapter10/PracticeProjects/debugCoinToss.py
+++ books/AutomateTheBoringStuffWithPython/Chapter10/PracticeProjects/debugCoinToss.py
@@ -4,7 +4,7 @@ import random
 guess = ''
 while guess not in ('heads', 'tails'):
     print('Guess the coin toss! Enter heads or tails:')
-    guess = input()
+    guess = input().lower()
 
 toss = random.randint(0, 1)  # 0 is tails, 1 is heads
 if toss == 0:
