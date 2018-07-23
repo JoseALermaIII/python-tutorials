f2cc2dc693eef38c947f75055d5cece9233cac77
JoseALermaIII <jose@JoseALerma.com>
2018-07-23 03:20:49 -0500

    Update P03_lucky added delay between tabs for slower machines

--- books/AutomateTheBoringStuffWithPython/Chapter11/P03_lucky.py
+++ books/AutomateTheBoringStuffWithPython/Chapter11/P03_lucky.py
@@ -1,7 +1,7 @@
 #! python3
 # lucky.py - Opens several Google search results.
 
-import requests, sys, webbrowser, bs4
+import requests, sys, webbrowser, bs4, time
 
 print("Googling...")  # display text while downloading the Google page
 res = requests.get("http://google.com/search?q=" + ' '.join(sys.argv[1:]))
@@ -13,5 +13,8 @@ soup = bs4.BeautifulSoup(res.text, "lxml")
 # Open a browser tab for each result.
 linkElems = soup.select(".r a")
 numOpen = min(5, len(linkElems))
-for i in range(numOpen):
-    webbrowser.open("http://google.com" + linkElems[i].get("href"))
+webbrowser.open("http://google.com" + linkElems[0].get("href"))
+time.sleep(10)  # add delay for slow virtual machine
+for i in range(1, numOpen):
+    webbrowser.open_new_tab("http://google.com" + linkElems[i].get("href"))
+    time.sleep(7)
