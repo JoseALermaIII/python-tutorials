85890cae67737477c8499058c467ddaf21675aec
JoseALermaIII <jose@JoseALerma.com>
2018-07-23 02:28:10 -0500

    Update P03_lucky specify parser to prevent warning

--- books/AutomateTheBoringStuffWithPython/Chapter11/P03_lucky.py
+++ books/AutomateTheBoringStuffWithPython/Chapter11/P03_lucky.py
@@ -8,7 +8,7 @@ res = requests.get("http://google.com/search?q=" + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
 # Retrieve top search result links.
-soup = bs4.BeautifulSoup(res.text)
+soup = bs4.BeautifulSoup(res.text, "lxml")
 
 # Open a browser tab for each result.
 linkElems = soup.select(".r a")
