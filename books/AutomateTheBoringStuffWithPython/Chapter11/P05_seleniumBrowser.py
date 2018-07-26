eb5d977e1fd19bfc24ee0d9fe90bf3afd08eb1c8
JoseALermaIII <jose@JoseALerma.com>
2018-07-26 05:16:56 -0500

    Update P05_seleniumBrowser added forms example

--- books/AutomateTheBoringStuffWithPython/Chapter11/P05_seleniumBrowser.py
+++ books/AutomateTheBoringStuffWithPython/Chapter11/P05_seleniumBrowser.py
@@ -25,3 +25,11 @@ linkElem = browser.find_element_by_link_text("Read Online for Free")
 print(type(linkElem))
 linkElem.click()  # follows the "Read Online for Free" link
 
+# Filling Out and Submitting Forms
+browser.get("https://mail.yahoo.com")
+emailElem = browser.find_element_by_id("login-username")
+emailElem.send_keys("not_my_real_email")
+passwordElem = browser.find_element_by_id("login-passwd")
+passwordElem.send_keys("12345")
+passwordElem.submit()
+
