eaba3e8c5f1c357b3b37507924f8c7f1303fcead
JoseALermaIII <jose@JoseALerma.com>
2018-07-29 03:51:12 -0500

    Initialize P1_commandLineEmailer

---
+++ books/AutomateTheBoringStuffWithPython/Chapter11/PracticeProjects/P1_commandLineEmailer.py
@@ -0,0 +1,4 @@
+# Write a program that takes an email address and string of text on the command line
+# and then, using Selenium, logs into your email account and sends an email of the
+# string to the provided address.
+# (You might want to set up a separate email account for this program.)
