bf4657ad0a37345eb449dca3eb25a0eb3c990e09
JoseALermaIII <jose@JoseALerma.com>
2018-07-29 04:56:41 -0500

    Update P1_commandLineEmailer add up to login to email account

--- books/AutomateTheBoringStuffWithPython/Chapter11/PracticeProjects/P1_commandLineEmailer.py
+++ books/AutomateTheBoringStuffWithPython/Chapter11/PracticeProjects/P1_commandLineEmailer.py
@@ -2,3 +2,35 @@
 # and then, using Selenium, logs into your email account and sends an email of the
 # string to the provided address.
 # (You might want to set up a separate email account for this program.)
+#
+# Note:
+# - "string of text" must be within quotes, like shown
+
+import sys
+from selenium import webdriver, common
+
+# Get arguments from command line
+email = ''.join(sys.argv[1])
+message = ''.join(sys.argv[2])
+
+# Open browser to email account
+browser = webdriver.Firefox()
+browser.get("https://www.gmail.com/")
+
+# Login to e-mail account
+try:
+    emailElem = browser.find_element_by_id("identifierId")
+    emailElem.send_keys("not_my_real_email")
+    emailElem.submit()
+
+    passwordElem = browser.find_element_by_name("password")
+    passwordElem.send_keys("not_my_real_password")
+    passwordElem.submit()
+except common.exceptions.NoSuchElementException as err:
+    print("Unable to locate element: %s" % err)
+
+# TODO: Open new message and fill To and Message fields
+
+# TODO: Send e-mail
+
+# TODO: Close browser
