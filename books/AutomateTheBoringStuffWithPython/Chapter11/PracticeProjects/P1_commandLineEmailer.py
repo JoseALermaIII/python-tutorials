e1d597a84d6e9c144c95cd75ce2cd14700ad7449
JoseALermaIII <jose@JoseALerma.com>
2018-07-30 05:15:53 -0500

    Update P1_commandLineEmailer add open, fill, and send message; and close browser

--- books/AutomateTheBoringStuffWithPython/Chapter11/PracticeProjects/P1_commandLineEmailer.py
+++ books/AutomateTheBoringStuffWithPython/Chapter11/PracticeProjects/P1_commandLineEmailer.py
@@ -5,9 +5,12 @@
 #
 # Note:
 # - "string of text" must be within quotes, like shown
+# - "new" gmail layout is used with keyboard shortcuts enabled
+# - not fully tested because account could end up banned for going against TOS
 
 import sys
 from selenium import webdriver, common
+from selenium.webdriver.common.keys import Keys
 
 # Get arguments from command line
 email = ''.join(sys.argv[1])
@@ -29,8 +32,24 @@ try:
 except common.exceptions.NoSuchElementException as err:
     print("Unable to locate element: %s" % err)
 
-# TODO: Open new message and fill To and Message fields
+# Open new message and fill To and Message fields
+try:
+    buttonElem = browser.find_element_by_link_text("Compose")
+    buttonElem.submit()
 
-# TODO: Send e-mail
+    toElem = browser.find_element_by_name("to")
+    toElem.send_keys(email)
 
-# TODO: Close browser
+    subjectElem = browser.find_element_by_name("subjectbox")
+    subjectElem.send_keys("Auto Generated Message")
+
+    messageElem = browser.find_element_by_id(":ia")
+    messageElem.send_keys(message)
+
+    # Send e-mail with keyboard shortcut
+    subjectElem.send_keys(Keys.LEFT_CONTROL + Keys.ENTER)
+except common.exceptions.NoSuchElementException as err:
+    print("Unable to locate element: %s" % err)
+
+# Close browser
+browser.quit()
