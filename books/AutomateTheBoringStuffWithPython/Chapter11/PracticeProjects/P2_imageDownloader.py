f8d12d8f7bc2c36c36789ca7a07649c764ef4f7a
JoseALermaIII <jose@JoseALerma.com>
2018-07-30 05:33:55 -0500

    Update P2_imageDownloader add open browser to site

--- books/AutomateTheBoringStuffWithPython/Chapter11/PracticeProjects/P2_imageDownloader.py
+++ books/AutomateTheBoringStuffWithPython/Chapter11/PracticeProjects/P2_imageDownloader.py
@@ -2,3 +2,16 @@
 # searches for a category of photos, and then downloads all the resulting images.
 # You could write a program that works with any photo site that has a search feature.
 
+import requests, os, bs4
+from selenium import webdriver, common
+
+# Open Browser to photo-sharing site
+url = "https://imgur.com"               # starting url
+os.makedirs("images", exist_ok=True)    # store images in ./images
+
+browser = webdriver.Firefox()
+browser.get(url)
+
+# Search for category of photos
+
+# Download all images
