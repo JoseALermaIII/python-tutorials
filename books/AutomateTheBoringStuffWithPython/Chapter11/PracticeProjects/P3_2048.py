adc75c5b5116f68c0c37e7a5cbebfbd95e152be1
JoseALermaIII <jose@JoseALerma.com>
2018-08-03 03:52:17 -0500

    Update P3_2048 working solution

--- books/AutomateTheBoringStuffWithPython/Chapter11/PracticeProjects/P3_2048.py
+++ books/AutomateTheBoringStuffWithPython/Chapter11/PracticeProjects/P3_2048.py
@@ -4,3 +4,36 @@
 # Write a program that will open the game at https://gabrielecirulli.github.io/2048/
 # and keep sending up, right, down, and left keystrokes to automatically play the game.
 
+from selenium import webdriver, common
+from selenium.webdriver.common.keys import Keys
+
+url = "https://gabrielecirulli.github.io/2048/"
+
+# Start Browser and go to 2048 game
+browser = webdriver.Firefox()
+browser.implicitly_wait(3)  # seconds
+browser.get(url)
+
+try:
+    htmlElem = browser.find_element_by_tag_name("html")
+    # Repeatedly send up, right, down, left
+    while not browser.find_element_by_class_name("retry-button").is_displayed():
+        htmlElem.send_keys(Keys.ARROW_UP)
+        htmlElem.send_keys(Keys.ARROW_RIGHT)
+        htmlElem.send_keys(Keys.ARROW_DOWN)
+        htmlElem.send_keys(Keys.ARROW_LEFT)
+    # Get current score and best score
+    scoreElem = browser.find_element_by_class_name("score-container")
+    score = scoreElem.text
+    bestElem = browser.find_element_by_class_name("best-container")
+    best = bestElem.text
+
+    # Display current score and best score
+    print("Current score: %s" % score)  # FIXME: Displays last point addition. e.g.: +4
+    print("Best score: %s" % best)
+
+except common.exceptions.NoSuchElementException as err:
+    print("Unable to locate element: %s" % err)
+
+# Close browser
+#browser.close()
