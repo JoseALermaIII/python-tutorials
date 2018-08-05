d77797ff9a6f4d0f0573548843583755ea9da80f
JoseALermaIII <jose@JoseALerma.com>
2018-08-05 03:57:02 -0500

    Update P01_readingExcel changed depreciated methods to properties

--- books/AutomateTheBoringStuffWithPython/Chapter12/P01_readingExcel.py
+++ books/AutomateTheBoringStuffWithPython/Chapter12/P01_readingExcel.py
@@ -6,10 +6,10 @@ wb = openpyxl.load_workbook("example.xlsx")
 print(type(wb))
 
 # Getting Sheets from the Workbook
-print(wb.get_sheet_names())
-sheet = wb.get_sheet_by_name("Sheet3")
+print(wb.sheetnames)
+sheet = wb["Sheet3"]
 print(sheet)
 print(type(sheet))
 print(sheet.title)
-anotherSheet = wb.get_active_sheet()
+anotherSheet = wb.active
 print(anotherSheet)
