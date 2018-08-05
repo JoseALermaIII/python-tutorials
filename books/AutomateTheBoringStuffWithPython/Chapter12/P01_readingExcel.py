5a0d98ee62a72f7843baa6e09e5442937367bd06
JoseALermaIII <jose@JoseALerma.com>
2018-08-05 03:52:34 -0500

    Update P01_readingExcel add getting sheets from the workbook

--- books/AutomateTheBoringStuffWithPython/Chapter12/P01_readingExcel.py
+++ books/AutomateTheBoringStuffWithPython/Chapter12/P01_readingExcel.py
@@ -4,3 +4,12 @@
 import openpyxl
 wb = openpyxl.load_workbook("example.xlsx")
 print(type(wb))
+
+# Getting Sheets from the Workbook
+print(wb.get_sheet_names())
+sheet = wb.get_sheet_by_name("Sheet3")
+print(sheet)
+print(type(sheet))
+print(sheet.title)
+anotherSheet = wb.get_active_sheet()
+print(anotherSheet)
