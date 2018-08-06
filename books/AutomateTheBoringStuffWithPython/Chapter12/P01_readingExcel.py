dd147f7c5aea9a70fd546344c99e7169c5c04fe9
JoseALermaIII <jose@JoseALerma.com>
2018-08-06 03:25:10 -0500

    Update P01_readingExcel add version info and getting cells from the sheets

--- books/AutomateTheBoringStuffWithPython/Chapter12/P01_readingExcel.py
+++ books/AutomateTheBoringStuffWithPython/Chapter12/P01_readingExcel.py
@@ -2,6 +2,8 @@
 
 # Opening Excel Documents with OpenPyXL
 import openpyxl
+print(openpyxl.__version__)  # Using version 2.5.5
+
 wb = openpyxl.load_workbook("example.xlsx")
 print(type(wb))
 
@@ -13,3 +15,21 @@ print(type(sheet))
 print(sheet.title)
 anotherSheet = wb.active
 print(anotherSheet)
+
+# Getting Cells from the Sheets
+print(anotherSheet["A1"])
+print(anotherSheet["A1"].value)
+print(type(anotherSheet["A1"].value))
+c = anotherSheet["B1"]
+print(c.value)
+print("Row " + str(c.row) + ", Column " + c.column + " is " + c.value)
+print(anotherSheet["C1"].value)
+
+print(anotherSheet.cell(row=1, column=2))
+print(anotherSheet.cell(row=1, column=2).value)
+for i in range(1, 8, 2):
+    print(i, anotherSheet.cell(row=i, column=2).value)
+
+sheet = wb["Sheet1"]
+print(sheet.get_highest_row())
+print(sheet.get_highest_column())
