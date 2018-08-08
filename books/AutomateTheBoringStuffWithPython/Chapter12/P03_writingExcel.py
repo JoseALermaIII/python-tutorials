a073c5bcea283c277befe67f9892543e4fcc1acb
JoseALermaIII <jose@JoseALerma.com>
2018-08-08 04:28:55 -0500

    Update P03_writingExcel added creating and removing sheets

--- books/AutomateTheBoringStuffWithPython/Chapter12/P03_writingExcel.py
+++ books/AutomateTheBoringStuffWithPython/Chapter12/P03_writingExcel.py
@@ -14,3 +14,17 @@ wb = openpyxl.load_workbook("example.xlsx")
 sheet = wb.active
 sheet.title = "Spam Spam Spam"
 wb.save("example_copy.xlsx")
+
+# Creating and Removing Sheets
+wb = openpyxl.Workbook()
+print(wb.get_sheet_names())
+wb.create_sheet()
+print(wb.get_sheet_names())
+wb.create_sheet(index=0, title="First Sheet")
+print(wb.get_sheet_names())
+wb.create_sheet(index=2, title="Middle Sheet")
+print(wb.get_sheet_names())
+
+wb.remove_sheet(wb.get_sheet_by_name("Middle Sheet"))
+wb.remove_sheet(wb.get_sheet_by_name("Sheet1"))
+print(wb.get_sheet_names())
