0239c56ba220c46d008b77c16d26d41a08ab7159
JoseALermaIII <jose@JoseALerma.com>
2018-08-08 04:14:41 -0500

    Update P03_writingExcel fixed warnings by changing depreciated methods

--- books/AutomateTheBoringStuffWithPython/Chapter12/P03_writingExcel.py
+++ books/AutomateTheBoringStuffWithPython/Chapter12/P03_writingExcel.py
@@ -4,13 +4,13 @@
 
 import openpyxl
 wb = openpyxl.Workbook()
-print(wb.get_sheet_names())
-sheet = wb.get_active_sheet()
+print(wb.sheetnames)
+sheet = wb.active
 print(sheet.title)
 sheet.title = "Spam Bacon Eggs Sheet"
-print(wb.get_sheet_names())
+print(wb.sheetnames)
 
 wb = openpyxl.load_workbook("example.xlsx")
-sheet = wb.get_active_sheet()
+sheet = wb.active
 sheet.title = "Spam Spam Spam"
 wb.save("example_copy.xlsx")
