d24b283d972765370a69b4aea255b523115236e4
JoseALermaIII <jose@JoseALerma.com>
2018-08-11 03:38:46 -0500

    Update P05_stylingExcel fixed ValueError by changing Style import to NamedStyle

--- books/AutomateTheBoringStuffWithPython/Chapter12/P05_stylingExcel.py
+++ books/AutomateTheBoringStuffWithPython/Chapter12/P05_stylingExcel.py
@@ -1,13 +1,15 @@
 # This program uses the OpenPyXL module to manipulate Excel documents
 
 import openpyxl
-from openpyxl.styles import Font, Style
+from openpyxl.styles import Font, NamedStyle
 
 wb = openpyxl.Workbook()
 sheet = wb.get_sheet_by_name("Sheet")
 
-italic24Font = Font(size=24, italic=True)
-styleObj = Style(font=italic24Font)
-sheet["A1"].style = styleObj
+italic24Font = NamedStyle(name="italic24Font")
+italic24Font.font = Font(size=24, italic=True)
+
+sheet["A1"].style = italic24Font
 sheet["A1"] = "Hello world!"
+
 wb.save("styled.xlsx")
