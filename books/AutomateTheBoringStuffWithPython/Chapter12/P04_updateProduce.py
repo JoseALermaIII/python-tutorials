950a532b544be36b75589c601c7837e657cdf88e
JoseALermaIII <jose@JoseALerma.com>
2018-08-11 03:13:42 -0500

    Update P04_updateProduce fixed warning by changing depreciated method

--- books/AutomateTheBoringStuffWithPython/Chapter12/P04_updateProduce.py
+++ books/AutomateTheBoringStuffWithPython/Chapter12/P04_updateProduce.py
@@ -8,7 +8,7 @@
 import openpyxl
 
 wb = openpyxl.load_workbook("produceSales.xlsx")
-sheet = wb.get_sheet_by_name("Sheet")
+sheet = wb["Sheet"]
 
 # The produce types and their updated prices
 PRICE_UPDATES = {"Garlic": 3.07,
