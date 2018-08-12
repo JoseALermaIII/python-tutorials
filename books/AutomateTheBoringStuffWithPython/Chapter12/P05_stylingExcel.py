1a41c2f6de8c77af2c57f006b807c77daa1f2c1a
JoseALermaIII <jose@JoseALerma.com>
2018-08-12 02:52:29 -0500

    Update P05_stylingExcel fixed style assignment (typo?)

--- books/AutomateTheBoringStuffWithPython/Chapter12/P05_stylingExcel.py
+++ books/AutomateTheBoringStuffWithPython/Chapter12/P05_stylingExcel.py
@@ -22,13 +22,13 @@ sheet = wb.get_sheet_by_name("Sheet")
 fontObj1 = Font(name="Times New Roman", bold=True)
 styleObj1 = NamedStyle(name="styleObj1")
 styleObj1.font = fontObj1
-sheet["A1"].style/styleObj1
+sheet["A1"].style = styleObj1
 sheet["A1"] = "Bold Times New Roman"
 
 fontObj2 = Font(size=24, italic=True)
 styleObj2 = NamedStyle(name="styleObj2")
 styleObj2.font = fontObj2
-sheet["B3"].style/styleObj2
+sheet["B3"].style = styleObj2
 sheet["B3"] = "24 pt Italic"
 
 wb.save("styles.xlsx")
