75f84dbad2995305cbafd49c1e5dd459e9ff7d40
JoseALermaIII <jose@JoseALerma.com>
2018-08-12 02:51:21 -0500

    Update P05_stylingExcel fixed depreciated Style class

--- books/AutomateTheBoringStuffWithPython/Chapter12/P05_stylingExcel.py
+++ books/AutomateTheBoringStuffWithPython/Chapter12/P05_stylingExcel.py
@@ -20,12 +20,14 @@ wb = openpyxl.Workbook()
 sheet = wb.get_sheet_by_name("Sheet")
 
 fontObj1 = Font(name="Times New Roman", bold=True)
-styleObj1 = NamedStyle(font=fontObj1)
+styleObj1 = NamedStyle(name="styleObj1")
+styleObj1.font = fontObj1
 sheet["A1"].style/styleObj1
 sheet["A1"] = "Bold Times New Roman"
 
 fontObj2 = Font(size=24, italic=True)
-styleObj2 = NamedStyle(font=fontObj2)
+styleObj2 = NamedStyle(name="styleObj2")
+styleObj2.font = fontObj2
 sheet["B3"].style/styleObj2
 sheet["B3"] = "24 pt Italic"
 
