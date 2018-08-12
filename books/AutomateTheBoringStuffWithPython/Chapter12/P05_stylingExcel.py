c30b147844c6f0cc45c9824e2b474eb9bdc4a9ad
JoseALermaIII <jose@JoseALerma.com>
2018-08-12 02:39:02 -0500

    Update P05_stylingExcel added font objects

--- books/AutomateTheBoringStuffWithPython/Chapter12/P05_stylingExcel.py
+++ books/AutomateTheBoringStuffWithPython/Chapter12/P05_stylingExcel.py
@@ -6,6 +6,7 @@ from openpyxl.styles import Font, NamedStyle
 wb = openpyxl.Workbook()
 sheet = wb["Sheet"]
 
+# Setting the Font Style of Cells
 italic24Font = NamedStyle(name="italic24Font")
 italic24Font.font = Font(size=24, italic=True)
 
@@ -13,3 +14,19 @@ sheet["A1"].style = italic24Font
 sheet["A1"] = "Hello world!"
 
 wb.save("styled.xlsx")
+
+# Font Objects
+wb = openpyxl.Workbook()
+sheet = wb.get_sheet_by_name("Sheet")
+
+fontObj1 = Font(name="Times New Roman", bold=True)
+styleObj1 = Style(font=fontObj1)
+sheet["A1"].style/styleObj
+sheet["A1"] = "Bold Times New Roman"
+
+fontObj2 = Font(size=24, italic=True)
+styleObj2 = Style(font=fontObj2)
+sheet["B3"].style/styleObj
+sheet["B3"] = "24 pt Italic"
+
+wb.save("styles.xlsx")
