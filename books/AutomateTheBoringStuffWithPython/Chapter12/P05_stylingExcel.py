fadf171509a3902ae372eaf20f24754bf3dd41d5
JoseALermaIII <jose@JoseALerma.com>
2018-08-13 03:18:11 -0500

    Update P05_stylingExcel reduced line spacing

--- books/AutomateTheBoringStuffWithPython/Chapter12/P05_stylingExcel.py
+++ books/AutomateTheBoringStuffWithPython/Chapter12/P05_stylingExcel.py
@@ -9,10 +9,8 @@ sheet = wb["Sheet"]
 # Setting the Font Style of Cells
 italic24Font = NamedStyle(name="italic24Font")
 italic24Font.font = Font(size=24, italic=True)
-
 sheet["A1"].style = italic24Font
 sheet["A1"] = "Hello world!"
-
 wb.save("styled.xlsx")
 
 # Font Objects
@@ -30,17 +28,14 @@ styleObj2 = NamedStyle(name="styleObj2")
 styleObj2.font = fontObj2
 sheet["B3"].style = styleObj2
 sheet["B3"] = "24 pt Italic"
-
 wb.save("styles.xlsx")
 
 # Formulas
 wb = openpyxl.Workbook()
 sheet = wb.active
-
 sheet["A1"] = 200
 sheet["A2"] = 300
 sheet["A3"] = "=SUM(A1:A2)"
-
 wb.save("writeFormula.xlsx")
 
 wbFormulas = openpyxl.load_workbook("writeFormula.xlsx")
@@ -54,27 +49,22 @@ print(sheet["A3"].value)  # Not working with LibreOffice 5.1.6.2
 # Adjusting Rows and Columns
 wb = openpyxl.Workbook()
 sheet = wb.active
-
 sheet["A1"] = "Tall row"
 sheet["B2"] = "Wide column"
 sheet.row_dimensions[1].height = 70
 sheet.column_dimensions['B'].width = 20
-
 wb.save("dimensions.xlsx")
 
 wb = openpyxl.Workbook()
 sheet = wb.active
-
 sheet.merge_cells("A1:D3")
 sheet["A1"] = "Twelve cells merged together."
 sheet.merge_cells("C5:D5")
 sheet["C5"] = "Two merged cells."
-
 wb.save("merged.xlsx")
 
 wb = openpyxl.load_workbook("merged.xlsx")
 sheet = wb.active
-
 sheet.unmerge_cells("A1:D3")
 sheet.unmerge_cells("C5:D5")
 #wb.save("merged.xlsx")  # uncomment to see changes
