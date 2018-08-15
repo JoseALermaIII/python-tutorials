faeaf10cc441c4dd8a37b62564759543ac1f6a3e
JoseALermaIII <jose@JoseALerma.com>
2018-08-15 03:33:07 -0500

    Update P05_stylingExcel added charts

--- books/AutomateTheBoringStuffWithPython/Chapter12/P05_stylingExcel.py
+++ books/AutomateTheBoringStuffWithPython/Chapter12/P05_stylingExcel.py
@@ -73,3 +73,23 @@ wb = openpyxl.load_workbook("produceSales.xlsx")
 sheet = wb.active
 sheet.freeze_panes = "A2"
 wb.save("freezeExample.xlsx")
+
+# Charts
+wb = openpyxl.Workbook()
+sheet = wb.get_active_sheet()
+for i in range(1, 11):    # create some data in column A
+    sheet['A' + str(i)] = i
+
+refObj = openpyxl.charts.Reference(sheet, (1, 1), (10, 1))
+
+seriesObj = openpyxl.charts.Series(refObj, title="First Series")
+
+chartObj = openpyxl.charts.BarChart()
+chartObj.append(seriesObj)
+chartObj.drawing.top = 50    # set the position
+chartObj.drawing.left = 100
+chartObj.drawing.width = 300 # set the size
+chartObj.drawing.height = 200
+
+sheet.add_chart(chartObj)
+wb.save("sampleChart.xlsx")
