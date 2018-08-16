5f7344b8a99880bec7195b951b495970116f0b0d
JoseALermaIII <jose@JoseALerma.com>
2018-08-16 04:22:14 -0500

    Initialize P2_blankRowInserter

---
+++ books/AutomateTheBoringStuffWithPython/Chapter12/PracticeProjects/P2_blankRowInserter.py
@@ -0,0 +1,4 @@
+# Create a program blankRowInserter.py that takes two integers and a filename
+# string as command line arguments. Let’s call the first integer N and the second
+# integer M. Starting at row N, the program should insert M blank rows into the
+# spreadsheet.
