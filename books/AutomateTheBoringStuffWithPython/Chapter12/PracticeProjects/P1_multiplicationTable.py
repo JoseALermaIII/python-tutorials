d78b6c8d0efa3c4b29f254b7465e5e6fcb889395
JoseALermaIII <jose@JoseALerma.com>
2018-08-16 03:31:32 -0500

    Initialize P1_multiplicationTable

---
+++ books/AutomateTheBoringStuffWithPython/Chapter12/PracticeProjects/P1_multiplicationTable.py
@@ -0,0 +1,3 @@
+# Create a program multiplicationTable.py that takes a number N from the
+# command line and creates an N×N multiplication table in an Excel spreadsheet.
+# Row 1 and column A should be used for labels and should be in bold.
