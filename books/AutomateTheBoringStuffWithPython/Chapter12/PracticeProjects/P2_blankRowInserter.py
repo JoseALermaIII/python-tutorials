ca2b33a748317658d1fdc88a5e316018e72f887b
JoseALermaIII <jose@JoseALerma.com>
2018-08-17 01:29:22 -0500

    Update P2_blankRowInserter working solution

--- books/AutomateTheBoringStuffWithPython/Chapter12/PracticeProjects/P2_blankRowInserter.py
+++ books/AutomateTheBoringStuffWithPython/Chapter12/PracticeProjects/P2_blankRowInserter.py
@@ -7,12 +7,9 @@ import sys
 import openpyxl
 
 # Get arguments from commandline
-#N = int(''.join(sys.argv[0]))
-#M = int(''.join(sys.argv[1]))
-#file = ''.join(sys.argv[2])
-N = 4
-M = 2
-file = "multTable.xlsx"
+N = int(''.join(sys.argv[1]))
+M = int(''.join(sys.argv[2]))
+file = ''.join(sys.argv[3])
 
 # Open workbooks
 readwb = openpyxl.load_workbook(file)
@@ -21,11 +18,14 @@ readsheet = readwb.active
 writewb = openpyxl.Workbook()
 writesheet = writewb.active
 
-# Read readwb and transpose into writewb, TODO: Insert blank lines
-for rowOfCellObjects in readsheet.rows:
-    for cellObj in rowOfCellObjects:
-            writesheet[cellObj.coordinate].value = cellObj.value
-
+# Read readwb and transpose into writewb
+for rowNum in range(1, readsheet.max_row + 1):
+    for colNum in range(1, readsheet.max_column + 1):
+            if rowNum < N:
+                writesheet.cell(row=rowNum, column=colNum).value = readsheet.cell(row=rowNum, column=colNum).value
+            else:
+                # Insert blank lines
+                writesheet.cell(row=(rowNum + M), column=colNum).value = readsheet.cell(row=rowNum, column=colNum).value
 
 # Save workbook
 writewb.save("updated" + file)
