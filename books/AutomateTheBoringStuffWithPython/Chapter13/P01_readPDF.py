0fe4756c10dddcbc2d61c68e484d8f395542b528
JoseALermaIII <jose@JoseALerma.com>
2018-08-19 03:20:07 -0500

    Update P01_readPDF added decrypting PDFs

--- books/AutomateTheBoringStuffWithPython/Chapter13/P01_readPDF.py
+++ books/AutomateTheBoringStuffWithPython/Chapter13/P01_readPDF.py
@@ -4,6 +4,7 @@
 # - Book uses PyPDF2; I'm an overachiever that uses PyPDF4
 
 import PyPDF4
+from PyPDF4.utils import PdfReadError
 
 # Extracting Text from PDFs
 pdfFileObj = open("meetingminutes.pdf", "rb")
@@ -12,3 +13,16 @@ print(pdfReader.numPages)
 
 pageObj = pdfReader.getPage(0)
 print(pageObj.extractText())
+
+# Decrypting PDFs
+pdfReader = PyPDF4.PdfFileReader(open("encrypted.pdf", "rb"))
+print(pdfReader.isEncrypted)
+
+try:
+    print(pdfReader.getPage(0))
+except PdfReadError as err:
+    print("PdfReadError: %s" % err)
+
+pdfReader = PyPDF4.PdfFileReader(open("encrypted.pdf", "rb"))
+print(pdfReader.decrypt("rosebud"))
+pageObj = pdfReader.getPage(0)
