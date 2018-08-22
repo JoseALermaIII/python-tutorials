54a5725416e6a9234e27ed71bc0ff237a9cea7f5
JoseALermaIII <jose@JoseALerma.com>
2018-08-22 02:49:31 -0500

    Update P04_readWord added section titles

--- books/AutomateTheBoringStuffWithPython/Chapter13/P04_readWord.py
+++ books/AutomateTheBoringStuffWithPython/Chapter13/P04_readWord.py
@@ -1,6 +1,8 @@
 # This program uses the python-docx module to manipulate Word documents
 
 import docx
+
+# Reading Word Documents
 doc = docx.Document("demo.docx")
 print(len(doc.paragraphs))
 print(doc.paragraphs[0].text)
@@ -13,5 +15,6 @@ print(doc.paragraphs[1].runs[2].text)
 print(doc.paragraphs[1].runs[3].text)
 print(doc.paragraphs[1].runs[4].text)
 
+# Getting the Full Text from a .docx File
 import P05_readDocx  # Don't do this, imports should be at the top of the file
 print(P05_readDocx.getText("demo.docx"))
