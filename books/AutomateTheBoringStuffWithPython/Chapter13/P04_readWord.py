db8aa8a85069452a01303776e6213eefeaf43b22
JoseALermaIII <jose@JoseALerma.com>
2018-08-22 02:36:30 -0500

    Update P04_readWord moved spacing to separate properties

--- books/AutomateTheBoringStuffWithPython/Chapter13/P04_readWord.py
+++ books/AutomateTheBoringStuffWithPython/Chapter13/P04_readWord.py
@@ -3,9 +3,9 @@
 import docx
 doc = docx.Document("demo.docx")
 print(len(doc.paragraphs))
-
 print(doc.paragraphs[0].text)
 print(doc.paragraphs[1].text)
+
 print(len(doc.paragraphs[1].runs))
 print(doc.paragraphs[1].runs[0].text)
 print(doc.paragraphs[1].runs[1].text)
