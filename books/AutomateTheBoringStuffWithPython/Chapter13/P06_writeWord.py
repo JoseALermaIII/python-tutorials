bc5aefd3e2bc421447976af1103326baa2497b90
JoseALermaIII <jose@JoseALerma.com>
2018-08-22 03:08:20 -0500

    Update P06_writeWord fixed typo

--- books/AutomateTheBoringStuffWithPython/Chapter13/P06_writeWord.py
+++ books/AutomateTheBoringStuffWithPython/Chapter13/P06_writeWord.py
@@ -12,7 +12,7 @@ print(doc.paragraphs[0].text)
 print(doc.paragraphs[0].style)
 doc.paragraphs[0].style = "Normal"
 print(doc.paragraphs[1].text)
-print((doc.paragraphs.[1].runs[0].text, doc.paragraphs[1].runs[1].text,
+print((doc.paragraphs[1].runs[0].text, doc.paragraphs[1].runs[1].text,
        doc.paragraphs[1].runs[2].text, doc.paragraphs[1].runs[3].text))
 doc.paragraphs[1].runs[0].style = "QuoteChar"
 doc.paragraphs[1].runs[1].underline = True
