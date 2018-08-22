39921f94c2b7e6af4a4fd28a5ada1ef7b0462e1c
JoseALermaIII <jose@JoseALerma.com>
2018-08-22 03:02:52 -0500

    Initialize P06_writeWord

---
+++ books/AutomateTheBoringStuffWithPython/Chapter13/P06_writeWord.py
@@ -0,0 +1,20 @@
+# This program uses the python-docx module to manipulate Word documents
+#
+# Note:
+# - Example .docx files can be downloaded from http://nostarch.com/automatestuff/
+
+
+import docx
+
+# Changing Run Attributes
+doc = docx.Document("demo.docx")
+print(doc.paragraphs[0].text)
+print(doc.paragraphs[0].style)
+doc.paragraphs[0].style = "Normal"
+print(doc.paragraphs[1].text)
+print((doc.paragraphs.[1].runs[0].text, doc.paragraphs[1].runs[1].text,
+       doc.paragraphs[1].runs[2].text, doc.paragraphs[1].runs[3].text))
+doc.paragraphs[1].runs[0].style = "QuoteChar"
+doc.paragraphs[1].runs[1].underline = True
+doc.paragraphs[1].runs[3].underline = True
+doc.save("restyled.docx")
