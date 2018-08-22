4dbb12e99eed8816dc8bc3dc368b068642da5afa
JoseALermaIII <jose@JoseALerma.com>
2018-08-22 03:15:25 -0500

    Update P06_writeWord fixed UserWarning

--- books/AutomateTheBoringStuffWithPython/Chapter13/P06_writeWord.py
+++ books/AutomateTheBoringStuffWithPython/Chapter13/P06_writeWord.py
@@ -14,7 +14,7 @@ doc.paragraphs[0].style = "Normal"
 print(doc.paragraphs[1].text)
 print((doc.paragraphs[1].runs[0].text, doc.paragraphs[1].runs[1].text,
        doc.paragraphs[1].runs[2].text, doc.paragraphs[1].runs[3].text))
-doc.paragraphs[1].runs[0].style = "QuoteChar"
+doc.paragraphs[1].runs[0].style = "Quote Char"
 doc.paragraphs[1].runs[1].underline = True
 doc.paragraphs[1].runs[3].underline = True
 doc.save("restyled.docx")
