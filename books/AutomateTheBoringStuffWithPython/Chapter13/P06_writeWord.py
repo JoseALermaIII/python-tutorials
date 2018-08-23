f6d3a7c55c77fb018a9f994afc56103588ed7fa9
JoseALermaIII <jose@JoseALerma.com>
2018-08-23 04:16:25 -0500

    Update P06_writeWord added pictures

--- books/AutomateTheBoringStuffWithPython/Chapter13/P06_writeWord.py
+++ books/AutomateTheBoringStuffWithPython/Chapter13/P06_writeWord.py
@@ -47,3 +47,9 @@ doc.add_paragraph("This is on the first page!")
 doc.paragraphs[0].runs[0].add_break(docx.enum.text.WD_BREAK.PAGE)
 doc.add_paragraph("This is on the second page!")
 doc.save("twoPage.docx")
+
+# Adding Pictures
+doc = docx.Document()
+doc.add_picture("zophie.png", width=docx.shared.Inches(1),
+                height=docx.shared.Cm(4))
+doc.save("picture.docx")
