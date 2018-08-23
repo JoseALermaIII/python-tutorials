58a0dfd7a901e28227908d5954842c38499b1b6e
JoseALermaIII <jose@JoseALerma.com>
2018-08-23 03:31:42 -0500

    Update P06_writeWord added headings

--- books/AutomateTheBoringStuffWithPython/Chapter13/P06_writeWord.py
+++ books/AutomateTheBoringStuffWithPython/Chapter13/P06_writeWord.py
@@ -31,3 +31,13 @@ paraObj2 = doc.add_paragraph("This is yet another paragraph.")
 print(paraObj1.add_run(" This text is being added to the second paragraph."))
 print(doc.add_paragraph("Hello world!", "Title"))
 doc.save("multipleParagraphs.docx")
+
+# Adding Headings
+doc = docx.Document()
+doc.add_heading("Header 0", 0)
+doc.add_heading("Header 1", 1)
+doc.add_heading("Header 2", 2)
+doc.add_heading("Header 3", 3)
+doc.add_heading("Header 4", 4)
+doc.save("headings.docx")
+
