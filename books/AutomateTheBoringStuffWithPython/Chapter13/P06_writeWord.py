d8d6ff660c9441f7943a0c58f8a7a2450b9a64ad
JoseALermaIII <jose@JoseALerma.com>
2018-08-23 03:21:05 -0500

    Update P06_writeWord added writing word documents

--- books/AutomateTheBoringStuffWithPython/Chapter13/P06_writeWord.py
+++ books/AutomateTheBoringStuffWithPython/Chapter13/P06_writeWord.py
@@ -18,3 +18,16 @@ doc.paragraphs[1].runs[0].style = "Quote Char"
 doc.paragraphs[1].runs[1].underline = True
 doc.paragraphs[1].runs[3].underline = True
 doc.save("restyled.docx")
+
+# Writing Word Documents
+doc = docx.Document()
+print(doc.add_paragraph("Hello world!"))
+doc.save("helloworld.docx")
+
+doc = docx.Document()
+print(doc.add_paragraph("Hello world!"))
+paraObj1 = doc.add_paragraph("This is a second paragraph.")
+paraObj2 = doc.add_paragraph("This is yet another paragraph.")
+print(paraObj1.add_run(" This text is being added to the second paragraph."))
+print(doc.add_paragraph("Hello world!", "Title"))
+doc.save("multipleParagraphs.docx")
