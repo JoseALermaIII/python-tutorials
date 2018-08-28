ad50613f54d766d2fb42897681382cfa05efa2bb
JoseALermaIII <jose@JoseALerma.com>
2018-08-28 03:47:18 -0500

    Update P1_encryptPDFparanoia working solution

--- books/AutomateTheBoringStuffWithPython/Chapter13/PracticeProjects/P1_encryptPDFparanoia.py
+++ books/AutomateTheBoringStuffWithPython/Chapter13/PracticeProjects/P1_encryptPDFparanoia.py
@@ -7,8 +7,10 @@
 # file to ensure that it was encrypted correctly.
 
 import PyPDF4, os
+from PyPDF4.utils import PdfReadError
 
 FOLDER = "../"
+SUFFIX = "_encrypted.pdf"
 
 # Get all PDF files in FOLDER
 files = []
@@ -24,11 +26,35 @@ for folderName, subfolders, filenames in os.walk(FOLDER):
             files.append(filepath)
 
 # Get password from user
+password = input("Please input encryption password:\n")
 
 # Encrypt list of PDF files with password
+for file in files:
+    pdfFile = open(file, "rb")
+    pdfReader = PyPDF4.PdfFileReader(pdfFile)
+    pdfWriter = PyPDF4.PdfFileWriter()
+    for pageNum in range(pdfReader.numPages):
+        pdfWriter.addPage(pdfReader.getPage(pageNum))
 
-# Append suffix when saving encrypted file
+    pdfWriter.encrypt(password)
 
-# Attempt to read and decrypt encrypted file
+    # Append SUFFIX when saving encrypted file
+    newfile = file[:-4] + SUFFIX
+    resultPdf = open(newfile, "wb")
+    pdfWriter.write(resultPdf)
+    resultPdf.close()
 
-# Delete original file
+    # Attempt to read and decrypt encrypted file
+    pdfReader = PyPDF4.PdfFileReader(open(newfile, "rb"))
+    pdfReader.decrypt(password)
+    # If the first page cannot be read, print error and move to next file
+    try:
+        pdfReader.getPage(0)
+    except PdfReadError as err:
+        print("PdfReadError: %s" % err)
+        print("Skipping: %s" % file)
+        continue
+
+    # Delete original file
+    print("Deleting: %s" % file)  # DEBUG
+    #os.remove(file)  # uncomment if sure
