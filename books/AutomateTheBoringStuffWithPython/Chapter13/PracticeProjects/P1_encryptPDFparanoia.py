9b94a32a601bfc7daaa20cbe5008ddea29f8041d
JoseALermaIII <jose@JoseALerma.com>
2018-08-28 02:46:21 -0500

    Initialize P1_encryptPDFparanoia

---
+++ books/AutomateTheBoringStuffWithPython/Chapter13/PracticeProjects/P1_encryptPDFparanoia.py
@@ -0,0 +1,7 @@
+# Using the os.walk() function, write a script that will go through every PDF
+# in a folder (and its subfolders) and encrypt the PDFs using a password provided
+# on the command line. Save each encrypted PDF with an _encrypted.pdf suffix added
+# to the original filename.
+#
+# Before deleting the original file, have the program attempt to read and decrypt the
+# file to ensure that it was encrypted correctly.
