dd7d2b2ded238aa42f89381b724d0356f63426c1
JoseALermaIII <jose@JoseALerma.com>
2018-08-29 04:20:33 -0500

    Update P2_decryptPDFparanoia added get all encrypted PDFs in folder

--- books/AutomateTheBoringStuffWithPython/Chapter13/PracticeProjects/P2_decryptPDFparanoia.py
+++ books/AutomateTheBoringStuffWithPython/Chapter13/PracticeProjects/P2_decryptPDFparanoia.py
@@ -3,7 +3,22 @@
 # password is incorrect, the program should print a message to the user and
 # continue to the next PDF.
 
-# Get all encrypted PDFs in a folder
+import PyPDF4, os
+
+FOLDER = "../"
+
+# Get all encrypted PDFs in FOLDER
+files = []
+for folderName, subfolders, filenames in os.walk(FOLDER):
+    for filename in filenames:
+        # If file is in subdirectory, append backslash to folderName
+        if folderName.endswith('/'):
+            filepath = folderName + filename
+        else:
+            filepath = folderName + '/' + filename
+
+        if filename.lower().endswith(".pdf") and PyPDF4.PdfFileReader(open(filepath, "rb")).isEncrypted:
+            files.append(filepath)
 
 # Get password from user
 
