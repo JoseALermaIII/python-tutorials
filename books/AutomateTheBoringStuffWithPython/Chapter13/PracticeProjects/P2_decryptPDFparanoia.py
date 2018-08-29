9cd0232e4b5bf58c0476059d0ff32328f73fa482
JoseALermaIII <jose@JoseALerma.com>
2018-08-29 04:15:49 -0500

    Update P2_decryptPDFparanoia added outline

--- books/AutomateTheBoringStuffWithPython/Chapter13/PracticeProjects/P2_decryptPDFparanoia.py
+++ books/AutomateTheBoringStuffWithPython/Chapter13/PracticeProjects/P2_decryptPDFparanoia.py
@@ -1,4 +1,14 @@
-# write a program that finds all encrypted PDFs in a folder (and its subfolders)
+# Write a program that finds all encrypted PDFs in a folder (and its subfolders)
 # and creates a decrypted copy of the PDF using a provided password. If the
 # password is incorrect, the program should print a message to the user and
 # continue to the next PDF.
+
+# Get all encrypted PDFs in a folder
+
+# Get password from user
+
+# Decrypt all PDFs with password
+
+# If incorrect password, print message and continue
+
+# Rename decrypted PDF by removing suffix
