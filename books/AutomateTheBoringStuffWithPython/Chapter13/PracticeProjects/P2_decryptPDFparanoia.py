af5b4b64184487566956b12c6d429af41de74221
JoseALermaIII <jose@JoseALerma.com>
2018-08-29 04:12:56 -0500

    Initialize P2_decryptPDFparanoia

---
+++ books/AutomateTheBoringStuffWithPython/Chapter13/PracticeProjects/P2_decryptPDFparanoia.py
@@ -0,0 +1,4 @@
+# write a program that finds all encrypted PDFs in a folder (and its subfolders)
+# and creates a decrypted copy of the PDF using a provided password. If the
+# password is incorrect, the program should print a message to the user and
+# continue to the next PDF.
