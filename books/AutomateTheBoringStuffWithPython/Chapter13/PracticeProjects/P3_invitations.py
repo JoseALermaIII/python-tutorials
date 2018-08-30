579ec8b5a06c03aecaba9538cb9f06fffeb000d5
JoseALermaIII <jose@JoseALerma.com>
2018-08-30 02:39:04 -0500

    Initialize P3_invitations

---
+++ books/AutomateTheBoringStuffWithPython/Chapter13/PracticeProjects/P3_invitations.py
@@ -0,0 +1,8 @@
+# Say you have a text file of guest names. This guests.txt file has one name
+# per line, as follows:
+# Prof. Plum
+# Miss Scarlet
+# Col. Mustard
+# Al Sweigart
+# RoboCop
+# Write a program that would generate a Word document with custom invitations
