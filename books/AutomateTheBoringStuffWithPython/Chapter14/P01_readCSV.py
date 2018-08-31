8945a93d65aa0b534bbdc5ffdc738cbbbee31546
JoseALermaIII <jose@JoseALerma.com>
2018-08-31 05:32:55 -0500

    Update P01_readCSV added reading data from reader objects in a for loop

--- books/AutomateTheBoringStuffWithPython/Chapter14/P01_readCSV.py
+++ books/AutomateTheBoringStuffWithPython/Chapter14/P01_readCSV.py
@@ -13,3 +13,9 @@ print(exampleData[0][1])
 print(exampleData[0][2])
 print(exampleData[1][1])
 print(exampleData[6][1])
+
+# Reading Data from Reader Objects in a for Loop
+exampleFile = open("example.csv")
+exampleReader = csv.reader(exampleFile)
+for row in exampleReader:
+    print("Row #" + str(exampleReader.line_num) + ' ' + str(row))
