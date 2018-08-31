7af955f1fd49b6242cfc24435ccc07dc63f1760b
JoseALermaIII <jose@JoseALerma.com>
2018-08-31 05:45:39 -0500

    Update P02_writeCSV added delimiter and lineterminator keyword arguments

--- books/AutomateTheBoringStuffWithPython/Chapter14/P02_writeCSV.py
+++ books/AutomateTheBoringStuffWithPython/Chapter14/P02_writeCSV.py
@@ -9,3 +9,11 @@ print(outputWriter.writerow(['spam', 'eggs', 'bacon', 'ham']))
 print(outputWriter.writerow(['Hello, world!', 'eggs', 'bacon', 'ham']))
 print(outputWriter.writerow([1, 2, 3.141592, 4]))
 outputFile.close()
+
+# Delimiter and lineterminator Keyword Arguments
+csvFile = open("example.tsv", 'w', newline='')
+csvWriter = csv.writer(csvFile, delimiter='\t', lineterminator='\n\n')
+print(csvWriter.writerow(['apples', 'oranges', 'grapes']))
+print(csvWriter.writerow(['eggs', 'bacon', 'ham']))
+print(csvWriter.writerow(['spam', 'spam', 'spam', 'spam', 'spam', 'spam']))
+csvFile.close()
