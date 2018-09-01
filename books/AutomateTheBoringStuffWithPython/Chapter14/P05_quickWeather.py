ecf37a7f1280deb986e443cb7e5b6af509affbd5
JoseALermaIII <jose@JoseALerma.com>
2018-09-01 03:08:08 -0500

    Update P05_quickWeather remove extra parenthesis

--- books/AutomateTheBoringStuffWithPython/Chapter14/P05_quickWeather.py
+++ books/AutomateTheBoringStuffWithPython/Chapter14/P05_quickWeather.py
@@ -10,7 +10,7 @@ if len(sys.argv) < 2:
 location = ' '.join(sys.argv[1:])
 
 # Download the JSON data from OpenWeatherMap.org's API.
-url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3' % (location)
+url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3' % location
 response = requests.get(url)
 response.raise_for_status()
 
@@ -18,7 +18,7 @@ response.raise_for_status()
 weatherData = json.loads(response.text)
 # Print weather descriptions
 w = weatherData['list']
-print('Current weather in %s:' %(location))
+print('Current weather in %s:' % location)
 print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
 print()
 print('Tomorrow:')
