3364bbb7fd256a32dc51927504a696b64542a870
JoseALermaIII <jose@JoseALerma.com>
2018-09-04 02:20:08 -0500

    Update excelToCSV working solution

--- books/AutomateTheBoringStuffWithPython/Chapter14/PracticeProject/excelToCSV.py
+++ books/AutomateTheBoringStuffWithPython/Chapter14/PracticeProject/excelToCSV.py
@@ -10,24 +10,40 @@
 # Note:
 # - Example Excel files can be downloaded from http://nostarch.com/automatestuff/
 
-import openpyxl, os
+import openpyxl, os, csv
 
-for excelFile in os.listdir('.'):
+FOLDER_IN = "./excelSpreadsheets"
+FOLDER_OUT = "./csvFiles"
+
+for excelFile in os.listdir(FOLDER_IN):
     # Skip non-xlsx files, load the workbook object.
+    if excelFile.endswith(".xlsx"):
+        if not FOLDER_IN.endswith('/'):
+            excelFilepath = FOLDER_IN + '/' + excelFile
+        else:
+            excelFilepath = FOLDER_IN + excelFile
+        wb = openpyxl.load_workbook(excelFilepath)
+    else:
+        continue
     for sheetName in wb.sheetnames:
         # Loop through every sheet in the workbook.
-        sheet = wb['sheetName']
+        sheet = wb[sheetName]
 
         # Create the CSV filename from the Excel filename and sheet title.
+        if not FOLDER_OUT.endswith('/'):
+            csvFilepath = FOLDER_OUT + '/' + excelFile[:-5] + '_' + sheet.title + ".csv"
+        else:
+            csvFilepath = FOLDER_OUT + excelFile[:-5] + '_' + sheet.title + ".csv"
         # Create the csv.writer object for this CSV file.
-
+        csvFile = open(csvFilepath, "w", newline='')
+        csvWriter = csv.writer(csvFile)
         # Loop through every row in the sheet.
         for rowNum in range(1, sheet.max_row + 1):
             rowData = []    # append each cell to this list
             # Loop through each cell in the row.
             for colNum in range(1, sheet.max_column + 1):
                 # Append each cell's data to rowData.
-
+                rowData.append(sheet.cell(row=rowNum, column=colNum).value)
             # Write the rowData list to the CSV file.
-
+            csvWriter.writerow(rowData)
         csvFile.close()
