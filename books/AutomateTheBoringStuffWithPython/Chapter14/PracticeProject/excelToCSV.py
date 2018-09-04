d15bc25fb1cb83184b609d5c8d412ffc7abea007
JoseALermaIII <jose@JoseALerma.com>
2018-09-04 01:13:59 -0500

    Update excelToCSV added imports and changed depreciated methods

--- books/AutomateTheBoringStuffWithPython/Chapter14/PracticeProject/excelToCSV.py
+++ books/AutomateTheBoringStuffWithPython/Chapter14/PracticeProject/excelToCSV.py
@@ -10,20 +10,22 @@
 # Note:
 # - Example Excel files can be downloaded from http://nostarch.com/automatestuff/
 
+import openpyxl, os
+
 for excelFile in os.listdir('.'):
     # Skip non-xlsx files, load the workbook object.
-    for sheetName in wb.get_sheet_names():
+    for sheetName in wb.sheetnames:
         # Loop through every sheet in the workbook.
-        sheet = wb.get_sheet_by_name(sheetName)
+        sheet = wb['sheetName']
 
         # Create the CSV filename from the Excel filename and sheet title.
         # Create the csv.writer object for this CSV file.
 
         # Loop through every row in the sheet.
-        for rowNum in range(1, sheet.get_highest_row() + 1):
+        for rowNum in range(1, sheet.max_row + 1):
             rowData = []    # append each cell to this list
             # Loop through each cell in the row.
-            for colNum in range(1, sheet.get_highest_column() + 1):
+            for colNum in range(1, sheet.max_column + 1):
                 # Append each cell's data to rowData.
 
             # Write the rowData list to the CSV file.
