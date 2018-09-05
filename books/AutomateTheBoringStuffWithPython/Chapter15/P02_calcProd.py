408a33f78430f5f6c527c50bce1e8796a2aaa5a0
JoseALermaIII <jose@JoseALerma.com>
2018-09-05 02:28:39 -0500

    Update P02_calcProd added cProfile example

--- books/AutomateTheBoringStuffWithPython/Chapter15/P02_calcProd.py
+++ books/AutomateTheBoringStuffWithPython/Chapter15/P02_calcProd.py
@@ -1,8 +1,12 @@
 #! python3
 #  P02_calcProd.py - Uses the time module to profile a function that calculates
 #  the product of the first 100,000 numbers.
+#
+#  Note:
+#  - added cProfile for an execution profile. Does add overhead, so not suitable
+#  for benchmarking.
 
-import time
+import time, cProfile
 
 
 def calcProd():
@@ -17,3 +21,7 @@ prod = calcProd()
 endTime = time.time()
 print('The result is %s digits long.' % (len(str(prod))))
 print('Took %s seconds to calculate.' % (endTime - startTime))
+
+time.sleep(1)
+
+cProfile.run('calcProd()')
