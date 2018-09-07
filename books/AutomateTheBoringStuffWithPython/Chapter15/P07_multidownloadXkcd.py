9535979d2a892c146fc7cea031c44f209f20b4e8
JoseALermaIII <jose@JoseALerma.com>
2018-09-07 03:11:23 -0500

    Update P07_multidownloadXkcd simplified comparison with empty list

--- books/AutomateTheBoringStuffWithPython/Chapter15/P07_multidownloadXkcd.py
+++ books/AutomateTheBoringStuffWithPython/Chapter15/P07_multidownloadXkcd.py
@@ -16,7 +16,7 @@ def downloadXkcd(startComic, endComic):
 
         # Find the URL of the comic image.
         comicElem = soup.select('#comic img')
-        if comicElem == []:
+        if not comicElem:
             print('Could not find comic image.')
         else:
             comicUrl = 'http:' + comicElem[0].get('src')
