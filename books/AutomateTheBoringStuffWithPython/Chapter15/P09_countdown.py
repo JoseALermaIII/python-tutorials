27a1632a768028477f54450ccf7aaa7fba8bc428
JoseALermaIII <jose@JoseALerma.com>
2018-09-08 11:16:35 -0500

    Initialize P09_countdown

---
+++ books/AutomateTheBoringStuffWithPython/Chapter15/P09_countdown.py
@@ -0,0 +1,16 @@
+#! python3
+# P09_countdown.py - A simple countdown script.
+#
+# Note:
+# - sound file can be downloaded from http://nostarch.com/automatestuff/
+
+import time, subprocess
+
+timeLeft = 60
+while timeLeft > 0:
+    print(timeLeft, end='')
+    time.sleep(1)
+    timeLeft = timeLeft - 1
+
+# At the end of the countdown, play a sound file.
+subprocess.Popen(['see', 'alarm.wav'])
