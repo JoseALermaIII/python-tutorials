7595e71d6e4991bb7c9034446b77579a0701cd73
JoseALermaIII <jose@JoseALerma.com>
2018-09-08 12:04:26 -0500

    Update P09_countdown get timeLeft from command line arguments

--- books/AutomateTheBoringStuffWithPython/Chapter15/P09_countdown.py
+++ books/AutomateTheBoringStuffWithPython/Chapter15/P09_countdown.py
@@ -4,9 +4,14 @@
 # Note:
 # - sound file can be downloaded from http://nostarch.com/automatestuff/
 
-import time, subprocess
+import time, subprocess, sys
 
-timeLeft = 60
+# Get timeLeft from command line arguments
+if len(sys.argv) < 2:
+    print('Usage: P09_countdown.py seconds')
+    sys.exit()
+timeLeft = int(sys.argv[1])
+
 while timeLeft > 0:
     print(timeLeft)
     time.sleep(1)
