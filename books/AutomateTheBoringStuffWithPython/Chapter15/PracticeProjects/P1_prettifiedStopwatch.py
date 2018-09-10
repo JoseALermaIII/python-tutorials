8a39bd5cea4b318f6cb1aa37d30f08e8a8970884
JoseALermaIII <jose@JoseALerma.com>
2018-09-10 17:41:29 -0500

    Initialize P1_prettifiedStopwatch

---
+++ books/AutomateTheBoringStuffWithPython/Chapter15/PracticeProjects/P1_prettifiedStopwatch.py
@@ -0,0 +1,16 @@
+# Expand the stopwatch project from this chapter so that it uses the rjust() and
+# ljust() string methods to “prettify” the output.
+#
+# Instead of output such as this:
+# Lap #1: 3.56 (3.56)
+# Lap #2: 8.63 (5.07)
+# Lap #3: 17.68 (9.05)
+# Lap #4: 19.11 (1.43)
+# ... the output will look like this:
+# Lap # 1:  3.56 (  3.56)
+# Lap # 2:  8.63 (  5.07)
+# Lap # 3: 17.68 (  9.05)
+# Lap # 4: 19.11 (  1.43)
+#
+# Next, use the pyperclip module introduced to copy the text output to the clipboard
+# so the user can quickly paste the output to a text file or email.
