24e52d5fa79ff1eafd7f8a8cd00ed513354259a8
JoseALermaIII <jose@JoseALerma.com>
2018-09-10 18:31:43 -0500

    Update P06_bulletPointAdder changed pyperclip import to local copy and for readability

--- books/AutomateTheBoringStuffWithPython/Chapter06/P06_bulletPointAdder.py
+++ books/AutomateTheBoringStuffWithPython/Chapter06/P06_bulletPointAdder.py
@@ -2,12 +2,13 @@
 # P06_bulletPointAdder.py - Adds Wikipedia bullet points to the start
 # of each line of text on the clipboard
 
-import books.CrackingCodesWithPython.pyperclip
-text = books.CrackingCodesWithPython.pyperclip.paste
+import books.AutomateTheBoringStuffWithPython.Chapter08.pyperclip as pyperclip
+
+text = pyperclip.paste
 
 # Separate lines and add stars.
 lines = text.split('\n')
 for i in range(len(lines)):  # loop through all the indexes in the "lines" list
     lines[i] = '* ' + lines[i]  # add star to each string in "lines" list
 text = '\n'.join(lines)
-books.CrackingCodesWithPython.pyperclip.copy(text)
+pyperclip.copy(text)
