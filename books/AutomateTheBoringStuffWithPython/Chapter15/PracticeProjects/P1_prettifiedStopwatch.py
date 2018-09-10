5ce3f944910b05a78aa570fdd615d9bc85750661
JoseALermaIII <jose@JoseALerma.com>
2018-09-10 17:42:37 -0500

    Update P1_prettifiedStopwatch added original stopwatch program

--- books/AutomateTheBoringStuffWithPython/Chapter15/PracticeProjects/P1_prettifiedStopwatch.py
+++ books/AutomateTheBoringStuffWithPython/Chapter15/PracticeProjects/P1_prettifiedStopwatch.py
@@ -14,3 +14,27 @@
 #
 # Next, use the pyperclip module introduced to copy the text output to the clipboard
 # so the user can quickly paste the output to a text file or email.
+
+import time
+
+# Display the program's instructions
+print("Press ENTER to begin. Afterwards, press ENTER to 'click' the stopwatch. "
+      "Press CTRL-C to quit.")
+input()  # Press Enter to begin
+print('Started.')
+startTime = time.time()  # Get the first lap's start time
+lastTime = startTime
+lapNum = 1
+
+# Start tracking the lap times.
+try:
+    while True:
+        input()
+        lapTime = round(time.time() - lastTime, 2)
+        totalTime = round(time.time() - startTime, 2)
+        print("Lap #%s: %s (%s)" % (lapNum, totalTime, lapTime), end='')
+        lapNum += 1
+        lastTime = time.time()  # reset the last lap time
+except KeyboardInterrupt:
+    # Handle the CTRL-C exception to keep its error message from displaying.
+    print("\nDone.")
