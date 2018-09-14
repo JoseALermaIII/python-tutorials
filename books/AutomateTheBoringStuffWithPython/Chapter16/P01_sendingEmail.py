f284a838cf61cd4c4e35a52e5e13e01abbca2afc
JoseALermaIII <jose@JoseALerma.com>
2018-09-14 03:20:27 -0500

    Update P01_sendingEmail added notes and logging into server

--- books/AutomateTheBoringStuffWithPython/Chapter16/P01_sendingEmail.py
+++ books/AutomateTheBoringStuffWithPython/Chapter16/P01_sendingEmail.py
@@ -1,4 +1,9 @@
 # This program uses the smtplib module to send emails
+#
+# Note:
+# - smtp_info file has each item on separate line
+# - email address used is specially created for this chapter
+# - use input() for password to prevent storing in unencrypted file
 
 # Connecting to an SMTP Server
 import smtplib
@@ -12,3 +17,6 @@ print(type(smtp_obj))
 
 # Sending the SMTP "Hello" Message
 print(smtp_obj.ehlo())
+
+# Logging in to the SMTP Server
+print(smtp_obj.login(smtp_cfg[0], smtp_cfg[1]))
