9c0d1d2ccc0f67e2ca9fe886459bc07ee67d4711
JoseALermaIII <jose@JoseALerma.com>
2018-09-17 05:37:27 -0500

    Update P01_sendingEmail multiple assignment for credentials

--- books/AutomateTheBoringStuffWithPython/Chapter16/P01_sendingEmail.py
+++ books/AutomateTheBoringStuffWithPython/Chapter16/P01_sendingEmail.py
@@ -9,20 +9,19 @@
 import smtplib
 
 with open('smtp_info') as config:
-    # smtp_cfg = [email, password, smtp server, port]
-    smtp_cfg = config.read().splitlines()
+    email, password, server, port = config.read().splitlines()
 
-smtp_obj = smtplib.SMTP_SSL(smtp_cfg[2], smtp_cfg[3])  # Using port 465
+smtp_obj = smtplib.SMTP_SSL(server, port)  # Using port 465
 print(type(smtp_obj))
 
 # Sending the SMTP "Hello" Message
 print(smtp_obj.ehlo())
 
 # Logging in to the SMTP Server
-print(smtp_obj.login(smtp_cfg[0], smtp_cfg[1]))
+print(smtp_obj.login(email, password))
 
 # Sending an Email
-unsent = smtp_obj.sendmail(smtp_cfg[0], 'contact.me@JoseALerma.com',
+unsent = smtp_obj.sendmail(email, 'contact.me@JoseALerma.com',
                            'Subject: Bot test.\nDear Myself,\nAlways remember that with great '
                            'power comes great responsibility.\nRegards,\nYou')
 print(unsent.keys())
