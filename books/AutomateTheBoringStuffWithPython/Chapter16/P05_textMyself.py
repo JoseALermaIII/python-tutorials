512c20fafa26523f919574624f0dfca90ec673c6
JoseALermaIII <jose@JoseALerma.com>
2018-09-18 14:05:25 -0500

    Update P05_textMyself fixed some PEP8 warnings

--- books/AutomateTheBoringStuffWithPython/Chapter16/P05_textMyself.py
+++ books/AutomateTheBoringStuffWithPython/Chapter16/P05_textMyself.py
@@ -2,14 +2,16 @@
 # P05_textMyself.py - Defines the textmyself() function that texts a message
 # passed to it as a string.
 
+from twilio.rest import TwilioRestClient
+
 # Preset values:
 accountSID = ''
 authToken = ''
 myNumber = ''
 twilioNumber = ''
 
-from twilio.rest import TwilioRestClient
 
 def textmyself(message):
     twilioCli = TwilioRestClient(accountSID, authToken)
     twilioCli.messages.create(body=message, from_=twilioNumber, to=myNumber)
+    return None
