f083789e5615d15715f49a7dbdb25505aa5efae2
JoseALermaIII <jose@JoseALerma.com>
2018-09-19 04:29:11 -0500

    Initialize P1_assignChores

---
+++ books/AutomateTheBoringStuffWithPython/Chapter16/PracticeProjects/P1_assignChores.py
@@ -0,0 +1,9 @@
+# Write a program that takes a list of people’s email addresses and a list of chores
+# that need to be done and randomly assigns chores to people. Email each person their
+# assigned chores.
+#
+# If you’re feeling ambitious, keep a record of each person’s previously assigned
+# chores so that you can make sure the program avoids assigning anyone the same chore
+# they did last time.
+#
+# For another possible feature, schedule the program to run once a week automatically.
