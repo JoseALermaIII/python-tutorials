4c64dae9049ca8228bcea3c7dba1846f6d9b2c5c
JoseALermaIII <jose@JoseALerma.com>
2018-09-20 00:04:31 -0500

    Initialize P2_rememberUmbrella

---
+++ books/AutomateTheBoringStuffWithPython/Chapter16/PracticeProjects/P2_rememberUmbrella.py
@@ -0,0 +1,4 @@
+# Use the requests module to scrape data from http://weather.gov/.
+# Write a program that runs just before you wake up in the morning and checks
+# whether it’s raining that day. If so, have the program text you a reminder to pack
+# an umbrella before leaving the house.
