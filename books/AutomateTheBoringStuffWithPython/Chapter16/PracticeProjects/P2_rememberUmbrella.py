7153e5f1e91dc4cebffca94649535b34490bd103
JoseALermaIII <jose@JoseALerma.com>
2018-09-20 00:09:03 -0500

    Update P2_rememberUmbrella added outline

--- books/AutomateTheBoringStuffWithPython/Chapter16/PracticeProjects/P2_rememberUmbrella.py
+++ books/AutomateTheBoringStuffWithPython/Chapter16/PracticeProjects/P2_rememberUmbrella.py
@@ -2,3 +2,11 @@
 # Write a program that runs just before you wake up in the morning and checks
 # whether it’s raining that day. If so, have the program text you a reminder to pack
 # an umbrella before leaving the house.
+
+import requests, bs4, twilio
+
+# Download weather url and soupify
+
+# Parse current weather from soup
+
+# If raining, text cellphone
