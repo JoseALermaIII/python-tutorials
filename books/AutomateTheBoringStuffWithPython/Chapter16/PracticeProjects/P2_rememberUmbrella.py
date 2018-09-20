52162fe60544a4fc63d1404218afe4a46f29cbd6
JoseALermaIII <jose@JoseALerma.com>
2018-09-20 01:32:12 -0500

    Update P2_rememberUmbrella added sleep until wake time

--- books/AutomateTheBoringStuffWithPython/Chapter16/PracticeProjects/P2_rememberUmbrella.py
+++ books/AutomateTheBoringStuffWithPython/Chapter16/PracticeProjects/P2_rememberUmbrella.py
@@ -3,33 +3,47 @@
 # whether it’s raining that day. If so, have the program text you a reminder to pack
 # an umbrella before leaving the house.
 
-import requests, bs4, datetime
+import requests, bs4, datetime, time
 from books.AutomateTheBoringStuffWithPython.Chapter16.P05_textMyself import textmyself
 
-# Check for wake time
-time_now = datetime.datetime.now().time()
-wake_time = datetime.time(hour=5)
 
-if time_now < wake_time:
-    print(f'RuntimeError: can\'t run until {wake_time}')
-    raise RuntimeError
+def check_time():
+    # Check for wake time
+    time_now = datetime.datetime.now().time()
+    wake_time = datetime.time(hour=5)
 
-# Download weather url and soupify
-url = 'https://forecast.weather.gov/MapClick.php?lat=30.26759000000004&lon=-97.74298999999996'
+    if time_now < wake_time:
+        print(f'RuntimeError: can\'t run until {wake_time}')
+        return False
+    return True
 
-res = requests.get(url)
-res.raise_for_status()
 
-soup = bs4.BeautifulSoup(res.text, 'lxml')
+def remember_umbrella():
+    # Download weather url and soupify
+    url = 'https://forecast.weather.gov/MapClick.php?lat=30.26759000000004&lon=-97.74298999999996'
 
-# Parse current weather from soup
-weather_element = soup.select('.myforecast-current')
-weather = weather_element[0].getText()
+    res = requests.get(url)
+    res.raise_for_status()
 
-# If raining, text cellphone
-tokens = ['rain', 't-storms']
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
 
-for token in tokens:
-    if token in weather.lower():
-        message = f'Bring an umbrella, there\'s {weather}'
-        textmyself(message)
+    # Parse current weather from soup
+    weather_element = soup.select('.myforecast-current')
+    weather = weather_element[0].getText()
+
+    # If raining, text cellphone
+    tokens = ['rain', 't-storms']
+
+    for token in tokens:
+        if token in weather.lower():
+            message = f'Bring an umbrella, there\'s {weather}'
+            textmyself(message)
+    return None
+
+
+# If run directly (instead of imported) wait for wake time, then run
+if __name__ == '__main__':
+    sleep_time = datetime.timedelta(minutes=5)
+    while not check_time():
+        time.sleep(sleep_time.total_seconds())
+    remember_umbrella()
