a08426e7f9c6e1b0d4f01861584580ebfcbda38a
JoseALermaIII <jose@JoseALerma.com>
2018-09-20 00:12:29 -0500

    Update P2_rememberUmbrella added download and soupify

--- books/AutomateTheBoringStuffWithPython/Chapter16/PracticeProjects/P2_rememberUmbrella.py
+++ books/AutomateTheBoringStuffWithPython/Chapter16/PracticeProjects/P2_rememberUmbrella.py
@@ -6,6 +6,12 @@
 import requests, bs4, twilio
 
 # Download weather url and soupify
+url = 'https://forecast.weather.gov/MapClick.php?lat=30.26759000000004&lon=-97.74298999999996'
+
+res = requests.get(url)
+res.raise_for_status()
+
+soup = bs4.BeautifulSoup(res.text, 'lxml')
 
 # Parse current weather from soup
 
