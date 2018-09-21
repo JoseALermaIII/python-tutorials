1b3cac5eb02384b60329c7018f8ca144a4b19306
JoseALermaIII <jose@JoseALerma.com>
2018-09-21 02:46:47 -0500

    Update P2_rememberUmbrella removed input from remember_umbrella

--- books/AutomateTheBoringStuffWithPython/Chapter16/PracticeProjects/P2_rememberUmbrella.py
+++ books/AutomateTheBoringStuffWithPython/Chapter16/PracticeProjects/P2_rememberUmbrella.py
@@ -14,9 +14,10 @@ def get_soup(url_arg):
     return bs4.BeautifulSoup(res.text, 'lxml')
 
 
-def remember_umbrella(url_arg):
+def remember_umbrella():
     # Download weather url and soupify
-    soup = get_soup(url_arg)
+    url = 'https://forecast.weather.gov/MapClick.php?lat=30.26759000000004&lon=-97.74298999999996'
+    soup = get_soup(url)
 
     # Parse current weather from soup
     weather_element = soup.select('.myforecast-current')
@@ -46,14 +47,13 @@ def main():
     # Wait for wake_time, then run remember_umbrella()
     import time
 
-    url = 'https://forecast.weather.gov/MapClick.php?lat=30.26759000000004&lon=-97.74298999999996'
     sleep_time = datetime.timedelta(minutes=5)
     wake_time = datetime.time(hour=5)
 
     while not check_time(wake_time):
         time.sleep(sleep_time.total_seconds())
 
-    remember_umbrella(url)
+    remember_umbrella()
     return None
 
 
