cba9c73150fed959e383017107c83fefa8df6088
JoseALermaIII <jose@JoseALerma.com>
2018-09-22 03:50:17 -0500

    Initialize P3_autoUnsubscribe

---
+++ books/AutomateTheBoringStuffWithPython/Chapter16/PracticeProjects/P3_autoUnsubscribe.py
@@ -0,0 +1,8 @@
+# Write a program that scans through your email account, finds all the unsubscribe
+# links in all your emails, and automatically opens them in a browser. This program
+# will have to log in to your email provider’s IMAP server and download all of your
+# emails. You can use BeautifulSoup to check for any instance where the word
+# unsubscribe occurs within an HTML link tag.
+#
+# Once you have a list of these URLs, you can use webbrowser.open() to automatically
+# open all of these links in a browser.
