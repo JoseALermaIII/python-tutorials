8e8b413bdddd41c69656bbad804c3bcb9540c543
JoseALermaIII <jose@JoseALerma.com>
2018-09-22 04:59:32 -0500

    Update P3_autoUnsubscribe working solution

--- books/AutomateTheBoringStuffWithPython/Chapter16/PracticeProjects/P3_autoUnsubscribe.py
+++ books/AutomateTheBoringStuffWithPython/Chapter16/PracticeProjects/P3_autoUnsubscribe.py
@@ -6,16 +6,49 @@
 #
 # Once you have a list of these URLs, you can use webbrowser.open() to automatically
 # open all of these links in a browser.
+#
+# Note:
+# - imap_info file has each item on separate line
+# - email address used is specially created for this chapter
+# - use input() for password to prevent storing in unencrypted file
 
-import imapclient, bs4, webbrowser
+import imapclient, pyzmail, bs4, webbrowser, imaplib
 
+# Increase size limit from 10,000 bytes to 10,000,000 bytes
+imaplib._MAXLINE = 10000000
 
 # Login to IMAP server
+with open('../imap_info') as config:
+    email, password, server, port = config.read().splitlines()
+
+imap_obj = imapclient.IMAPClient(server, ssl=True)
+
+imap_obj.login(email, password)
 
 # Get message and soupify
+imap_obj.select_folder('INBOX', readonly=True)  # Don't mark as read or delete
+uids = imap_obj.search(['SINCE', '21-Sep-2018'])
+raw_messages = imap_obj.fetch(uids, ['BODY[]'])
 
-# Check soup for unsubscribe links
+for uid in uids:
+    message = pyzmail.PyzMessage.factory(raw_messages[uid][b'BODY[]'])
+    if message.html_part is not None:
+        print(f'Accessing "{message.get_subject()}" from: {message.get_address("from")}...')
+        html = message.html_part.get_payload().decode(message.html_part.charset)
+        soup = bs4.BeautifulSoup(html, 'lxml')
+    else:
+        print('RuntimeError: Email is not HTML.')
+        print(f'Skipping "{message.get_subject()}" from: {message.get_address("from")}')
+        break
 
-# Open link in browser
+    # Check soup for unsubscribe links
+    anchors = soup.select('a')
+    for anchor in anchors:
+        if anchor.getText().lower() == 'unsubscribe':
+            # Open link in browser
+            url = anchor.get('href')
+            print(f'Opening: {url}')
+            webbrowser.open(url)
 
-# Log off
+# Disconnect from IMAP server
+imap_obj.logout()
