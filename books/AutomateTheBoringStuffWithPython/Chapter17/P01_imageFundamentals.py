2f48bfd81b4392d4660a9f1e55b01db0f00164fe
JoseALermaIII <jose@JoseALerma.com>
2018-09-26 02:16:52 -0500

    Initialize P01_imageFundamentals

---
+++ books/AutomateTheBoringStuffWithPython/Chapter17/P01_imageFundamentals.py
@@ -0,0 +1,9 @@
+# This program uses the pillow module to manipulate digital images
+
+# Colors and RGBA Values
+from PIL import ImageColor
+print(ImageColor.getcolor('red', 'RGBA'))
+print(ImageColor.getcolor('RED', 'RGBA'))
+print(ImageColor.getcolor('Black', 'RGBA'))
+print(ImageColor.getcolor('chocolate', 'RGBA'))
+print(ImageColor.getcolor('CornflowerBlue', 'RGBA'))
