8f850a6ced99829471c1484cebe19fc369a6e040
JoseALermaIII <jose@JoseALerma.com>
2018-09-26 03:16:40 -0500

    Update P02_manipulatingImages added changing pixels

--- books/AutomateTheBoringStuffWithPython/Chapter17/P02_manipulatingImages.py
+++ books/AutomateTheBoringStuffWithPython/Chapter17/P02_manipulatingImages.py
@@ -59,3 +59,20 @@ catIm.rotate(6, expand=True).save('rotated6_expanded.png')  # fit image
 
 catIm.transpose(Image.FLIP_LEFT_RIGHT).save('horizontal_flip.png')
 catIm.transpose(Image.FLIP_TOP_BOTTOM).save('vertical_flip.png')
+
+# Changing Individual Pixels
+im = Image.new('RGBA', (100, 100))
+print(im.getpixel((0, 0)))
+
+for x in range(100):
+    for y in range(50):
+        im.putpixel((x, y), (210, 210, 210))
+
+from PIL import ImageColor  # Don't do this - imports should be at the top of the file
+for x in range(100):
+    for y in range(50, 100):
+        im.putpixel((x, y), ImageColor.getcolor('darkgray', 'RGBA'))
+
+print(im.getpixel((0, 0)))
+print(im.getpixel((0, 50)))
+im.save('putPixel.png')
