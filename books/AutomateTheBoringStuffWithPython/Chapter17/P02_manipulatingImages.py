8e88b8cf4f7b73b8cd3d3b981af94bc48038e9ab
JoseALermaIII <jose@JoseALerma.com>
2018-09-26 03:16:17 -0500

    Update P02_manipulatingImages added rotating and flipping

--- books/AutomateTheBoringStuffWithPython/Chapter17/P02_manipulatingImages.py
+++ books/AutomateTheBoringStuffWithPython/Chapter17/P02_manipulatingImages.py
@@ -48,3 +48,14 @@ quartersizedIm = catIm.resize((int(width / 2), int(height / 2)))
 quartersizedIm.save('quartersized.png')
 svelteIm = catIm.resize((width, height + 300))
 svelteIm.save('svelte.png')
+
+# Rotating and Flipping Images
+catIm.rotate(90).save('rotated90.png')  # counterclockwise rotations
+catIm.rotate(180).save('rotated180.png')
+catIm.rotate(270).save('rotated270.png')  # Sometimes uses black bars to fill gaps at 90° and 270°
+
+catIm.rotate(6).save('rotated6.png')
+catIm.rotate(6, expand=True).save('rotated6_expanded.png')  # fit image
+
+catIm.transpose(Image.FLIP_LEFT_RIGHT).save('horizontal_flip.png')
+catIm.transpose(Image.FLIP_TOP_BOTTOM).save('vertical_flip.png')
