1101cd5c7c65dada28c3d99a006761ec0e5266d4
JoseALermaIII <jose@JoseALerma.com>
2018-09-26 02:56:54 -0500

    Update P02_manipulatingImages added copying and pasting images

--- books/AutomateTheBoringStuffWithPython/Chapter17/P02_manipulatingImages.py
+++ books/AutomateTheBoringStuffWithPython/Chapter17/P02_manipulatingImages.py
@@ -22,3 +22,22 @@ im2.save('transparentImage.png')
 # Cropping Images
 croppedIm = catIm.crop((335, 345, 565, 560))  # (origin-x, origin-y, x + 1, y + 1), where (x/y + 1) are exclusive
 croppedIm.save('cropped.png')
+
+# Copying and Pasting Images onto Other Images
+catIm = Image.open('zophie.png')
+catCopyIm = catIm.copy()
+
+faceIm = catIm.crop((335, 345, 565, 560))
+print(faceIm.size)
+catCopyIm.paste(faceIm, (0, 0))
+catCopyIm.paste(faceIm, (400, 500))
+catCopyIm.save('pasted.png')
+
+catImWidth, catImHeight = catIm.size
+faceImWidth, faceImHeight = faceIm.size
+catCopyTwo = catIm.copy()
+for left in range(0, catImWidth, faceImWidth):
+    for top in range(0, catImHeight, faceImHeight):
+        print(left, top)
+        catCopyTwo.paste(faceIm, (left, top))
+catCopyTwo.save('tiled.png')
