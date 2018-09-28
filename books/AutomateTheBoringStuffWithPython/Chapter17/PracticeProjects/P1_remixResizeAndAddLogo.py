903f8b34d269e675a83edd6bcff79e191d27f478
JoseALermaIII <jose@JoseALerma.com>
2018-09-28 02:09:03 -0500

    Update P1_remixResizeAndAddLogo added new extensions and process files from FOLDER

--- books/AutomateTheBoringStuffWithPython/Chapter17/PracticeProjects/P1_remixResizeAndAddLogo.py
+++ books/AutomateTheBoringStuffWithPython/Chapter17/PracticeProjects/P1_remixResizeAndAddLogo.py
@@ -11,3 +11,44 @@
 # ugly. Modify resizeAndAddLogo.py so that the image must be at least
 # twice the width and height of the logo image before the logo is pasted. Otherwise,
 # it should skip adding the logo.
+
+import os
+from PIL import Image
+
+SQUARE_FIT_SIZE = 300
+LOGO_FILENAME = '../catlogo.png'
+EXTENSIONS = ['.png', '.jpg', '.gif', '.bmp']
+FOLDER = '../'
+
+logoIm = Image.open(LOGO_FILENAME)
+logoWidth, logoHeight = logoIm.size
+
+os.makedirs(os.path.join(FOLDER, 'withLogo'), exist_ok=True)
+# Loop over all files in the working directory.
+for filename in os.listdir(FOLDER):
+    if not filename.endswith(tuple(EXTENSIONS)) or filename == LOGO_FILENAME:
+        continue  # skip non-image files and the logo file itself
+
+    im = Image.open(os.path.join(FOLDER, filename))
+    width, height = im.size
+
+    # Check if image needs to be resized.
+    if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
+        # Calculate the new width and height to resize to.
+        if width > height:
+            height = int((SQUARE_FIT_SIZE / width) * height)
+            width = SQUARE_FIT_SIZE
+        else:
+            width = int((SQUARE_FIT_SIZE / height) * width)
+            height = SQUARE_FIT_SIZE
+
+        # Resize the image.
+        print('Resizing %s...' % filename)
+        im = im.resize((width, height))
+
+    # Add the logo.
+    print('Adding logo to %s...\n' % filename)
+    im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
+
+    # Save changes.
+    im.save(os.path.join(os.path.join(FOLDER, 'withLogo'), filename))
