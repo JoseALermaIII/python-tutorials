109cf3fc767fad4bfe71155e9dc88c57bd58f0cf
JoseALermaIII <jose@JoseALerma.com>
2018-09-29 02:39:15 -0500

    Update P2_findPhotoFolders working solution

--- books/AutomateTheBoringStuffWithPython/Chapter17/PracticeProjects/P2_findPhotoFolders.py
+++ books/AutomateTheBoringStuffWithPython/Chapter17/PracticeProjects/P2_findPhotoFolders.py
@@ -5,19 +5,29 @@
 import os
 from PIL import Image
 
-for foldername, subfolders, filenames in os.walk('C:\\'):
+FOLDER = '/home/jose'
+EXTENSIONS = ['.png', '.jpg', '.jpeg']
+MIN_SIZE = 500
+
+for foldername, subfolders, filenames in os.walk(FOLDER):
     photo_files = 0
     non_photo_files = 0
     for filename in filenames:
         # Check if file extension isn't .png or .jpg.
-        if TODO:
+        if not filename.lower().endswith(tuple(EXTENSIONS)):
             non_photo_files += 1
             continue    # skip to next filename
 
         # Open image file using Pillow.
+        try:
+            image = Image.open(os.path.join(foldername, filename))
+            width, height = image.size
+        except OSError as instance:
+            print(f'OSError: {instance}')
+            continue  # skip to next filename
 
         # Check if width & height are larger than 500.
-        if TODO:
+        if width > MIN_SIZE and height > MIN_SIZE:
             # Image is large enough to be considered a photo.
             photo_files += 1
         else:
@@ -26,5 +36,6 @@ for foldername, subfolders, filenames in os.walk('C:\\'):
 
     # If more than half of files were photos,
     # print the absolute path of the folder.
-    if TODO:
-        print(TODO)
+    total_files = photo_files + non_photo_files
+    if photo_files > total_files / 2:
+        print(f'Found folder: {foldername}')
