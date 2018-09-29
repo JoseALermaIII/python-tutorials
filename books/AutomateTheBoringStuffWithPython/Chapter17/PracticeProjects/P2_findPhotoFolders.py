28249990dbb58eafb1c6acd5a18c4b42e5df742e
JoseALermaIII <jose@JoseALerma.com>
2018-09-29 02:20:02 -0500

    Update P2_findPhotoFolders added description, imports, and changed variable names

--- books/AutomateTheBoringStuffWithPython/Chapter17/PracticeProjects/P2_findPhotoFolders.py
+++ books/AutomateTheBoringStuffWithPython/Chapter17/PracticeProjects/P2_findPhotoFolders.py
@@ -1,13 +1,17 @@
 #! python3
-# Import modules and write comments to describe this program.
+# P2_findPhotoFolders.py - This program identifies all folders with over
+# 50% of images of a certain size.
+
+import os
+from PIL import Image
 
 for foldername, subfolders, filenames in os.walk('C:\\'):
-    numPhotoFiles = 0
-    numNonPhotoFiles = 0
+    photo_files = 0
+    non_photo_files = 0
     for filename in filenames:
         # Check if file extension isn't .png or .jpg.
         if TODO:
-            numNonPhotoFiles += 1
+            non_photo_files += 1
             continue    # skip to next filename
 
         # Open image file using Pillow.
@@ -15,10 +19,10 @@ for foldername, subfolders, filenames in os.walk('C:\\'):
         # Check if width & height are larger than 500.
         if TODO:
             # Image is large enough to be considered a photo.
-            numPhotoFiles += 1
+            photo_files += 1
         else:
             # Image is too small to be a photo.
-            numNonPhotoFiles += 1
+            non_photo_files += 1
 
     # If more than half of files were photos,
     # print the absolute path of the folder.
