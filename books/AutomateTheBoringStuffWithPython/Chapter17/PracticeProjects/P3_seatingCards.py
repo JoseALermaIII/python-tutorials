60d989c91069788e12f4b7632ae66e41236c3a3d
JoseALermaIII <jose@JoseALerma.com>
2018-09-29 03:19:54 -0500

    Update P3_seatingCards added imports and outline

--- books/AutomateTheBoringStuffWithPython/Chapter17/PracticeProjects/P3_seatingCards.py
+++ books/AutomateTheBoringStuffWithPython/Chapter17/PracticeProjects/P3_seatingCards.py
@@ -8,3 +8,16 @@
 # of the invitation image so that when the image is printed out, there will be a
 # guideline for cutting. The PNG files that Pillow produces are set to 72 pixels per
 # inch, so a 4×5-inch card would require a 288×360-pixel image.
+
+import os
+from PIL import Image, ImageFont, ImageDraw
+
+# Get guest list from file
+
+# For each guest, make black 4x5-inch card
+
+# Add guest's name
+
+# Add flowery decoration
+
+# Save seating card
