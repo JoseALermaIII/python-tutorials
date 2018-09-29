a0333aa80dd6a6baeb24e32deeecd0288419328e
JoseALermaIII <jose@JoseALerma.com>
2018-09-29 03:11:10 -0500

    Initialize P3_seatingCards

---
+++ books/AutomateTheBoringStuffWithPython/Chapter17/PracticeProjects/P3_seatingCards.py
@@ -0,0 +1,10 @@
+# Chapter 13 included a practice project to create custom invitations from a list of
+# guests in a plaintext file. As an additional project, use the pillow module to
+# create images for custom seating cards for your guests. For each of the guests listed
+# in the guests.txt, generate an image file with the guest name and some flowery
+# decoration.
+#
+# To ensure that each seating card is the same size, add a black rectangle on the edges
+# of the invitation image so that when the image is printed out, there will be a
+# guideline for cutting. The PNG files that Pillow produces are set to 72 pixels per
+# inch, so a 4×5-inch card would require a 288×360-pixel image.
