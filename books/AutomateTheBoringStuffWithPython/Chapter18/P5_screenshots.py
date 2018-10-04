36977c450f8a75b773f16add49beb3348b98f678
JoseALermaIII <jose@JoseALerma.com>
2018-10-04 05:55:18 -0500

    Update P5_screenshots added image recognition

--- books/AutomateTheBoringStuffWithPython/Chapter18/P5_screenshots.py
+++ books/AutomateTheBoringStuffWithPython/Chapter18/P5_screenshots.py
@@ -15,3 +15,9 @@ print(im.getpixel((50, 200)))
 im.getpixel((50, 200))  # identify RGB to match
 print(pyautogui.pixelMatchesColor(50, 200, (64, 0, 193)))
 print(pyautogui.pixelMatchesColor(50, 200, (65, 0, 193)))
+
+# Image Recognition
+print(pyautogui.locateOnScreen('books.png'))  # must be pixel-perfect match
+print(list(pyautogui.locateAllOnScreen('books.png')))
+print(pyautogui.center((217, 85, 75, 21)))  # xy of center of match area
+pyautogui.click((254, 95))
