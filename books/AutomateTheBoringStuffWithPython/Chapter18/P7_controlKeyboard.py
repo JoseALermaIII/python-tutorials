3e469432f4f8b11b999625c54bea43cf52539b3e
JoseALermaIII <jose@JoseALerma.com>
2018-10-04 06:24:52 -0500

    Update P7_controlKeyboard added hotkey combinations

--- books/AutomateTheBoringStuffWithPython/Chapter18/P7_controlKeyboard.py
+++ books/AutomateTheBoringStuffWithPython/Chapter18/P7_controlKeyboard.py
@@ -15,3 +15,23 @@ pyautogui.press('enter')
 pyautogui.keyDown('shift')
 pyautogui.press('4')
 pyautogui.keyUp('shift')
+
+# Hotkey Combinations
+pyautogui.keyDown('ctrl')
+pyautogui.keyDown('c')
+pyautogui.keyUp('c')
+pyautogui.keyUp('ctrl')
+
+pyautogui.hotkey('ctrl', 'c')
+
+import time  # imports should be at the top of the file
+
+
+def commentAfterDelay():
+    pyautogui.click(100, 100)  # focus IDLE file editor
+    pyautogui.typewrite('In IDLE, Alt-3 comments out a line.')
+    time.sleep(2)
+    pyautogui.hotkey('alt', '3')
+
+
+commentAfterDelay()
