29ba48e4c39a102f66126301bc772b3593a0d4f3
JoseALermaIII <jose@JoseALerma.com>
2018-10-18 02:32:46 -0500

    Update P1_lookingBusy.py working solution

--- books/AutomateTheBoringStuffWithPython/Chapter18/PracticeProjects/P1_lookingBusy.py
+++ books/AutomateTheBoringStuffWithPython/Chapter18/PracticeProjects/P1_lookingBusy.py
@@ -1,3 +1,16 @@
 # Write a script to nudge your mouse cursor slightly every ten seconds.
 # The nudge should be small enough so that it won’t get in the way if
 # you do happen to need to use your computer while the script is running.
+
+import pyautogui, time
+
+
+def main():
+    while True:
+        time.sleep(10)
+        pyautogui.moveRel(1, 0)
+
+
+# If run directly (instead of imported), run main()
+if __name__ == '__main__':
+    main()
