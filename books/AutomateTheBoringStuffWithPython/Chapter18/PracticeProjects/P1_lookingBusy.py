297a8043383bb31fd4e5541f9886c7a3b56fe216
JoseALermaIII <jose@JoseALerma.com>
2018-10-18 02:25:08 -0500

    Initialize P1_lookingBusy.py

---
+++ books/AutomateTheBoringStuffWithPython/Chapter18/PracticeProjects/P1_lookingBusy.py
@@ -0,0 +1,3 @@
+# Write a script to nudge your mouse cursor slightly every ten seconds.
+# The nudge should be small enough so that it won’t get in the way if
+# you do happen to need to use your computer while the script is running.
