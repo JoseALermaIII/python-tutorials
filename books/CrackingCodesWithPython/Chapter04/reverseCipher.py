2e25125904c94fa7250f6a8a0f4de4e2c463644b
jose <jose@JoseALerma.com>
2018-11-22 22:05:06 -0600

    Update Chapter04/reverseCipher.py added docstring and wrapped in main() function

--- books/CrackingCodesWithPython/Chapter04/reverseCipher.py
+++ books/CrackingCodesWithPython/Chapter04/reverseCipher.py
@@ -1,22 +1,31 @@
-# Reverse Cipher
-# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
-# Note: pretty much the same, except I use double quotes,
-# expand variable names for readability, simplify the while loop,
-# and use fancier operators
+"""Reverse Cipher
 
-message = "Three can keep a secret, if two of them are dead."
-#message = input("Enter message: ")  # Allow users to enter message
-translated = ""
+https://www.nostarch.com/crackingcodes/ (BSD Licensed)
 
-index = len(message)
-while index:
-    index -= 1  # equivalent to index = index - 1
-    translated += message[index]
-    #print("index is", i, ", message[index] is", message[index], ", translated is", translated)  #DEBUG
+Note:
+    Pretty much the same, except I use double quotes,
+    expand variable names for readability, simplify the while loop, and use fancier operators
+"""
 
-print(translated)
 
-# Decrypt example
-# Was going to make a function out of it so it's easier, but I
-# didn't want to change it more than I already did. (^_^;)
-#print(translated[::-1])  # a step of -1 reverses the string
+def main():
+    message = "Three can keep a secret, if two of them are dead."
+    #message = input("Enter message: ")  # Allow users to enter message
+    translated = ""
+
+    index = len(message)
+    while index:
+        index -= 1  # equivalent to index = index - 1
+        translated += message[index]
+        #print("index is", i, ", message[index] is", message[index], ", translated is", translated)  #DEBUG
+
+    print(translated)
+
+    # Decrypt example
+    # Was going to make a function out of it so it's easier, but I
+    # didn't want to change it more than I already did. (^_^;)
+    #print(translated[::-1])  # a step of -1 reverses the string
+
+
+if __name__ == '__main__':
+    main()
