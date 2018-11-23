69daf89d9d28e97d9b535365dfab059b989d78ae
JoseALermaIII <jose@JoseALerma.com>
2018-11-23 17:58:13 -0600

    Update Chapter09/passingReference.py added docstrings and wrapped in main() function

--- books/CrackingCodesWithPython/Chapter09/passingReference.py
+++ books/CrackingCodesWithPython/Chapter09/passingReference.py
@@ -1,10 +1,28 @@
-# Passing references in a function
+"""Passing references in a function
+
+Demonstrates how to pass a reference to a function.
+"""
 
 
-def eggs(someParameter):
+def eggs(someParameter: list) -> None:
+    """Append to a parameter.
+
+    Appends 'Hello' to a given parameter.
+
+    Args:
+        someParameter: List of elements.
+
+    Returns:
+        None. Only appends a string to a provided parameter.
+    """
     someParameter.append('Hello')
 
 
-spam = [1, 2, 3]
-eggs(spam)
-print(spam)
+def main():
+    spam = [1, 2, 3]
+    eggs(spam)
+    print(spam)
+
+
+if __name__ == '__main__':
+    main()
