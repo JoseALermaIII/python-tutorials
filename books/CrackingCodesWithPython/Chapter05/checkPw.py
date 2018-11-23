108195ec1a6187aaec59a177b434b2fc1457a71d
jose <jose@JoseALerma.com>
2018-11-22 23:08:27 -0600

    Update Chapter05/checkPw.py added docstring and wrapped in main() function

--- books/CrackingCodesWithPython/Chapter05/checkPw.py
+++ books/CrackingCodesWithPython/Chapter05/checkPw.py
@@ -1,11 +1,22 @@
-# Password checker
-typedPassword = input("Enter your password.\n")
-if typedPassword == "hunter2":
-    print("You entered: *******")
-elif typedPassword == "jose":
-    print("Hint: the password is a meme.")
-elif typedPassword == "12345":
-    print("That is a really obvious password.")
-else:
-    print("You entered: " + typedPassword)
-print("Done")
+"""Password checker.
+
+Checks given input to saved password.
+"""
+
+
+def main():
+    # Password checker
+    typedPassword = input("Enter your password.\n")
+    if typedPassword == "hunter2":
+        print("You entered: *******")
+    elif typedPassword == "jose":
+        print("Hint: the password is a meme.")
+    elif typedPassword == "12345":
+        print("That is a really obvious password.")
+    else:
+        print("You entered: " + typedPassword)
+    print("Done")
+
+
+if __name__ == '__main__':
+    main()
