77974a0512ade56e5c68bdb2649b77bc21cfb021
jose <jose@JoseALerma.com>
2018-11-22 23:05:30 -0600

    Update Chapter05/caesarCipher.py added docstring and wrapped in main() function

--- books/CrackingCodesWithPython/Chapter05/caesarCipher.py
+++ books/CrackingCodesWithPython/Chapter05/caesarCipher.py
@@ -1,45 +1,56 @@
-# Caesar Cipher
-# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+"""Caesar Cipher
+
+Demonstrates the use of a caesar cipher. Prints output and copies to clipboard.
+
+Note:
+    https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+"""
 
 from books.CrackingCodesWithPython.pyperclip import copy
 
-# The string to be encrypted/decrypted:
-message = 'This is my secret message.'
 
-# The encryption/decryption key:
-key = 13
+def main():
+    # The string to be encrypted/decrypted:
+    message = 'This is my secret message.'
 
-# Whether the program encrypts or decrypts:
-mode = 'encrypt'  # Set to either 'encrypt' or 'decrypt'.
+    # The encryption/decryption key:
+    key = 13
 
-# Every possible symbol that can be encrypted:
-SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
+    # Whether the program encrypts or decrypts:
+    mode = 'encrypt'  # Set to either 'encrypt' or 'decrypt'.
 
-# Store the encrypted/decrypted form of the message:
-translated = ''
+    # Every possible symbol that can be encrypted:
+    SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
 
-for symbol in message:
-    # Note: Only symbols in the SYMBOLS string can be encrypted/decrypted.
-    if symbol in SYMBOLS:
-        symbolIndex = SYMBOLS.find(symbol)
+    # Store the encrypted/decrypted form of the message:
+    translated = ''
 
-        # Perform encryption/decryption:
-        if mode == 'encrypt':
-            translatedIndex = symbolIndex + key
-        elif mode == 'decrypt':
-            translatedIndex = symbolIndex - key
+    for symbol in message:
+        # Note: Only symbols in the SYMBOLS string can be encrypted/decrypted.
+        if symbol in SYMBOLS:
+            symbolIndex = SYMBOLS.find(symbol)
 
-        # Handle wraparound, if needed:
-        if translatedIndex >= len(SYMBOLS):
-            translatedIndex -= len(SYMBOLS)
-        elif translatedIndex < 0:
-            translatedIndex += len(SYMBOLS)
+            # Perform encryption/decryption:
+            if mode == 'encrypt':
+                translatedIndex = symbolIndex + key
+            elif mode == 'decrypt':
+                translatedIndex = symbolIndex - key
 
-        translated += SYMBOLS[translatedIndex]
-    else:
-        # Append the symbol without encrypting/decrypting:
-        translated += symbol
+            # Handle wraparound, if needed:
+            if translatedIndex >= len(SYMBOLS):
+                translatedIndex -= len(SYMBOLS)
+            elif translatedIndex < 0:
+                translatedIndex += len(SYMBOLS)
 
-# Output the translated string:
-print(translated)
-copy(translated)
+            translated += SYMBOLS[translatedIndex]
+        else:
+            # Append the symbol without encrypting/decrypting:
+            translated += symbol
+
+    # Output the translated string:
+    print(translated)
+    copy(translated)
+
+
+if __name__ == '__main__':
+    main()
