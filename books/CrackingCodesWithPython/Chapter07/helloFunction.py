70f5043bdf95557dbc071a72f802293bdb2b669f
jose <jose@JoseALerma.com>
2018-11-22 23:44:55 -0600

    Update Chapter07/helloFunction.py added docstrings and wrapped in main() function

--- books/CrackingCodesWithPython/Chapter07/helloFunction.py
+++ books/CrackingCodesWithPython/Chapter07/helloFunction.py
@@ -1,10 +1,30 @@
-# Hello function
-def hello(name):
+"""Hello function.
+
+Contains function that prints hello to given name.
+"""
+
+
+def hello(name: str) -> None:
+    """Prints hello.
+
+    Prints hello to given name.
+
+    Args:
+        name: Name to say hello to.
+
+    Returns:
+        Prints hello to given name.
+    """
     print('Hello, ' + name)
 
 
-print('Start.')
-hello('Clarice')
-print('Call the function again:')
-hello('Dr. Lecter')
-print('Done.')
+def main():
+    print('Start.')
+    hello('Clarice')
+    print('Call the function again:')
+    hello('Dr. Lecter')
+    print('Done.')
+
+
+if __name__ == '__main__':
+    main()
