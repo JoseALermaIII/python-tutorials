e9a9dcb7e1d949717db313516f9d6f2ca9afbd90
jose <jose@JoseALerma.com>
2018-11-22 20:37:53 -0600

    Update Chapter01/config.py added docstring

--- books/CrackingCodesWithPython/Chapter01/config.py
+++ books/CrackingCodesWithPython/Chapter01/config.py
@@ -1,4 +1,9 @@
-# Configuration File with global variables
+"""Configuration file with global variables.
 
+Mainly contains definition of every possible encryptable symbol.
+
+Attributes:
+    SYMBOLS (str): Every possible symbol that can be encrypted.
+"""
 # Every possible symbol that can be encrypted:
 SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
