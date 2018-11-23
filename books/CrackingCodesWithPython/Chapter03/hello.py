b5fb2d5363a5604f3fd238058882be0494e084ee
jose <jose@JoseALerma.com>
2018-11-22 21:47:52 -0600

    Update Chapter03/hello.py added docstring and switched double quotes to single quotes

--- books/CrackingCodesWithPython/Chapter03/hello.py
+++ books/CrackingCodesWithPython/Chapter03/hello.py
@@ -1,8 +1,17 @@
-# This program says hello and asks for my name.
-# Note: using double quotes for strings because I'm a nitpicker -
-# author admits that he uses single quotes because it is easier to type
-# and it technically doesn't matter
+"""Asks for name and says hello.
 
-print("Hello, world!")
-myName = input("What is your name?\n")
-print("It is good to meet you, " + myName)
+This program says hello and asks for my name.
+
+Note:
+    Using double quotes for strings because I'm a nitpicker - author admits that he uses single quotes because it is
+    easier to type and it technically doesn't matter.
+    Nov. 22, 2018 Update: Switching back to single quotes because a system was compromised because of `double quotes`_.
+    
+
+.. _double quotes:
+    https://www.gq.com/story/how-to-hack-an-election
+
+"""
+print('Hello, world!')
+myName = input('What is your name?\n')
+print('It is good to meet you, ' + myName)
