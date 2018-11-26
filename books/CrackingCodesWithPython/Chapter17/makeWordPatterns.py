1e81eaa809f60e0c05dca7c04cec4324221586e1
JoseALermaIII <jose@JoseALerma.com>
2018-11-26 05:19:52 -0600

    Update Chapter17/makeWordPatterns.py added docstrings

--- books/CrackingCodesWithPython/Chapter17/makeWordPatterns.py
+++ books/CrackingCodesWithPython/Chapter17/makeWordPatterns.py
@@ -1,18 +1,38 @@
-# Makes the wordPatterns.py File
-# https://www.nostarch.com/crackingcodes (BSD Licensed)
+"""Make wordPatterns.py file
 
-# Creates wordPatterns.py based on the words in our dictionary
-# text file, dictionary.txt. (Download this file from
-# https://invpy.com/dictionary.txt)
+Creates wordPatterns.py based on the words in our dictionary text file, dictionary.txt.
+A word pattern assigns a number to each letter in a word, then generates a pattern representation of that word
+based on the number assigned to each letter.
+
+Attributes:
+    DICTIONARY_FILE (str): String containing absolute path to dictionary.txt file.
+
+Note:
+    * Download the dictionary file from https://invpy.com/dictionary.txt
+    * https://www.nostarch.com/crackingcodes (BSD Licensed)
+"""
 
 import pprint
 
 DICTIONARY_FILE = '/home/jose/PycharmProjects/python-tutorials/books/CrackingCodesWithPython/Chapter11/dictionary.txt'
 
 
-def getWordPattern(word):
-    # Returns a string of the pattern form of the given word.
-    # e.g. '0.1.2.3.4.1.2.3.5.6' for 'DUSTBUSTER'
+def getWordPattern(word: str) -> str:
+    """Get word pattern
+
+    Returns a string of the pattern form of the given word.
+
+    Args:
+        word: String containing word to convert into word pattern.
+
+    Example:
+    >>> import books.CrackingCodesWithPython.Chapter17.makeWordPatterns as makeWordPatterns
+    >>> makeWordPatterns.getWordPattern('DUSTBUSTER')
+    '0.1.2.3.4.1.2.3.5.6'
+
+    Returns:
+        String containing word pattern.
+    """
     word = word.upper()
     nextNum = 0
     letterNums = {}
