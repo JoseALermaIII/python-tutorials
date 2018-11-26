8517fbb4072e6bb3e66101bdf3290fe02cf57c5e
JoseALermaIII <jose@JoseALerma.com>
2018-11-26 06:09:02 -0600

    Update Chapter17/simpleSubHacker.py added docstrings

--- books/CrackingCodesWithPython/Chapter17/simpleSubHacker.py
+++ books/CrackingCodesWithPython/Chapter17/simpleSubHacker.py
@@ -1,5 +1,16 @@
-# Simple Substitution Cipher Hacker
-# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+"""Simple Substitution Cipher Hacker
+
+Implements a function that can hack a substitution cipher encrypted message.
+
+Attributes:
+    LETTERS (str): String containing uppercase latin letters.
+    nonLettersOrSpacePattern (_sre.SRE_Pattern): Regular expression object representing all non-letter
+        characters and space.
+
+Note:
+    * https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+
+"""
 
 import re
 import copy
@@ -39,32 +50,60 @@ rmsuijarj aqsoaxwa. Jia pcsusx py nhjir sr agbmlsxao sx jisr elh.
     print(hackedMessage)
 
 
-def getBlankCipherletterMapping():
-    # Returns a dictionary value that is a blank cipherletter mapping:
+def getBlankCipherletterMapping() -> dict:
+    """Get blank cipherletter mapping
+
+    Returns a dictionary value that is a blank cipherletter mapping
+
+    Returns:
+         Returns dictionary with uppercase latin letters as keys and empty lists as values.
+    """
     return {'A': [], 'B': [], 'C': [], 'D': [], 'E': [], 'F': [], 'G': [],
             'H': [], 'I': [], 'J': [], 'K': [], 'L': [], 'M': [], 'N': [],
             'O': [], 'P': [], 'Q': [], 'R': [], 'S': [], 'T': [], 'U': [],
             'V': [], 'W': [], 'X': [], 'Y': [], 'Z': []}
 
 
-def addLettersToMapping(letterMapping, cipherword, candidate):
-    # The letterMapping parameter takes a dictionary value that
-    # stores a cipherletter mapping, which is copied by the function.
-    # The cipherword parameter is a string value of the ciphertext word.
-    # The candidate parameter is a possible English word that the
-    # cipherword could decrypt to.
+def addLettersToMapping(letterMapping: dict, cipherword: str, candidate: str) -> None:
+    """Add letters to cipherletter mapping
 
-    # This function adds the letters in the candidate as potential
-    # decryption letters for the cipherletters in the cipherletter
-    # mapping.
+    The letterMapping parameter takes a dictionary value that
+    stores a cipherletter mapping, which is copied by the function.
+    The cipherword parameter is a string value of the ciphertext word.
+    The candidate parameter is a possible English word that the
+    cipherword could decrypt to.
 
+    This function adds the letters in the candidate as potential
+    decryption letters for the cipherletters in the cipherletter
+    mapping.
+
+    Args:
+        letterMapping: Dictionary containing a cipherletter mapping.
+        cipherword: String containing an encrypted ciphertext word.
+        candidate: String containing an English word the cipherword could potentially decrypt to.
+
+    Return:
+        None. Modifies contents of letterMapping by adding letters to the cipherletter mapping.
+    """
     for i in range(len(cipherword)):
         if candidate[i] not in letterMapping[cipherword[i]]:
             letterMapping[cipherword[i]].append(candidate[i])
 
 
+def intersectMappings(mapA: dict, mapB: dict) -> dict:
+    """Intersects two cipherletter mappings
 
-def intersectMappings(mapA, mapB):
+    Checks each letter in LETTERS and adds to intersected map if it exists in both given maps.
+    If either map is empty, the non-empty map is copied to the intersected map.
+
+    Args:
+        mapA: Dictionary containing potential decryption letters.
+        mapB: Dictionary containing potential decryption letters.
+
+    Returns:
+        Dictionary containing intersected map of potential decryption letters.
+
+    """
     # To intersect two maps, create a blank map and then add only the
     # potential decryption letters if they exist in BOTH maps:
     intersectedMapping = getBlankCipherletterMapping()
@@ -86,15 +125,28 @@ def intersectMappings(mapA, mapB):
     return intersectedMapping
 
 
-def removeSolvedLettersFromMapping(letterMapping):
-    # Cipherletters in the mapping that map to only one letter are
-    # "solved" and can be removed from the other letters.
-    # For example, if 'A' maps to potential letters ['M', 'N'] and 'B'
-    # maps to ['N'], then we know that 'B' must map to 'N', so we can
-    # remove 'N' from the list of what 'A' could map to. So 'A' then maps
-    # to ['M']. Note that now that 'A' maps to only one letter, we can
-    # remove 'M' from the list of letters for every other letter.
-    # (This is why there is a loop that keeps reducing the map.)
+def removeSolvedLettersFromMapping(letterMapping: dict) -> dict:
+    """Removes solved letters from cipherletter mapping
+
+    Cipherletters in the mapping that map to only one letter are
+    "solved" and can be removed from the other letters.
+
+    For example, if 'A' maps to potential letters ['M', 'N'] and 'B'
+    maps to ['N'], then we know that 'B' must map to 'N', so we can
+    remove 'N' from the list of what 'A' could map to. So 'A' then maps
+    to ['M'].
+
+    Note that now that 'A' maps to only one letter, we can
+    remove 'M' from the list of letters for every other letter.
+    (This is why there is a loop that keeps reducing the map.)
+
+    Args:
+        letterMapping: Cipherletter map dictionary to remove solved letters from.
+
+    Returns:
+        Dictionary containing cipherletter map with solved letters removed.
+
+    """
 
     loopAgain = True
     while loopAgain:
@@ -121,7 +173,19 @@ def removeSolvedLettersFromMapping(letterMapping):
     return letterMapping
 
 
-def hackSimpleSub(message):
+def hackSimpleSub(message: str) -> dict:
+    """Hack simple substitution cipher
+
+    Hacks simple substitution cipher and returns dictionary with cipherletter map that may be able
+    to decrypt given message.
+
+    Args:
+        message: String containing substitution cipher encrypted message.
+
+    Returns:
+        Dictionary with cipherletter map that may decrypt given message.
+
+    """
     intersectedMap = getBlankCipherletterMapping()
     cipherwordList = nonLettersOrSpacePattern.sub('', message.upper()).split()
     for cipherword in cipherwordList:
@@ -143,7 +207,23 @@ def hackSimpleSub(message):
     return removeSolvedLettersFromMapping(intersectedMap)
 
 
-def decryptWithCipherletterMapping(ciphertext, letterMapping):
+def decryptWithCipherletterMapping(ciphertext: str, letterMapping: dict) -> str:
+    """Decrypt substitution cipher message with cipherletter map
+
+    Decrypts given substitution cipher encrypted message with given dictionary containing a
+    cipherletter map.
+
+    Args:
+        ciphertext: Substitution cipher encrypted message to decrypt.
+        letterMapping: Dictionary with cipherletter map that may decrypt the ciphertext.
+
+    Returns:
+        String containing decrypted ciphertext message.
+
+    Note:
+        * Ambiguous decrypted letters are replaced with an underscore, '_'
+
+    """
     # Return a string of the ciphertext decrypted with the letter mapping,
     # with any ambiguous decrypted letters replaced with an underscore.
 
