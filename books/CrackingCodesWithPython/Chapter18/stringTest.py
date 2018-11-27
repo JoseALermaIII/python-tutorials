41a42d56cff0da2623d87e742128a64f85ceda4f
JoseALermaIII <jose@JoseALerma.com>
2018-11-27 02:25:33 -0600

    Update Chapter18/stringTest.py added docstring and wrapped in main() function

--- books/CrackingCodesWithPython/Chapter18/stringTest.py
+++ books/CrackingCodesWithPython/Chapter18/stringTest.py
@@ -1,18 +1,31 @@
-# Timing string concatenation vs list appending to make a string
+"""Create string test
+
+Timing string concatenation vs list appending to make a string.
+
+Note:
+    * Prints time to make a 10000 character string 10000 times as seconds since the Unix epoch.
+
+"""
 
 import time
 
-startTime = time.time()
-for trial in range(10000):
-    building = ''
-    for i in range(10000):
-        building += 'x'
-print('String concatenation: ', (time.time() - startTime))
 
-startTime = time.time()
-for trial in range(10000):
-    building = []
-    for i in range(10000):
-        building.append('x')
-    building = ''.join(building)
-print('List appending:       ', (time.time() - startTime))
+def main():
+    startTime = time.time()
+    for trial in range(10000):
+        building = ''
+        for i in range(10000):
+            building += 'x'
+    print('String concatenation: ', (time.time() - startTime))
+
+    startTime = time.time()
+    for trial in range(10000):
+        building = []
+        for i in range(10000):
+            building.append('x')
+        building = ''.join(building)
+    print('List appending:       ', (time.time() - startTime))
+
+
+if __name__ == '__main__':
+    main()
