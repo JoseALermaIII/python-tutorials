d936fa882fd6bec851a23c0af92694abc4dc28e1
JoseALermaIII <jose@JoseALerma.com>
2018-11-28 03:44:52 -0600

    Update Chapter20/vigenereDictionaryHacker.py added docstrings

--- books/CrackingCodesWithPython/Chapter20/vigenereDictionaryHacker.py
+++ books/CrackingCodesWithPython/Chapter20/vigenereDictionaryHacker.py
@@ -1,5 +1,14 @@
-# Vigenère Cipher Dictionary Hacker
-# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+"""Vigenère Cipher Dictionary Hacker
+
+Implements a function that can hack a Vigenère cipher encrypted message using a dictionary.
+
+Attributes:
+    DICTIONARY_FILE (str): String with absolute location of dictionary.txt file.
+
+Note:
+    * https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+
+"""
 
 from books.CrackingCodesWithPython.pyperclip import copy
 from books.CrackingCodesWithPython.Chapter11.detectEnglish import isEnglish
@@ -20,7 +29,19 @@ def main():
         print('Failed to hack encryption.')
 
 
-def hackVigenereDictionary(ciphertext):
+def hackVigenereDictionary(ciphertext: str):
+    """Hack Vigenère Dictionary
+
+    Brute-forces ciphertext by using every word in the dictionary file as a key. Checks if decrypted message is
+    English with the :func:`~books.CrackingCodesWithPython.Chapter11.detectEnglish.isEnglish` module, and prompts user
+    for confirmation by displaying first 100 characters.
+
+    Args:
+        ciphertext: String containing Vigenère cipher encrypted message.
+
+    Returns:
+         Decrypted message, if confirmed, None otherwise.
+    """
     fo = open(DICTIONARY_FILE)
     words = fo.readlines()
     fo.close()
