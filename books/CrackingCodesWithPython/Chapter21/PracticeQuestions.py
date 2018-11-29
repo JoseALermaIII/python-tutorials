cbf78fb1b8c0821e320c469d73136e8893f6eb5c
JoseALermaIII <jose@JoseALerma.com>
2018-11-29 03:13:46 -0600

    Update Chapter21/PracticeQuestions.py added docstring

--- books/CrackingCodesWithPython/Chapter21/PracticeQuestions.py
+++ books/CrackingCodesWithPython/Chapter21/PracticeQuestions.py
@@ -1,4 +1,7 @@
-# Chapter 21 Practice Questions
+"""Chapter 21 Practice Questions
+
+Answers Chapter 21 Practice Questions via Python code.
+"""
 
 from books.CrackingCodesWithPython.Chapter18.vigenereCipher import decryptMessage
 
