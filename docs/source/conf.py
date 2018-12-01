d0ecbb8a215099904e79345ad5491a846bec1dd4
JoseALermaIII <jose@JoseALerma.com>
2018-12-01 03:09:05 -0600

    move version info from version.py to __init__.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../pythontutorials/books/'))
 
-from version import __version__
+from pythontutorials import __version__
 
 # -- Project information -----------------------------------------------------
 
---
+++ pythontutorials/__init__.py
@@ -0,0 +1,5 @@
+# python-tutorials: Persistent Python practice produces prodigious productivity.
+# MIT license
+# Jose A. Lerma III jose@JoseALerma.com
+
+__version__ = '1.0.2'
