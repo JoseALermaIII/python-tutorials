7810388eb6407e4a138d7c2c98a43f237fb04e03
JoseALermaIII <jose@JoseALerma.com>
2018-12-15 03:48:56 -0600

    Update P09_fiveTimes2.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P09_fiveTimes2.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P09_fiveTimes2.py
@@ -1,6 +1,19 @@
-# This program also runs five times
-print('My name is')
-i = 0
-while i < 5:
-    print('Jimmy Five Times (' + str(i) + ')')
-    i = i + 1
+"""Five times 2.0
+
+This program also runs five times, but using a while loop.
+
+"""
+
+
+def main():
+    print('My name is')
+    i = 0
+    while i < 5:
+        print('Jimmy Five Times (' + str(i) + ')')
+        i = i + 1
+
+
+# If P09_fiveTimes2.py is run (instead of imported as a module), call
+# the main() function:
+if __name__ == '__main__':
+    main()
