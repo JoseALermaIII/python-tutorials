e9b1e6e9d9e6799aac0ffaa5f948137bc88f3daf
JoseALermaIII <jose@JoseALerma.com>
2018-12-15 03:04:41 -0600

    Update P01_vampire.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P01_vampire.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P01_vampire.py
@@ -1,11 +1,24 @@
-# This program checks name and age
-name = input("What's your name?")
-age = int(input("How old are you?"))
-if name == 'Alice':
-    print('Hi, Alice.')
-elif age < 12:
-    print('You are not Alice, kiddo.')
-elif age > 2000:
-    print('Unlike you, Alice is not an undead, immortal vampire.')
-elif age > 100:
-    print('You are not Alice, grannie.')
+"""Vampire
+
+This program checks name and age.
+
+"""
+
+
+def main():
+    name = input("What's your name?")
+    age = int(input("How old are you?"))
+    if name == 'Alice':
+        print('Hi, Alice.')
+    elif age < 12:
+        print('You are not Alice, kiddo.')
+    elif age > 2000:
+        print('Unlike you, Alice is not an undead, immortal vampire.')
+    elif age > 100:
+        print('You are not Alice, grannie.')
+
+
+# If P01_vampire.py is run (instead of imported as a module), call
+# the main() function:
+if __name__ == '__main__':
+    main()
