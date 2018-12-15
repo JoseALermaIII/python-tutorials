906468c3c81f726933704346c7b48751686824c6
JoseALermaIII <jose@JoseALerma.com>
2018-12-15 03:15:11 -0600

    Update P05_infiniteloop.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P05_infiniteloop.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P05_infiniteloop.py
@@ -1,4 +1,19 @@
-# This program runs in an infinite loop (don't run unless you know how to stop it)
-# Hint: CTRL-C usually works
-while True:
-    print('Hello world!')
+"""Infinite loop
+
+This program runs in an infinite loop (don't run unless you know how to stop it!).
+
+Note:
+    * CTRL-C usually works
+
+"""
+
+
+def main():
+    while True:
+        print('Hello world!')
+
+
+# If P05_infiniteloop.py is run (instead of imported as a module), call
+# the main() function:
+if __name__ == '__main__':
+    main()
