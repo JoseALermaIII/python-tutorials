88f52ffb0ed08ef84f7fbe3e155fbe7d4235b997
JoseALermaIII <jose@JoseALerma.com>
2018-12-15 03:12:51 -0600

    Update P04_yourName.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P04_yourName.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P04_yourName.py
@@ -1,6 +1,19 @@
-# This program forces you to type your name
-name = ''
-while name != 'your name':
-    print('Please type your name.')
-    name = input()
-print('Thank you!')
+"""Your name
+
+This program forces you to type your name
+
+"""
+
+
+def main():
+    name = ''
+    while name != 'your name':
+        print('Please type your name.')
+        name = input()
+    print('Thank you!')
+
+
+# If P04_yourName.py is run (instead of imported as a module), call
+# the main() function:
+if __name__ == '__main__':
+    main()
