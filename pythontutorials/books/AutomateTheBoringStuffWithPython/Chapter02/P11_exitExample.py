40be6c66dcda22da6d80cbd4fe4542cf174f5a17
JoseALermaIII <jose@JoseALerma.com>
2018-12-15 04:07:37 -0600

    Update P11_exitExample.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P11_exitExample.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P11_exitExample.py
@@ -1,9 +1,22 @@
-# This program terminates itself when told to
-import sys
+"""Exit example
 
-while True:
-    print('Type exit to exit.')
-    response = input()
-    if response == 'exit':
-        sys.exit()
-    print('You typed ' + response + '.')
+This program terminates itself when told to using the :py:mod:`sys` module.
+
+"""
+
+
+def main():
+    import sys
+
+    while True:
+        print('Type exit to exit.')
+        response = input()
+        if response == 'exit':
+            sys.exit()
+        print('You typed ' + response + '.')
+
+
+# If P11_exitExample.py is run (instead of imported as a module), call
+# the main() function:
+if __name__ == '__main__':
+    main()
