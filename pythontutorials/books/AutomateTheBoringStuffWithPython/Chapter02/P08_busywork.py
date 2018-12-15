5d6e36f2b381a466c681c7a668c35cfeaa1ba2fe
JoseALermaIII <jose@JoseALerma.com>
2018-12-15 03:46:02 -0600

    Update P08_busywork.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P08_busywork.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P08_busywork.py
@@ -1,5 +1,18 @@
-# This program adds numbers 0 to 100
-total = 0
-for num in range(101):
-    total = total + num
-print(total)
+"""Busy work
+
+This program adds numbers 0 to 100.
+
+"""
+
+
+def main():
+    total = 0
+    for num in range(101):
+        total = total + num
+    print(total)
+
+
+# If P08_busywork.py is run (instead of imported as a module), call
+# the main() function:
+if __name__ == '__main__':
+    main()
