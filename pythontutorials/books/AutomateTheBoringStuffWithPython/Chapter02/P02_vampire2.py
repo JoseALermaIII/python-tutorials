ed51a3ea43af547f039a16332ab29e92449b5fc6
JoseALermaIII <jose@JoseALerma.com>
2018-12-15 03:06:52 -0600

    Update P02_vampire2.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P02_vampire2.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P02_vampire2.py
@@ -1,11 +1,24 @@
-# This program checks name and age, but won't work as planned
-name = input("What's your name?")
-age = int(input("How old are you?"))
-if name == 'Alice':
-    print('Hi, Alice.')
-elif age < 12:
-    print('You are not Alice, kiddo.')
-elif age > 100:
-    print('You are not Alice, grannie.')
-elif age > 2000:
-    print('Unlike you, Alice is not an undead, immortal vampire.')
+"""Vampire 2.0
+
+This program checks name and age, but won't work as planned.
+
+"""
+
+
+def main():
+    name = input("What's your name?")
+    age = int(input("How old are you?"))
+    if name == 'Alice':
+        print('Hi, Alice.')
+    elif age < 12:
+        print('You are not Alice, kiddo.')
+    elif age > 100:
+        print('You are not Alice, grannie.')
+    elif age > 2000:
+        print('Unlike you, Alice is not an undead, immortal vampire.')
+
+
+# If P02_vampire2.py is run (instead of imported as a module), call
+# the main() function:
+if __name__ == '__main__':
+    main()
