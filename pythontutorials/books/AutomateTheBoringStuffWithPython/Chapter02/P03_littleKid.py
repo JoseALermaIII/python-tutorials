06a8d3c78c574750f1ea66a939813f4791940272
JoseALermaIII <jose@JoseALerma.com>
2018-12-15 03:09:01 -0600

    Update P03_littleKid.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P03_littleKid.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P03_littleKid.py
@@ -1,9 +1,22 @@
-# This program checks name and age
-name = input("What's your name?")
-age = int(input("How old are you?"))
-if name == 'Alice':
-    print('Hi, Alice.')
-elif age < 12:
-    print('You are not Alice, kiddo.')
-else:
-    print('You are neither Alice nor a little kid.')
+"""Little kid
+
+This program checks name and age, but not as many ages.
+
+"""
+
+
+def main():
+    name = input("What's your name?")
+    age = int(input("How old are you?"))
+    if name == 'Alice':
+        print('Hi, Alice.')
+    elif age < 12:
+        print('You are not Alice, kiddo.')
+    else:
+        print('You are neither Alice nor a little kid.')
+
+
+# If P03_littleKid.py is run (instead of imported as a module), call
+# the main() function:
+if __name__ == '__main__':
+    main()
