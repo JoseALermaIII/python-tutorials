126f311bbe5644784977923d99c675fb30383fdb
JoseALermaIII <jose@JoseALerma.com>
2018-12-15 04:11:49 -0600

    Update P12_yourName2.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P12_yourName2.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P12_yourName2.py
@@ -1,7 +1,20 @@
-# This program also forces you to type your name
-while True:
-    print('Please type your name.')
-    name = input()
-    if name == 'your name':
-        break
-print('Thank you!')
+"""Your name 2.0
+
+This program also forces you to type your name, but using a different way to exit the loop.
+
+"""
+
+
+def main():
+    while True:
+        print('Please type your name.')
+        name = input()
+        if name == 'your name':
+            break
+    print('Thank you!')
+
+
+# If P12_yourName2.py is run (instead of imported as a module), call
+# the main() function:
+if __name__ == '__main__':
+    main()
