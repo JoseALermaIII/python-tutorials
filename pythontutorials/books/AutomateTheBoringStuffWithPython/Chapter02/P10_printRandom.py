1b2a28f9137fc715b0b9760b9cdc6edc9d69ffaa
JoseALermaIII <jose@JoseALerma.com>
2018-12-15 04:01:54 -0600

    Update P10_printRandom.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P10_printRandom.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P10_printRandom.py
@@ -1,4 +1,17 @@
-# This program prints five random numbers
-import random
-for i in range(5):
-    print(random.randint(1, 10))
+"""Print random
+
+This program prints five random numbers using the :py:mod:`random` module.
+
+"""
+
+
+def main():
+    import random
+    for i in range(5):
+        print(random.randint(1, 10))
+
+
+# If P10_printRandom.py is run (instead of imported as a module), call
+# the main() function:
+if __name__ == '__main__':
+    main()
