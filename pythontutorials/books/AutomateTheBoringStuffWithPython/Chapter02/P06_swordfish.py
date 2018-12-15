291f034e2a31fe834549b1cf378f0cb1e13dd106
JoseALermaIII <jose@JoseALerma.com>
2018-12-15 03:43:36 -0600

    Update P06_swordfish.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P06_swordfish.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter02/P06_swordfish.py
@@ -1,11 +1,24 @@
-# This program asks for a name and password
-while True:
-    print('Who are you?')
-    name = input()
-    if name != 'Joe':
-        continue
-    print('Hello, Joe. What is the password? (It is a fish.)')
-    password = input()
-    if password == 'swordfish':
-        break
-print('Access granted.')
+"""Swordfish
+
+This program asks for a name and password.
+
+"""
+
+
+def main():
+    while True:
+        print('Who are you?')
+        name = input()
+        if name != 'Joe':
+            continue
+        print('Hello, Joe. What is the password? (It is a fish.)')
+        password = input()
+        if password == 'swordfish':
+            break
+    print('Access granted.')
+
+
+# If P06_swordfish.py is run (instead of imported as a module), call
+# the main() function:
+if __name__ == '__main__':
+    main()
