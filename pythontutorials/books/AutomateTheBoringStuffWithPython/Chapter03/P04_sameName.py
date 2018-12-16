67b7901e27b8957dd0b47a64931c3beb6c29420a
JoseALermaIII <jose@JoseALerma.com>
2018-12-16 03:20:42 -0600

    Update P04_sameName.py added docstrings and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/P04_sameName.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/P04_sameName.py
@@ -1,16 +1,48 @@
-# This program uses the same variable name throughout
-def spam():
-    eggs = 'spam local'
+"""Same name
+
+This program uses the same variable name throughout.
+
+Attributes:
+    eggs (str): String denoting global variable.
+
+Note:
+    Not recommended, but possible.
+
+"""
+
+eggs = 'global'
+
+
+def spam() -> None:
+    """Spam
+
+    Prints its local variable called eggs.
+
+    Returns:
+        None. Prints the local variable.
+    """
+    eggs = 'spam local'  #: String denoting local variable.
     print(eggs)  # prints 'spam local'
 
 
-def bacon():
-    eggs = 'bacon local'
+def bacon() -> None:
+    """Bacon
+
+    Prints its local variable called eggs and also calls :meth:`spam`.
+
+    Returns:
+        None. Prints local variables.
+    """
+    eggs = 'bacon local'  #: String denoting local variable.
     print(eggs)  # prints 'bacon local'
     spam()
     print(eggs)  # prints 'bacon local'
 
 
-eggs = 'global'
-bacon()
-print(eggs)  # prints 'global'
+def main():
+    bacon()
+    print(eggs)  # prints 'global'
+
+
+if __name__ == '__main__':
+    main()
