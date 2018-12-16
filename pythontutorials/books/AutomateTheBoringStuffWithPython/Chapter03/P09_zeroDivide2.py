4dc85ec72bb0fe5cb2eaed36023d28bde0fafb85
JoseALermaIII <jose@JoseALerma.com>
2018-12-16 04:10:21 -0600

    Update P09_zeroDivide2.py added docstrings and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/P09_zeroDivide2.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/P09_zeroDivide2.py
@@ -1,12 +1,33 @@
-# This program handles an error
-def spam(divideBy):
+"""Zero Divide 2.0
+
+This program handles a :class:`ZeroDivisionError`.
+
+"""
+
+
+def spam(divideBy: int) -> float:
+    """Spam
+
+    Divides integer 42 by given integer, but also handles a :class:`ZeroDivisionError`.
+
+    Args:
+        divideBy: Integer to divide 42 by.
+
+    Returns:
+        Float result of 42 divided by given integer.
+    """
     try:
         return 42 / divideBy
     except ZeroDivisionError:
         print('Error: Invalid argument.')
 
 
-print(spam(2))
-print(spam(12))
-print(spam(0))
-print(spam(1))
+def main():
+    print(spam(2))
+    print(spam(12))
+    print(spam(0))
+    print(spam(1))
+
+
+if __name__ == '__main__':
+    main()
