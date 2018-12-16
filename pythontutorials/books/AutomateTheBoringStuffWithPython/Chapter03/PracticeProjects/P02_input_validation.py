1074fe5f3c0c478d993fe21ca56eabf60953d2f2
JoseALermaIII <jose@JoseALerma.com>
2018-12-16 04:46:56 -0600

    Update P02_input_validation.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/PracticeProjects/P02_input_validation.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/PracticeProjects/P02_input_validation.py
@@ -1,17 +1,28 @@
-# This program adds input validation to make_collatz_seq.py
+"""Input validation
 
-# Add try and except statements to the previous project to detect whether
-# the user types in a noninteger string. Normally, the int() function will
-# raise a ValueError error if it is passed a noninteger string, as in int('puppy').
-# In the except clause, print a message to the user saying they must enter an integer.
+This program adds input validation to :py:mod:`.P01_make_collatz_seq`
 
-from P01_make_collatz_seq import collatz
+Add try and except statements to the previous project to detect whether
+the user types in a noninteger string. Normally, the :obj:`int` function will
+raise a :class:`ValueError` error if it is passed a noninteger string, as in `int('puppy')`.
+In the except clause, print a message to the user saying they must enter an integer.
 
-try:
-    n = int(input("Input a number: "))
-    while n != 1:
-        print(n)
-        n = collatz(n)
-    print(n)  # When n == 1
-except ValueError:
-    print("Error: Input must be an integer.")
+
+"""
+
+
+def main():
+    from .P01_make_collatz_seq import collatz
+
+    try:
+        n = int(input("Input a number: "))
+        while n != 1:
+            print(n)
+            n = collatz(n)
+        print(n)  # When n == 1
+    except ValueError:
+        print("Error: Input must be an integer.")
+
+
+if __name__ == "__main__":
+    main()
