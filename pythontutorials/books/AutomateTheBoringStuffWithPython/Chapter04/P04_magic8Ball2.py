bcc39e0fa16cf6dbb601a5cd4f4c30ba61801bb6
JoseALermaIII <jose@JoseALerma.com>
2018-12-16 04:57:10 -0600

    Update P04_magic8Ball2.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter04/P04_magic8Ball2.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter04/P04_magic8Ball2.py
@@ -1,14 +1,26 @@
-# This program more efficiently answers your questions
-import random
+"""Magic 8 Ball 2.0
 
-messages = ['It is certain',
-            'It is decidedly so',
-            'Yes definitely',
-            'Reply hazy try again',
-            'Ask again later',
-            'Concentrate and ask again',
-            'My reply is no',
-            'Outlook not so good',
-            'Very doubtful']
+This program more efficiently answers your questions by indexing a
+list with the answer strings.
 
-print(messages[random.randint(0, len(messages) - 1)])
+"""
+
+
+def main():
+    import random
+
+    messages = ['It is certain',
+                'It is decidedly so',
+                'Yes definitely',
+                'Reply hazy try again',
+                'Ask again later',
+                'Concentrate and ask again',
+                'My reply is no',
+                'Outlook not so good',
+                'Very doubtful']
+
+    print(messages[random.randint(0, len(messages) - 1)])
+
+
+if __name__ == "__main__":
+    main()
