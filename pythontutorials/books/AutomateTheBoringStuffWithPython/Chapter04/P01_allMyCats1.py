f84b9d02599279ed70c226dbbd02c975663f577b
JoseALermaIII <jose@JoseALerma.com>
2018-12-16 04:51:19 -0600

    Update P01_allMyCats1.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter04/P01_allMyCats1.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter04/P01_allMyCats1.py
@@ -1,16 +1,27 @@
-# This program inefficiently showcases your cats
-print('Enter the name of cat 1: ')
-catName1 = input()
-print('Enter the name of cat 2: ')
-catName2 = input()
-print('Enter the name of cat 3: ')
-catName3 = input()
-print('Enter the name of cat 4: ')
-catName4 = input()
-print('Enter the name of cat 5: ')
-catName5 = input()
-print('Enter the name of cat 6: ')
-catName6 = input()
-print('The cat names are:')
-print(catName1 + ' ' + catName2 + ' ' + catName3 + ' ' + catName4 + ' ' +
-      catName5 + ' ' + catName6)
+"""All my cats 1.0
+
+This program inefficiently showcases your cats by prompting for user input for each cat.
+
+"""
+
+
+def main():
+    print('Enter the name of cat 1: ')
+    catName1 = input()
+    print('Enter the name of cat 2: ')
+    catName2 = input()
+    print('Enter the name of cat 3: ')
+    catName3 = input()
+    print('Enter the name of cat 4: ')
+    catName4 = input()
+    print('Enter the name of cat 5: ')
+    catName5 = input()
+    print('Enter the name of cat 6: ')
+    catName6 = input()
+    print('The cat names are:')
+    print(catName1 + ' ' + catName2 + ' ' + catName3 + ' ' + catName4 + ' ' +
+          catName5 + ' ' + catName6)
+
+
+if __name__ == "__main__":
+    main()
