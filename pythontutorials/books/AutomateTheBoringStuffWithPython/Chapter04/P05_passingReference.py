4c7e59bc74f0b57ddcb02919d1c7773f8e262ed3
JoseALermaIII <jose@JoseALerma.com>
2018-12-16 05:02:02 -0600

    Update P05_passingReference.py added docstrings and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter04/P05_passingReference.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter04/P05_passingReference.py
@@ -1,8 +1,30 @@
-# This program demonstrates how mutable data types are passed to functions
-def eggs(someParameter):
+"""Passing reference
+
+This program demonstrates how mutable data types are passed to functions by using
+the :meth:`eggs` function to append a string to a list of integers.
+
+"""
+
+
+def eggs(someParameter: list) -> None:
+    """eggs
+
+    Appends `'hello'` to a given list.
+
+    Args:
+        someParameter: List to append to.
+
+    Returns:
+        None.
+    """
     someParameter.append('Hello')
 
 
-spam = [1, 2, 3]
-eggs(spam)
-print(spam)
+def main():
+    spam = [1, 2, 3]
+    eggs(spam)
+    print(spam)
+
+
+if __name__ == "__main__":
+    main()
