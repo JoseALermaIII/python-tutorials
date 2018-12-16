69a6a0f016cf0ae798d95b3e04388559318758f4
JoseALermaIII <jose@JoseALerma.com>
2018-12-16 04:55:29 -0600

    Update P03_myPets.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter04/P03_myPets.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter04/P03_myPets.py
@@ -1,8 +1,19 @@
-# This program checks if a given pet is in a list of pets
-myPets = ['Zophie', 'Pooka', 'Fat-tail']
-print('Enter a pet name:')
-name = input()
-if name not in myPets:
-    print('I do not have a pet named ' + name)
-else:
-    print(name + ' is my pet.')
+"""My pets
+
+This program checks if a given pet's name is in a list of pet names.
+
+"""
+
+
+def main():
+    myPets = ['Zophie', 'Pooka', 'Fat-tail']
+    print('Enter a pet name:')
+    name = input()
+    if name not in myPets:
+        print('I do not have a pet named ' + name)
+    else:
+        print(name + ' is my pet.')
+
+
+if __name__ == "__main__":
+    main()
