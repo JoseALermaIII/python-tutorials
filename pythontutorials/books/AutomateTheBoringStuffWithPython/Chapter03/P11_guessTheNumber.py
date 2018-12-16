215c173603119ad93cfa137e2253bc2620791a8b
JoseALermaIII <jose@JoseALerma.com>
2018-12-16 04:19:28 -0600

    Update P11_guessTheNumber.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/P11_guessTheNumber.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/P11_guessTheNumber.py
@@ -1,22 +1,35 @@
-# This is a guess the number game.
-import random
-secretNumber = random.randint(1, 20)
-print('I am thinking of a number between 1 and 20.')
+"""Guess the number
 
-# Ask the player to guess 6 times.
-for guessesTaken in range(1, 7):
-    print('Take a guess.')
-    guess = int(input())
+This is a guess the number game.
 
-    if guess < secretNumber:
-        print('Your guess is too low.')
-    elif guess > secretNumber:
-        print('Your guess is too high.')
+Note:
+    Numbers between 1 and 20.
+
+"""
+
+
+def main():
+    import random
+    secretNumber = random.randint(1, 20)
+    print('I am thinking of a number between 1 and 20.')
+
+    # Ask the player to guess 6 times.
+    for guessesTaken in range(1, 7):
+        print('Take a guess.')
+        guess = int(input())
+
+        if guess < secretNumber:
+            print('Your guess is too low.')
+        elif guess > secretNumber:
+            print('Your guess is too high.')
+        else:
+            break  # This condition is the correct guess!
+
+    if guess == secretNumber:
+        print('Good job! You guessed my number in ' + str(guessesTaken) + ' guesses!')
     else:
-        break  # This condition is the correct guess!
+        print('Nope. The number I was thinking of was ' + str(secretNumber))
 
-if guess == secretNumber:
-    print('Good job! You guessed my number in ' + str(guessesTaken) + ' guesses!')
-else:
-    print('Nope. The number I was thinking of was ' + str(secretNumber))
 
+if __name__ == '__main__':
+    main()
