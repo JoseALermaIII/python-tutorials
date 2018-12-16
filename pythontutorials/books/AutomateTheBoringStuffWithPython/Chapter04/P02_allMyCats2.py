8c8a141e937a927510bbe652d3f42307d17f14e6
JoseALermaIII <jose@JoseALerma.com>
2018-12-16 04:53:55 -0600

    Update P02_allMyCats2.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter04/P02_allMyCats2.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter04/P02_allMyCats2.py
@@ -1,12 +1,25 @@
-# This program efficiently showcases your cats
-catNames = []
-while True:
-    print('Enter the name of cat ' + str(len(catNames) + 1) +
-          ' (Or enter nothing to stop.):')
-    name = input()
-    if name == '':
-        break
-    catNames = catNames + [name]  # list concatenation
-print('The cat names are:')
-for name in catNames:
-    print('  ' + name)
+"""All my cats 2.0
+
+This program efficiently showcases your cats by using a list to store the cat's names
+and a while loop to prompt for them. Then, using a for loop on the list to print them
+all.
+
+"""
+
+
+def main():
+    catNames = []
+    while True:
+        print('Enter the name of cat ' + str(len(catNames) + 1) +
+              ' (Or enter nothing to stop.):')
+        name = input()
+        if name == '':
+            break
+        catNames = catNames + [name]  # list concatenation
+    print('The cat names are:')
+    for name in catNames:
+        print('  ' + name)
+
+
+if __name__ == "__main__":
+    main()
