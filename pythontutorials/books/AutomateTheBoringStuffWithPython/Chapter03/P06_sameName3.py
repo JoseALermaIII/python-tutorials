1f7729f95736526226da23f756336f35843c720f
JoseALermaIII <jose@JoseALerma.com>
2018-12-16 03:31:42 -0600

    Update P06_sameName3.py added docstrings and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/P06_sameName3.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/P06_sameName3.py
@@ -1,17 +1,53 @@
-# This program demonstrates global and local variable rules
-def spam():
+"""Same name 3.0
+
+This program demonstrates global and local variable rules.
+
+Attributes:
+    eggs (int): Integer defining the answer to all life, universe, and everything.
+
+"""
+
+eggs = 42  # this is the global (outside all functions)
+
+
+def spam() -> None:
+    """Spam
+
+    Reassigns the global variable called eggs.
+
+    Returns:
+        None.
+    """
     global eggs
-    eggs = 'spam'  # this is the global (global statement)
+    eggs = 'spam'  #: this is the global (global statement)
 
 
-def bacon():
-    eggs = 'bacon'  # this is a local (assignment)
+def bacon() -> None:
+    """Bacon
+
+    Assigns a local variable called eggs.
+
+    Returns:
+        None.
+    """
+    eggs = 'bacon'  #: this is a local (assignment)
 
 
-def ham():
+def ham() -> None:
+    """Ham
+
+    Prints global variable called eggs.
+
+    Returns:
+        None. Prints global variable, eggs.
+    """
     print(eggs)  # this is the global (no assignment)
 
 
-eggs = 42  # this is the global (outside all functions)
-spam()
-print(eggs)
+def main():
+    spam()
+    print(eggs)
+
+
+if __name__ == '__main__':
+    main()
