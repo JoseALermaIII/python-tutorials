fd1416ee9c702ae621648e137572595af026445a
JoseALermaIII <jose@JoseALerma.com>
2018-12-16 04:38:54 -0600

    Update P01_make_collatz_seq.py added docstrings

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/PracticeProjects/P01_make_collatz_seq.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/PracticeProjects/P01_make_collatz_seq.py
@@ -1,25 +1,45 @@
-# This program makes a Collatz Sequence for a given number
+"""Make Collatz Sequence
 
-# Write a function named collatz() that has one parameter named number.
-# If number is even, then collatz() should print number // 2 and return this value.
-# If number is odd, then collatz() should print and return 3 * number + 1.
+This program makes a `Collatz Sequence`_ for a given number.
 
-# Then write a program that lets the user type in an integer and that keeps calling
-# collatz() on that number until the function returns the value 1.
+Write a function named :meth:`collatz` that has one parameter named number.
+If number is even, then :meth:`collatz` should print `number // 2` and return this value.
+If number is odd, then :meth:`collatz` should print and return `3 * number + 1`.
 
-# Sample output:
-# Enter number:
-# 3
-# 10
-# 5
-# 16
-# 8
-# 4
-# 2
-# 1
+Then write a program that lets the user type in an integer and that keeps calling
+:meth:`collatz` on that number until the function returns the value `1`.
+
+Example:
+    ::
+
+        Enter number:
+        3
+        10
+        5
+        16
+        8
+        4
+        2
+        1
+
+.. _Collatz Sequence:
+    https://en.wikipedia.org/wiki/Collatz_conjecture
+
+"""
 
 
-def collatz(number):
+def collatz(number: int) -> int:
+    """Collatz
+
+    If number is even, then return `number // 2`.
+    If number is odd, then return `3 * number + 1`.
+
+    Args:
+         number: Integer to generate a Collatz conjecture term for.
+
+    Returns:
+        Integer that is either a quotient or a product and sum.
+    """
     if not number % 2:
         return number // 2
     else:
