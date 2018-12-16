47353d81d76e008f6115e1bd8f692aa54765d63c
JoseALermaIII <jose@JoseALerma.com>
2018-12-16 02:42:24 -0600

    Update P02_helloFunc2.py added docstrings and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/P02_helloFunc2.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/P02_helloFunc2.py
@@ -1,7 +1,28 @@
-# This program says hello to Alice and Bob
-def hello(name):
+"""Hello function 2.0
+
+This program uses a function with an input argument to say hello to Alice and Bob.
+
+"""
+
+
+def hello(name: str) -> None:
+    """Hello
+
+    Says hello to a given name.
+
+    Args:
+        name: String containing name of person to say hello to.
+
+    Returns:
+        None. Prints out a statement.
+    """
     print('Hello ' + name)
 
 
-hello('Alice')
-hello('Bob')
+def main():
+    hello('Alice')
+    hello('Bob')
+
+
+if __name__ == '__main__':
+    main()
