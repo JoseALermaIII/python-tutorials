af81e323fa065137963a9beb54c7855ef9ce0c32
JoseALermaIII <jose@JoseALerma.com>
2018-12-16 03:35:28 -0600

    Update P07_sameName4.py added docstrings and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/P07_sameName4.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter03/P07_sameName4.py
@@ -1,8 +1,31 @@
-# This program produces an error
-def spam():
-    print(eggs)  # ERROR!
-    eggs = 'spam local'  # looks ahead and says eggs is local
+"""Same name 4.0
 
+This program produces an error trying to print a
+local global variable while assigning a local variable with the same name.
+
+Attributes:
+    eggs (str): String denoting global variable.
+
+"""
 
 eggs = 'global'
-spam()
+
+
+def spam() -> None:
+    """Spam
+
+    Attempts to print global variable, eggs, while assigning local variable, eggs.
+
+    Returns:
+        None.
+    """
+    print(eggs)  # ERROR!
+    eggs = 'spam local'  #: looks ahead and says eggs is local
+
+
+def main():
+    spam()
+
+
+if __name__ == '__main__':
+    main()
