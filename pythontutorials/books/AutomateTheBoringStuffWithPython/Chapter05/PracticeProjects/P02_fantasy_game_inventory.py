b90fe686287af496a57b51eecba4339416352695
JoseALermaIII <jose@JoseALerma.com>
2018-12-17 23:57:27 -0600

    Update P02_fantasy_game_inventory.py added docstrings, wrapped in main() function, and imported displayInventory()

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter05/PracticeProjects/P02_fantasy_game_inventory.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter05/PracticeProjects/P02_fantasy_game_inventory.py
@@ -1,35 +1,51 @@
-# This program models a player's inventory from a fantasy game with the ability to add
-# to the inventory
-#
-# Imagine that a vanquished dragon’s loot is represented as a list of strings like this:
-# dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-#
-# Write a function named addToInventory(inventory, addedItems), where the inventory
-# parameter is a dictionary representing the player’s inventory (like in the previous
-# project) and the addedItems parameter is a list like dragonLoot. The
-# addToInventory() function should return a dictionary that represents the updated
-# inventory.
-#
-# Note that the addedItems list can contain multiples of the same item.
+"""Fantasy game inventory 2.0
+
+This program models a player's inventory from a fantasy game with the ability to add
+to the inventory.
+
+Imagine that a vanquished dragon’s loot is represented as a :obj:`list` of :obj:`strings <str>`
+like this::
+
+    dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+Write a function named :meth:`addToInventory(inventory, addedItems) <addToInventory>`,
+where the inventory parameter is a :obj:`dictionary <dict>` representing the player’s inventory
+(like in the :py:mod:`previous project <.P01_fantasy_game_inventory>`) and the `addedItems`
+parameter is a :obj:`list` like `dragonLoot`. The :meth:`addToInventory` function should return a
+:obj:`dictionary <dict>` that represents the updated inventory.
+
+Note:
+    The `addedItems` list can contain multiples of the same item.
+"""
 
 
-def displayInventory(inventory):
-    print("Inventory:")
-    item_total = 0
-    for k, v in inventory.items():
-        item_total += v
-        print(str(v) + " " + k)
-    print("Total number of items: " + str(item_total))
+from pythontutorials.books.AutomateTheBoringStuffWithPython.Chapter05.PracticeProjects.P01_fantasy_game_inventory import displayInventory
 
 
-def addToInventory(inventory, addedItems):
+def addToInventory(inventory: dict, addedItems: list) -> dict:
+    """Add to inventory
+
+    Adds given list of items to given dictionary inventory.
+
+    Args:
+        inventory: Dictionary inventory to add items to.
+        addedItems: List of strings to add to inventory.
+
+    Note:
+        If the item already exists in the dictionary, its count is incremented.
+    """
     for item in addedItems:
         inventory.setdefault(item, 0)
         inventory[item] += 1
     return inventory
 
 
-inv = {'gold coin': 42, 'rope': 1}
-dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-inv = addToInventory(inv, dragonLoot)
-displayInventory(inv)
+def main():
+    inv = {'gold coin': 42, 'rope': 1}
+    dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+    inv = addToInventory(inv, dragonLoot)
+    displayInventory(inv)
+
+
+if __name__ == '__main__':
+    main()
