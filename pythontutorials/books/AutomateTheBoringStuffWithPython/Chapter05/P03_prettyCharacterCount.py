88cc3034ae365436bd676a7484062e175d4ec778
JoseALermaIII <jose@JoseALerma.com>
2018-12-17 22:54:23 -0600

    Update P03_prettyCharacterCount.py added docstrings and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter05/P03_prettyCharacterCount.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter05/P03_prettyCharacterCount.py
@@ -1,12 +1,26 @@
-# This program also counts the number of times a character appears in a string
-# but with a prettier output
-import pprint
+"""Pretty character count
 
-message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
-count = {}
+This program also counts the number of times a character appears in a string
+but with a prettier output via the :py:mod:`pprint` module.
 
-for character in message:
-    count.setdefault(character, 0)
-    count[character] += 1
+"""
 
-pprint.pprint(count)
+
+def main():
+    import pprint
+
+    message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+    """str: Message to count characters."""
+
+    count = {}
+    """dict: Characters as keys and counts as values."""
+
+    for character in message:
+        count.setdefault(character, 0)
+        count[character] += 1
+
+    pprint.pprint(count)
+
+
+if __name__ == '__main__':
+    main()
