9ab01105d853e0debb345f1c3b6ed009ddadef76
JoseALermaIII <jose@JoseALerma.com>
2018-12-17 22:40:40 -0600

    Update P02_characterCount.py added docstrings and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter05/P02_characterCount.py
+++ pythontutorials/books/AutomateTheBoringStuffWithPython/Chapter05/P02_characterCount.py
@@ -1,10 +1,23 @@
-# This program counts how often a character appears in a string
+"""Character count
 
-message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
-count = {}
+This program counts how often each character appears in a string.
 
-for character in message:
-    count.setdefault(character, 0)
-    count[character] = count[character] + 1
+"""
 
-print(count)
+
+def main():
+    message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+    """str: Message to count characters."""
+
+    count = {}
+    """dict: Characters as keys and counts as values."""
+    
+    for character in message:
+        count.setdefault(character, 0)
+        count[character] = count[character] + 1
+    
+    print(count)
+
+
+if __name__ == '__main__':
+    main()
