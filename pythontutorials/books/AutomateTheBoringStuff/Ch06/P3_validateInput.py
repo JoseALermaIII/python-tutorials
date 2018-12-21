2345f7a811345343b94dd9dc2818071345ad9708
JoseALermaIII <jose@JoseALerma.com>
2018-12-21 03:42:44 -0600

    Update P3_validateInput.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch06/P3_validateInput.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch06/P3_validateInput.py
@@ -1,14 +1,26 @@
-# This program asks for age and password until they are valid inputs
-while True:
-    print('Enter your age:')
-    age = input()
-    if age.isdecimal():
-        break
-    print('Please enter a number for your age.')
+"""Validate input
 
-while True:
-    print('Select a new password (letters and numbers only):')
-    password = input()
-    if password.isalnum():
-        break
-    print('Passwords can only have letters and numbers.')
+This program asks for numerical age and alphanumeric password
+until they are valid inputs.
+
+"""
+
+
+def main():
+    while True:
+        print('Enter your age:')
+        age = input()
+        if age.isdecimal():
+            break
+        print('Please enter a number for your age.')
+
+    while True:
+        print('Select a new password (letters and numbers only):')
+        password = input()
+        if password.isalnum():
+            break
+        print('Passwords can only have letters and numbers.')
+
+
+if __name__ == '__main__':
+    main()
