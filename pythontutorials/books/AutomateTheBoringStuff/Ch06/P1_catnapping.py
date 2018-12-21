37cf78a1a80b8ed98d0b363c8edb071d589e758f
JoseALermaIII <jose@JoseALerma.com>
2018-12-21 03:37:59 -0600

    Update P1_catnapping.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch06/P1_catnapping.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch06/P1_catnapping.py
@@ -1,7 +1,19 @@
-# This program prints a message for Alice
-print('''Dear Alice,
+"""Cat napping
 
+This program prints a message for Alice using a multi-line
+string with triple quotes.
+
+"""
+
+
+def main():
+    print('''Dear Alice,
+    
 Eve's cat has been arrested for catnapping, cat burglary, and extortion.
-
+    
 Sincerely,
 Bob''')
+
+
+if __name__ == '__main__':
+    main()
