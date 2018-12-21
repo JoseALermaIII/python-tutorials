4d06f4d9acd22341d840a519d8bbfcf7af737ae6
JoseALermaIII <jose@JoseALerma.com>
2018-12-21 03:40:22 -0600

    Update P2_great.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch06/P2_great.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch06/P2_great.py
@@ -1,7 +1,19 @@
-# This program asks how you're doing and responds
-print('How are you?')
-feeling = input()
-if feeling.lower() == 'great':
-    print('I feel great too.')
-else:
-    print('I hope the rest of your day is good.')
+"""Great
+
+This program asks how you're doing and responds by filtering input
+through an if-else statement.
+
+"""
+
+
+def main():
+    print('How are you?')
+    feeling = input()
+    if feeling.lower() == 'great':
+        print('I feel great too.')
+    else:
+        print('I hope the rest of your day is good.')
+
+
+if __name__ == '__main__':
+    main()
