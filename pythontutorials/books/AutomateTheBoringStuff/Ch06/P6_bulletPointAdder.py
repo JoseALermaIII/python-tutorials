da2a25b865b6cf299e8f1289684d70b004ab61ec
JoseALermaIII <jose@JoseALerma.com>
2018-12-22 03:38:50 -0600

    Update P6_bulletPointAdder.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch06/P6_bulletPointAdder.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch06/P6_bulletPointAdder.py
@@ -1,14 +1,26 @@
 #! python3
-# P6_bulletPointAdder.py - Adds Wikipedia bullet points to the start
-# of each line of text on the clipboard
+"""Bullet point adder
 
-import pythontutorials.books.AutomateTheBoringStuff.Ch08.pyperclip as pyperclip
+Adds `Wikipedia style`_ bullet points to the start of each line of text on the clipboard.
 
-text = pyperclip.paste
+.. _Wikipedia style:
+    https://en.wikipedia.org/wiki/Help:List
 
-# Separate lines and add stars.
-lines = text.split('\n')
-for i in range(len(lines)):  # loop through all the indexes in the "lines" list
-    lines[i] = '* ' + lines[i]  # add star to each string in "lines" list
-text = '\n'.join(lines)
-pyperclip.copy(text)
+"""
+
+
+def main():
+    import pythontutorials.books.AutomateTheBoringStuff.Ch08.pyperclip as pyperclip
+
+    text = pyperclip.paste
+
+    # Separate lines and add stars.
+    lines = text.split('\n')
+    for i in range(len(lines)):  # loop through all the indexes in the "lines" list
+        lines[i] = '* ' + lines[i]  # add star to each string in "lines" list
+    text = '\n'.join(lines)
+    pyperclip.copy(text)
+
+
+if __name__ == '__main__':
+    main()
