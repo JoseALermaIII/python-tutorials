751110e59be965d50504f28439e5901c50df429a
JoseALermaIII <jose@JoseALerma.com>
2018-12-24 13:54:23 -0600

    Update P3_zipfile.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch09/P3_zipfile.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch09/P3_zipfile.py
@@ -1,25 +1,39 @@
-# This file manipulates a compressed file
-import zipfile, os
-os.chdir('./')  # move to the folder with example.zip
-exampleZip = zipfile.ZipFile('example.zip')
+"""ZIP file
 
-# Reading ZIP Files
-print(exampleZip.namelist())
-# ['spam.txt', 'cats/', 'cats/catnames.txt', 'cats/zophie.jpg']
-spamInfo = exampleZip.getinfo('spam.txt')
-print(spamInfo.file_size)  # in bytes
-# 25
-print(spamInfo.compress_size)  # in bytes
-# 25
-print('Compressed file is %sx smaller!' % (round(spamInfo.file_size / spamInfo.compress_size, 2)))
-# Compressed file is 1.0x smaller!
+This program manipulates a compressed file using :py:mod:`zipfile` and :py:mod:`os`.
 
-# Extracting from ZIP Files
-#exampleZip.extractall()  # extracts all files to current working directory
-exampleZip.extractall('./testdir')  # auto-creates folder
-exampleZip.close()
+Note:
+    Works with provided ZIP file ``example.zip``.
 
-# Creating and Adding to ZIP Files
-newZip = zipfile.ZipFile('new.zip', 'w')  # use 'a' to append
-newZip.write('./delicious/spam.txt', compress_type=zipfile.ZIP_DEFLATED)
-newZip.close()
+"""
+
+
+def main():
+    import zipfile, os
+    os.chdir('./')  # move to the folder with example.zip
+    exampleZip = zipfile.ZipFile('example.zip')
+
+    # Reading ZIP Files
+    print(exampleZip.namelist())
+    # ['spam.txt', 'cats/', 'cats/catnames.txt', 'cats/zophie.jpg']
+    spamInfo = exampleZip.getinfo('spam.txt')
+    print(spamInfo.file_size)  # in bytes
+    # 25
+    print(spamInfo.compress_size)  # in bytes
+    # 25
+    print('Compressed file is %sx smaller!' % (round(spamInfo.file_size / spamInfo.compress_size, 2)))
+    # Compressed file is 1.0x smaller!
+
+    # Extracting from ZIP Files
+    #exampleZip.extractall()  # extracts all files to current working directory
+    exampleZip.extractall('./testdir')  # auto-creates folder
+    exampleZip.close()
+
+    # Creating and Adding to ZIP Files
+    newZip = zipfile.ZipFile('new.zip', 'w')  # use 'a' to append
+    newZip.write('./delicious/spam.txt', compress_type=zipfile.ZIP_DEFLATED)
+    newZip.close()
+
+
+if __name__ == '__main__':
+    main()
