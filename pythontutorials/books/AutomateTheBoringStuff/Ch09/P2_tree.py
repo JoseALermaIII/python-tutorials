7acc6973c8d262a53a096325bfd359be0ae7822a
JoseALermaIII <jose@JoseALerma.com>
2018-12-24 13:51:21 -0600

    Update P2_tree.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch09/P2_tree.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch09/P2_tree.py
@@ -1,12 +1,26 @@
-# This program walks a directory and displays the contents
-import os
+"""Tree
 
-for folderName, subfolders, filenames in os.walk('./delicious'):
-    print('The current folder is ' + folderName)
+This program walks a directory tree and prints the contents.
 
-    for subfolder in subfolders:
-        print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
-    for filename in filenames:
-        print('FILE INSIDE ' + folderName + ': ' + filename)
+Note:
+    Walks the ``./delicious`` directory.
 
-    print('')
+"""
+
+
+def main():
+    import os
+
+    for folderName, subfolders, filenames in os.walk('./delicious'):
+        print('The current folder is ' + folderName)
+
+        for subfolder in subfolders:
+            print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
+        for filename in filenames:
+            print('FILE INSIDE ' + folderName + ': ' + filename)
+
+        print('')
+
+
+if __name__ == '__main__':
+    main()
