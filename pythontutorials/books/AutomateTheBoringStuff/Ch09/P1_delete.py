6ff68adb443929fdde27116819650c055db78ed5
JoseALermaIII <jose@JoseALerma.com>
2018-12-24 13:49:16 -0600

    Update P1_delete.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch09/P1_delete.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch09/P1_delete.py
@@ -1,9 +1,21 @@
-# This program permanently deletes files ending with a .txt extension
-#
-# Note:
-# - Demonstrates testing with delete functions
-import os
-for filename in os.listdir('./'):
-    if filename.endswith('.txt'):
-        #os.unlink(filename)
-        print(filename)  # DEBUG
+"""Delete
+
+This program permanently deletes files ending with a .txt extension in the current directory
+using :py:mod:`os`.
+
+Note:
+    Demonstrates testing/debugging using comments.
+
+"""
+
+
+def main():
+    import os
+    for filename in os.listdir('./'):
+        if filename.endswith('.txt'):
+            #os.unlink(filename)
+            print(filename)  # DEBUG
+
+
+if __name__ == '__main__':
+    main()
