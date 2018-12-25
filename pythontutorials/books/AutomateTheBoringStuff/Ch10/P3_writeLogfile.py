c54fbd073de69b400e82fdd1ceebba672eb19cae
jose <jose@JoseALerma.com>
2018-12-25 16:03:38 -0600

    Update P3_writeLogfile.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch10/P3_writeLogfile.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch10/P3_writeLogfile.py
@@ -1,11 +1,23 @@
-# This program raises an exception but outputs traceback to a logfile
+"""Write logfile
 
-import traceback
+This program raises an exception but outputs traceback to a logfile.
 
-try:
-    raise Exception('This is the error message.')
-except:
-    errorFile = open('errorInfo.txt', 'w')
-    errorFile.write(traceback.format_exc())
-    errorFile.close()
-    print('The traceback info was written to errorInfo.txt.')
+Note:
+    Default logfile is ``errorInfo.txt``
+"""
+
+
+def main():
+    import traceback
+
+    try:
+        raise Exception('This is the error message.')
+    except Exception:
+        errorFile = open('errorInfo.txt', 'w')
+        errorFile.write(traceback.format_exc())
+        errorFile.close()
+        print('The traceback info was written to errorInfo.txt.')
+
+
+if __name__ == '__main__':
+    main()
