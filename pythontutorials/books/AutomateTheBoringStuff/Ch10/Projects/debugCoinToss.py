d066ea97c80058841531aa43cff40632e028714c
jose <jose@JoseALerma.com>
2018-12-25 17:04:39 -0600

    Update debugCoinToss.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch10/Projects/debugCoinToss.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch10/Projects/debugCoinToss.py
@@ -1,20 +1,30 @@
-# This program is a simple coin toss guessing game. The player gets two guesses.
+"""Debug coin toss
 
-import random
-guess = ''
-options = ("tails", "heads")
-while guess not in options:
-    print('Guess the coin toss! Enter heads or tails:')
-    guess = input().lower()
+This program is a simple coin toss guessing game. The player gets two guesses.
 
-toss = options[random.randint(0, 1)]  # 0 is tails, 1 is heads
+"""
 
-if toss == guess:
-    print('You got it!')
-else:
-    print('Nope! Guess again!')
-    guess = input().lower()
+
+def main():
+    import random
+    guess = ''
+    options = ("tails", "heads")
+    while guess not in options:
+        print('Guess the coin toss! Enter heads or tails:')
+        guess = input().lower()
+
+    toss = options[random.randint(0, 1)]  # 0 is tails, 1 is heads
+
     if toss == guess:
         print('You got it!')
     else:
-        print('Nope. You are really bad at this game.')
+        print('Nope! Guess again!')
+        guess = input().lower()
+        if toss == guess:
+            print('You got it!')
+        else:
+            print('Nope. You are really bad at this game.')
+
+
+if __name__ == '__main__':
+    main()
