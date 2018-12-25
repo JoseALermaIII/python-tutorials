9ac360af78d0d3c4808ee18dc16590d8ac56888f
jose <jose@JoseALerma.com>
2018-12-25 16:08:00 -0600

    Update P4_podBayDoor.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch10/P4_podBayDoor.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch10/P4_podBayDoor.py
@@ -1,7 +1,18 @@
-# This program raises an AssertionError
-# Note: correction submitted for line 6
+"""Pod Bay Door
 
-podBayDoorStatus = "open"
-assert podBayDoorStatus == "open", "The pod bay doors need to be 'open'."
-podBayDoorStatus = 'I\'m sorry, Dave. I\'m afraid I can\'t do that.'
-assert podBayDoorStatus == "open", "The pod bay doors need to be 'open'."
+This program raises an AssertionError.
+
+Note:
+    Correction submitted for line 13
+"""
+
+
+def main():
+    podBayDoorStatus = "open"
+    assert podBayDoorStatus == "open", "The pod bay doors need to be 'open'."
+    podBayDoorStatus = 'I\'m sorry, Dave. I\'m afraid I can\'t do that.'
+    assert podBayDoorStatus == "open", "The pod bay doors need to be 'open'."
+
+
+if __name__ == '__main__':
+    main()
