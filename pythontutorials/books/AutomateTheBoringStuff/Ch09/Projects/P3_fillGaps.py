da45854449c51febf48bceebe611a99ab103709f
JoseALermaIII <jose@JoseALerma.com>
2018-12-25 03:50:10 -0600

    Update P3_fillGaps.py added docstrings and removed extra return statements

--- pythontutorials/books/AutomateTheBoringStuff/Ch09/Projects/P3_fillGaps.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch09/Projects/P3_fillGaps.py
@@ -1,8 +1,15 @@
-# Write a program that finds all files with a given prefix, such as spam001.txt,
-# spam002.txt, and so on, in a single folder and locates any gaps in the numbering
-# (such as if there is a spam001.txt and spam003.txt but no spam002.txt).
-#
-# Have the program rename all the later files to close this gap.
+"""P3_fillGaps.py
+
+Write a program that finds all files with a given prefix, such as spam001.txt,
+spam002.txt, and so on, in a single folder and locates any gaps in the numbering
+(such as if there is a spam001.txt and spam003.txt but no spam002.txt).
+
+Have the program rename all the later files to close this gap.
+
+Attributes:
+    seqRegex (re.compile): Regular expression object used to group numbers in
+        filename.
+"""
 
 import os, re
 
@@ -13,11 +20,34 @@ seqRegex = re.compile(r"""
                       """, re.VERBOSE)
 
 
-def is_sequence(numberlist):
+def is_sequence(numberlist: list) -> bool:
+    """Is sequence
+
+    Can take a list returned by :meth:`get_numbers` and determine if
+    it is a sequence based on the property
+    `list_length == (last_element - first_element + 1)`.
+
+    Args:
+        numberlist: List containing integers to check for a sequence.
+
+    Returns:
+        True if list contains a sequence of numbers, False otherwise.
+    """
     return len(numberlist) == (numberlist[-1] - numberlist[0] + 1)
 
 
-def get_gap(numberlist):
+def get_gap(numberlist: list):
+    """Get gap
+
+    Can take an integer list returned by :meth:`get_numbers` and determine
+    the missing sequence number.
+
+    Args:
+        numberlist: List of numbers to find a gap in.
+
+    Returns:
+        Missing number in the sequence or None if there is no gap.
+    """
     seqlist = list(range(numberlist[0], numberlist[-1] + 1))
     for element in seqlist:
         if element not in numberlist:
@@ -25,7 +55,24 @@ def get_gap(numberlist):
     return None
 
 
-def get_filenames(folder):
+def get_filenames(folder: str) -> list:
+    """Get file names
+
+    Called by :meth:`fill_gaps` to makes a list of all numbered
+    file names in a given folder.
+
+    Breaks each file into the prefix (before numbering), numbering,
+    and suffix (after numbering).
+
+    Args:
+        folder: String containing folder path to get file names of.
+            Relative paths are okay.
+
+    Returns:
+        List of lists of strings with segmented names of files in
+        alphanumerical order.
+    """
+    folder = os.path.abspath(folder)
     files = []
     for foldername, subfolders, filenames in os.walk(folder):
         filenames.sort()
@@ -45,14 +92,40 @@ def get_filenames(folder):
     return files
 
 
-def get_numbers(files):
+def get_numbers(files: list) -> list:
+    """Get numbers
+
+    Can take a list returned by :meth:`get_filenames` and make
+    an integer list of the numerical parts of the file names.
+
+    Args:
+        files: List of segmented file names.
+
+    Returns:
+        List of integers from file names in numerical order.
+    """
     numlist = []
     for file in files:
         numlist.append(int(file[1]))
     return numlist
 
 
-def fill_gaps(folder):
+def fill_gaps(folder: str) -> None:
+    """Fill gaps
+
+    Fills gaps in file name numbering of a given folder.
+
+    Args:
+        folder: String containing path of folder to fill filename gaps.
+            Relative paths are okay.
+
+    Returns:
+        None. Prints applicable error message and renames files.
+
+    Note:
+        Running in debug mode. Files to be renamed are printed. Uncomment
+        after testing to rename files.
+    """
     files = get_filenames(folder)
 
     # Get list of numbers used in file names
@@ -85,12 +158,9 @@ def fill_gaps(folder):
             print("Renaming '%s' to '%s'..." % (oldfilename, newfilename))  # DEBUG
             #os.rename(oldfilepath, newfilepath)  # Uncomment after testing
 
-    return None
-
 
 def main():
     fill_gaps("./testdir")
-    return None
 
 
 # If P3_fillGaps.py is run instead of imported, run main():
