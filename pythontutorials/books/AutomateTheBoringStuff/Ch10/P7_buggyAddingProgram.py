95ac2b26e96331ceeeb7eab07054deef95b62065
jose <jose@JoseALerma.com>
2018-12-25 16:48:48 -0600

    Update P7_buggyAddingProgram.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch10/P7_buggyAddingProgram.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch10/P7_buggyAddingProgram.py
@@ -1,9 +1,21 @@
-# This program adds three numbers and displays the sum
+"""Buggy adding program
 
-print("Enter the first number to add:")
-first = input()
-print("Enter the second number to add:")
-second = input()
-print("Enter the third number to add:")
-third = input()
-print("The sum is " + first + second + third)
+This program adds three user inputted numbers and displays the sum.
+
+Note:
+    May not work as expected.
+"""
+
+
+def main():
+    print("Enter the first number to add:")
+    first = input()
+    print("Enter the second number to add:")
+    second = input()
+    print("Enter the third number to add:")
+    third = input()
+    print("The sum is " + first + second + third)
+
+
+if __name__ == '__main__':
+    main()
