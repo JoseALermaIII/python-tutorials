cbc2db7201684402d1f7e1bf0be579dccd3d5b25
JoseALermaIII <jose@JoseALerma.com>
2018-12-25 04:40:29 -0600

    Update P4_makeGaps.py added docstrings and removed extra return statements

--- pythontutorials/books/AutomateTheBoringStuff/Ch09/Projects/P4_makeGaps.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch09/Projects/P4_makeGaps.py
@@ -1,11 +1,17 @@
-# Write a program that finds all files with a given prefix, such as spam001.txt,
-# spam002.txt, and so on, in a single folder and locates any gaps in the numbering
-# (such as if there is a spam001.txt and spam003.txt but no spam002.txt).
-#
-# Have the program rename all the later files to close this gap.
-#
-# As an added challenge, write another program that can insert gaps into numbered
-# files so that a new file can be added.
+"""P4_makeGaps.py
+
+Write a program that finds all files with a given prefix, such as spam001.txt,
+spam002.txt, and so on, in a single folder and locates any gaps in the numbering
+(such as if there is a spam001.txt and spam003.txt but no spam002.txt).
+
+Have the program rename all the later files to close this gap.
+
+As an added challenge, write another program that can insert gaps into numbered
+files so that a new file can be added.
+
+Note:
+    By default, uses not provided `./testdir` and a gap of `2`.
+"""
 
 from pythontutorials.books.AutomateTheBoringStuff.Ch09.Projects.P3_fillGaps \
     import is_sequence, get_filenames, get_numbers
@@ -13,7 +19,31 @@ from pythontutorials.books.AutomateTheBoringStuff.Ch09.Projects.P3_fillGaps \
 import os
 
 
-def make_gaps(folder, gap):
+def make_gaps(folder: str, gap: int) -> None:
+    """Make gaps
+
+    Makes given integer a gap in a sequence of numerical file names from a
+    given folder.
+
+    Args:
+        folder: String containing path to folder to make gaps in file names.
+            Relative paths are okay.
+        gap: Integer number to make a gap.
+
+    Returns:
+        None. Prints applicable error message and renames files.
+
+    Example:
+        If folder `test` has files `text1.txt`, `text2.txt`, and `text3.txt`. Then,
+
+        >>> make_gaps('test', 2)
+
+        would change the file names to `text1.txt`, `text3.txt`, and `text4.txt`.
+
+    Note:
+        Running in debug mode. Files to be renamed are printed. Uncomment
+        after testing to rename files.
+    """
     # Get names of all files
     files = get_filenames(folder)
 
@@ -44,12 +74,9 @@ def make_gaps(folder, gap):
             print("Renaming '%s' to '%s'..." % (oldfilename, newfilename))  # DEBUG
             #os.rename(oldfilepath, newfilepath)  # Uncomment after testing
 
-    return None
-
 
 def main():
     make_gaps("./testdir", 2)
-    return None
 
 
 # If P4_makeGaps.py is run instead of imported, run main():
