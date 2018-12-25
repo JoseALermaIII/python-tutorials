f73a00172a9d99efa797e2774a0cbd0f60d0a69f
jose <jose@JoseALerma.com>
2018-12-25 16:45:57 -0600

    Update P6_factorialLog.py added docstrings and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch10/P6_factorialLog.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch10/P6_factorialLog.py
@@ -1,4 +1,8 @@
-# This program calculates factorial and logs debug messages
+"""Factorial log
+
+This program calculates factorial and logs debug messages.
+
+"""
 
 import logging
 #logging.basicConfig(level=logging.DEBUG, format=" %(asctime)s - %(levelname)s - %(message)s")  # log to terminal
@@ -8,7 +12,17 @@ logging.disable(logging.CRITICAL)  # Stop logging, comment out to debug
 logging.debug("Start of program")
 
 
-def factorial(n):
+def factorial(n: int) -> int:
+    """Factorial
+
+    Calculates factorial of given number.
+
+    Args:
+        n: Integer number to calculate factorial.
+
+    Returns:
+        Factorial of given number.
+    """
     logging.debug("Start of factorial(%s%%)" % n)
     total = 1
     for i in range(1, n + 1):
@@ -18,5 +32,10 @@ def factorial(n):
     return total
 
 
-print(factorial(5))
-logging.debug("End of program")
+def main():
+    print(factorial(5))
+    logging.debug("End of program")
+
+
+if __name__ == '__main__':
+    main()
