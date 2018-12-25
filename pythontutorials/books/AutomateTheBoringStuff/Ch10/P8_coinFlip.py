bf5d0a9700382835a0e543622f327431b752f539
jose <jose@JoseALerma.com>
2018-12-25 16:54:26 -0600

    Update P8_coinFlip.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch10/P8_coinFlip.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch10/P8_coinFlip.py
@@ -1,10 +1,20 @@
-# This program simulates flipping a coin 1000 times
+"""Coin flip
 
-import random
-heads = 0
-for i in range(1, 1001):
-    if random.randint(0, 1) == 1:
-        heads += 1
-    if i == 500:
-        print("Halfway done!")
-print("Heads came up " + str(heads) + " times.")
+This program simulates flipping a coin 1000 times and prints the number
+of times it landed on heads.
+"""
+
+
+def main():
+    import random
+    heads = 0
+    for i in range(1, 1001):
+        if random.randint(0, 1) == 1:
+            heads += 1
+        if i == 500:
+            print("Halfway done!")
+    print("Heads came up " + str(heads) + " times.")
+
+
+if __name__ == '__main__':
+    main()
