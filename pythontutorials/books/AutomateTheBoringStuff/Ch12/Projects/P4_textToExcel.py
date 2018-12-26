84b197c8c43a7d88196f0a3f1f8ebc2664d12bcd
jose <jose@JoseALerma.com>
2018-12-25 22:19:19 -0600

    Update P4_textToExcel.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch12/Projects/P4_textToExcel.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch12/Projects/P4_textToExcel.py
@@ -1,30 +1,40 @@
-# Write a program to read in the contents of several text files (you can make
-# the text files yourself) and insert those contents into a spreadsheet, with
-# one line of text per row. The lines of the first text file will be in the
-# cells of column A, the lines of the second text file will be in the cells of
-# column B, and so on.
+"""Text to Excel
 
-import openpyxl
-import os
+Write a program to read in the contents of several text files (you can make
+the text files yourself) and insert those contents into a spreadsheet, with
+one line of text per row. The lines of the first text file will be in the
+cells of column A, the lines of the second text file will be in the cells of
+column B, and so on.
 
-FOLDER = "./p4files/"
+"""
 
-# Open workbook
-wb = openpyxl.Workbook()
-sheet = wb.active
 
-# Get list of files
-filelist = os.listdir(FOLDER)
-filelist.sort()
+def main():
+    import openpyxl
+    import os
 
-# Open file
-for file in filelist:
-    with open(FOLDER + file) as fileObj:
-        index = 1
-        for line in fileObj:
-            # Transpose line into relevant workbook column
-            sheet.cell(row=index, column=(filelist.index(file) + 1)).value = line.strip()
-            index += 1
+    FOLDER = "./p4files/"
 
-# Save workbook
-wb.save("textToExcel.xlsx")
+    # Open workbook
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+
+    # Get list of files
+    filelist = os.listdir(FOLDER)
+    filelist.sort()
+
+    # Open file
+    for file in filelist:
+        with open(FOLDER + file) as fileObj:
+            index = 1
+            for line in fileObj:
+                # Transpose line into relevant workbook column
+                sheet.cell(row=index, column=(filelist.index(file) + 1)).value = line.strip()
+                index += 1
+
+    # Save workbook
+    wb.save("textToExcel.xlsx")
+
+
+if __name__ == '__main__':
+    main()
