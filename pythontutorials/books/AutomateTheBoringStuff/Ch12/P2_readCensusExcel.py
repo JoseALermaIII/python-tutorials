c874101a4bd13299531a8d4316474349f0f565d7
jose <jose@JoseALerma.com>
2018-12-25 20:59:52 -0600

    Update P2_readCensusExcel.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch12/P2_readCensusExcel.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch12/P2_readCensusExcel.py
@@ -1,38 +1,46 @@
 #! python3
-# P2_readCensusExcel.py - Tabulates population and number of census tracts for
-# each county.
-#
-# Note:
-# - The censuspopdata.xlsx workbook can be downloaded from
-#   https://nostarch.com/automatestuff/
+"""Read census Excel
 
-import openpyxl, pprint
-print("Opening workbook...")
-wb = openpyxl.load_workbook("censuspopdata.xlsx")
-sheet = wb["Population by Census Tract"]
-countyData = {}
+Tabulates population and number of census tracts for each county.
 
-# Fill in countyData with each county's population and tracts.
-print("Reading rows...")
-for row in range(2, sheet.max_row + 1):
-    # Each row in the spreadsheet has data for one census tract.
-    state = sheet['B' + str(row)].value
-    county = sheet['C' + str(row)].value
-    pop = sheet['D' + str(row)].value
+Note:
+    * The censuspopdata.xlsx workbook can be downloaded from https://nostarch.com/automatestuff/
 
-    # Make sure the key for this state exists.
-    countyData.setdefault(state, {})
-    # Make sure the key for this county in this state exists.
-    countyData[state].setdefault(county, {"tracts": 0, "pop": 0})
+"""
 
-    # Each row represents one census tract, so increment by one.
-    countyData[state][county]["tracts"] += 1
-    # Increase the county pop by the pop in this census tract.
-    countyData[state][county]["pop"] += int(pop)
 
-# Open a new text file and write the contents of countyData to it.
-print("Writing results...")
-resultFile = open("census2010.py", 'w')
-resultFile.write("allData = " + pprint.pformat(countyData))
-resultFile.close()
-print("Done.")
+def main():
+    import openpyxl, pprint
+    print("Opening workbook...")
+    wb = openpyxl.load_workbook("censuspopdata.xlsx")
+    sheet = wb["Population by Census Tract"]
+    countyData = {}
+
+    # Fill in countyData with each county's population and tracts.
+    print("Reading rows...")
+    for row in range(2, sheet.max_row + 1):
+        # Each row in the spreadsheet has data for one census tract.
+        state = sheet['B' + str(row)].value
+        county = sheet['C' + str(row)].value
+        pop = sheet['D' + str(row)].value
+
+        # Make sure the key for this state exists.
+        countyData.setdefault(state, {})
+        # Make sure the key for this county in this state exists.
+        countyData[state].setdefault(county, {"tracts": 0, "pop": 0})
+
+        # Each row represents one census tract, so increment by one.
+        countyData[state][county]["tracts"] += 1
+        # Increase the county pop by the pop in this census tract.
+        countyData[state][county]["pop"] += int(pop)
+
+    # Open a new text file and write the contents of countyData to it.
+    print("Writing results...")
+    resultFile = open("census2010.py", 'w')
+    resultFile.write("allData = " + pprint.pformat(countyData))
+    resultFile.close()
+    print("Done.")
+
+
+if __name__ == '__main__':
+    main()
