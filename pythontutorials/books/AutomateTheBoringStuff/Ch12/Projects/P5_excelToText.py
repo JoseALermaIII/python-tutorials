363249a64e8f29a56e6f52121ac10c799a192eb1
jose <jose@JoseALerma.com>
2018-12-25 22:23:45 -0600

    Update P5_excelToText.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch12/Projects/P5_excelToText.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch12/Projects/P5_excelToText.py
@@ -1,24 +1,34 @@
-# Write a program that performs the tasks of the previous program in reverse
-# order: The program should open a spreadsheet and write the cells of column A
-# into one text file, the cells of column B into another text file, and so on.
+"""Excel to test
 
-import openpyxl
-import os
+Write a program that performs the tasks of the previous program in reverse
+order: The program should open a spreadsheet and write the cells of column A
+into one text file, the cells of column B into another text file, and so on.
 
-# Make directory
-FOLDER = "./p5files/"
-os.makedirs(FOLDER, exist_ok=True)
+"""
 
-# Open workbook
-wb = openpyxl.load_workbook("example.xlsx")
-sheet = wb.active
 
-# Open file
-index = 1
-while index <= sheet.max_column:
-    fileObj = open(FOLDER + "text" + str(index) + ".txt", 'w')
-    for rowNum in range(1, sheet.max_row + 1):
-        # Transpose column into relevant text file
-        fileObj.write(str(sheet.cell(row=rowNum, column=index).value) + '\n')
-    index += 1
-    fileObj.close()
+def main():
+    import openpyxl
+    import os
+
+    # Make directory
+    FOLDER = "./p5files/"
+    os.makedirs(FOLDER, exist_ok=True)
+
+    # Open workbook
+    wb = openpyxl.load_workbook("example.xlsx")
+    sheet = wb.active
+
+    # Open file
+    index = 1
+    while index <= sheet.max_column:
+        fileObj = open(FOLDER + "text" + str(index) + ".txt", 'w')
+        for rowNum in range(1, sheet.max_row + 1):
+            # Transpose column into relevant text file
+            fileObj.write(str(sheet.cell(row=rowNum, column=index).value) + '\n')
+        index += 1
+        fileObj.close()
+
+
+if __name__ == '__main__':
+    main()
