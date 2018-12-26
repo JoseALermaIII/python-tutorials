84298c790d53ef1c4750e0e06d40a8d8bcba71ed
jose <jose@JoseALerma.com>
2018-12-25 23:47:42 -0600

    Update P5_readDocx.py added docstrings

--- pythontutorials/books/AutomateTheBoringStuff/Ch13/P5_readDocx.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch13/P5_readDocx.py
@@ -1,14 +1,26 @@
 #! python3
-# P5_readDocx.py - Accepts a filename of a .docx file and returns a single
-# string value of its text.
-#
-# Note:
-# - Example .docx files can be downloaded from http://nostarch.com/automatestuff/
+"""Read docx
+
+Accepts a filename of a .docx file and returns a single string value of its text.
+
+Note:
+    * Example .docx files can be downloaded from http://nostarch.com/automatestuff/
+"""
 
 import docx
 
 
-def getText(filename):
+def getText(filename: str) -> str:
+    """Get text
+
+    Gets text from a given .docx file.
+
+    Args:
+         filename: Path to .docx file to get text from.
+
+    Returns:
+        String with all document text.
+    """
     doc = docx.Document(filename)
     fullText = []
     for para in doc.paragraphs:
