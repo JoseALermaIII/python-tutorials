3778ba8635c47d02e8b3801da7c844b68d391883
jose <jose@JoseALerma.com>
2018-12-25 22:38:12 -0600

    Update P1_readPDF.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch13/P1_readPDF.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch13/P1_readPDF.py
@@ -1,28 +1,39 @@
-# This program manipulates PDF files
-# Note:
-# - Example PDFs can be downloaded from http://nostarch.com/automatestuff/
-# - Book uses PyPDF2; I'm an overachiever that uses PyPDF4
+"""Read PDF
 
-import PyPDF4
-from PyPDF4.utils import PdfReadError
+This program reads PDF files.
 
-# Extracting Text from PDFs
-pdfFileObj = open("meetingminutes.pdf", "rb")
-pdfReader = PyPDF4.PdfFileReader(pdfFileObj)
-print(pdfReader.numPages)
+Note:
+    * Example PDFs can be downloaded from http://nostarch.com/automatestuff/
+    * Book uses PyPDF2; I'm an overachiever that uses PyPDF4
 
-pageObj = pdfReader.getPage(0)
-print(pageObj.extractText())
+"""
 
-# Decrypting PDFs
-pdfReader = PyPDF4.PdfFileReader(open("encrypted.pdf", "rb"))
-print(pdfReader.isEncrypted)
 
-try:
-    print(pdfReader.getPage(0))
-except PdfReadError as err:
-    print("PdfReadError: %s" % err)
+def main():
+    import PyPDF4
+    from PyPDF4.utils import PdfReadError
 
-pdfReader = PyPDF4.PdfFileReader(open("encrypted.pdf", "rb"))
-print(pdfReader.decrypt("rosebud"))
-pageObj = pdfReader.getPage(0)
+    # Extracting Text from PDFs
+    pdfFileObj = open("meetingminutes.pdf", "rb")
+    pdfReader = PyPDF4.PdfFileReader(pdfFileObj)
+    print(pdfReader.numPages)
+
+    pageObj = pdfReader.getPage(0)
+    print(pageObj.extractText())
+
+    # Decrypting PDFs
+    pdfReader = PyPDF4.PdfFileReader(open("encrypted.pdf", "rb"))
+    print(pdfReader.isEncrypted)
+
+    try:
+        print(pdfReader.getPage(0))
+    except PdfReadError as err:
+        print("PdfReadError: %s" % err)
+
+    pdfReader = PyPDF4.PdfFileReader(open("encrypted.pdf", "rb"))
+    print(pdfReader.decrypt("rosebud"))
+    pageObj = pdfReader.getPage(0)
+
+
+if __name__ == '__main__':
+    main()
