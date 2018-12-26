3eb6a67ea0d8ac59b267ec3d83f64cf1dd79f6ee
jose <jose@JoseALerma.com>
2018-12-25 19:35:11 -0600

    Update P1_commandLineEmailer.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch11/Projects/P1_commandLineEmailer.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch11/Projects/P1_commandLineEmailer.py
@@ -1,55 +1,65 @@
-# Write a program that takes an email address and string of text on the command line
-# and then, using Selenium, logs into your email account and sends an email of the
-# string to the provided address.
-# (You might want to set up a separate email account for this program.)
-#
-# Note:
-# - "string of text" must be within quotes, like shown
-# - "new" gmail layout is used with keyboard shortcuts enabled
-# - not fully tested because account could end up banned for going against TOS
+"""Command line emailer
 
-import sys
-from selenium import webdriver, common
-from selenium.webdriver.common.keys import Keys
+Write a program that takes an email address and string of text on the command line
+and then, using Selenium, logs into your email account and sends an email of the
+string to the provided address.
+(You might want to set up a separate email account for this program.)
 
-# Get arguments from command line
-email = ''.join(sys.argv[1])
-message = ''.join(sys.argv[2])
+Note:
+    * "string of text" must be within quotes, like shown
+    * "new" gmail layout is used with keyboard shortcuts enabled
+    * not fully tested because account could end up banned for going against TOS
 
-# Open browser to email account
-browser = webdriver.Firefox()
-browser.get("https://www.gmail.com/")
+"""
 
-# Login to e-mail account
-try:
-    emailElem = browser.find_element_by_id("identifierId")
-    emailElem.send_keys("not_my_real_email")
-    emailElem.submit()
 
-    passwordElem = browser.find_element_by_name("password")
-    passwordElem.send_keys("not_my_real_password")
-    passwordElem.submit()
-except common.exceptions.NoSuchElementException as err:
-    print("Unable to locate element: %s" % err)
+def main():
+    import sys
+    from selenium import webdriver, common
+    from selenium.webdriver.common.keys import Keys
 
-# Open new message and fill To and Message fields
-try:
-    buttonElem = browser.find_element_by_link_text("Compose")
-    buttonElem.submit()
+    # Get arguments from command line
+    email = ''.join(sys.argv[1])
+    message = ''.join(sys.argv[2])
 
-    toElem = browser.find_element_by_name("to")
-    toElem.send_keys(email)
+    # Open browser to email account
+    browser = webdriver.Firefox()
+    browser.get("https://www.gmail.com/")
 
-    subjectElem = browser.find_element_by_name("subjectbox")
-    subjectElem.send_keys("Auto Generated Message")
+    # Login to e-mail account
+    try:
+        emailElem = browser.find_element_by_id("identifierId")
+        emailElem.send_keys("not_my_real_email")
+        emailElem.submit()
 
-    messageElem = browser.find_element_by_id(":ia")
-    messageElem.send_keys(message)
+        passwordElem = browser.find_element_by_name("password")
+        passwordElem.send_keys("not_my_real_password")
+        passwordElem.submit()
+    except common.exceptions.NoSuchElementException as err:
+        print("Unable to locate element: %s" % err)
 
-    # Send e-mail with keyboard shortcut
-    subjectElem.send_keys(Keys.LEFT_CONTROL + Keys.ENTER)
-except common.exceptions.NoSuchElementException as err:
-    print("Unable to locate element: %s" % err)
+    # Open new message and fill To and Message fields
+    try:
+        buttonElem = browser.find_element_by_link_text("Compose")
+        buttonElem.submit()
 
-# Close browser
-browser.quit()
+        toElem = browser.find_element_by_name("to")
+        toElem.send_keys(email)
+
+        subjectElem = browser.find_element_by_name("subjectbox")
+        subjectElem.send_keys("Auto Generated Message")
+
+        messageElem = browser.find_element_by_id(":ia")
+        messageElem.send_keys(message)
+
+        # Send e-mail with keyboard shortcut
+        subjectElem.send_keys(Keys.LEFT_CONTROL + Keys.ENTER)
+    except common.exceptions.NoSuchElementException as err:
+        print("Unable to locate element: %s" % err)
+
+    # Close browser
+    browser.quit()
+
+
+if __name__ == '__main__':
+    main()
