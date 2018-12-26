ff164e8c0c83b41e402b6d97feec38c13837864a
jose <jose@JoseALerma.com>
2018-12-25 18:30:01 -0600

    Update P1_mapIt.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch11/P1_mapIt.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch11/P1_mapIt.py
@@ -1,15 +1,24 @@
 #! python3
-# mapIt.py - Launches a map in the browser using an address from the
-# command line or clipboard.
+"""Map it
 
-import webbrowser, sys
-from pyperclip import paste
+Launches a map in the browser using an address from the command line or clipboard.
 
-if len(sys.argv) > 1:
-    # Get address from command line.
-    address = ' '.join(sys.argv[1:])
-else:
-    # Get address from clipboard.
-    address = paste()
+"""
 
-webbrowser.open("https://www.google.com/maps/place/" + address)
+
+def main():
+    import webbrowser, sys
+    from pyperclip import paste
+
+    if len(sys.argv) > 1:
+        # Get address from command line.
+        address = ' '.join(sys.argv[1:])
+    else:
+        # Get address from clipboard.
+        address = paste()
+
+    webbrowser.open("https://www.google.com/maps/place/" + address)
+
+
+if __name__ == '__main__':
+    main()
