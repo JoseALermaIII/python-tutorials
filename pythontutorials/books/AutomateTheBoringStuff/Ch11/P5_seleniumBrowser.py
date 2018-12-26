3bfcba67df896945e21db781803bd649e79f81f2
jose <jose@JoseALerma.com>
2018-12-25 19:53:03 -0600

    Update P5_seleniumBrowser.py fixed sphinx html docstring formatting

--- pythontutorials/books/AutomateTheBoringStuff/Ch11/P5_seleniumBrowser.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch11/P5_seleniumBrowser.py
@@ -4,6 +4,7 @@ This program uses selenium to parse and interact with websites
 
 Note:
     * geckodriver is needed for Linux
+
         * download from https://github.com/mozilla/geckodriver/releases
         * place in /usr/local/bin
         * more info https://github.com/SeleniumHQ/selenium/blob/master/py/docs/source/index.rst
