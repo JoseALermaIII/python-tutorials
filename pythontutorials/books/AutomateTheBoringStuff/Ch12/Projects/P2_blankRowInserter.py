faf280c1fc857dea4b9d06ae71749b0bf16bed81
jose <jose@JoseALerma.com>
2018-12-25 21:59:11 -0600

    Update P2_blankRowInserter.py added docstring and wrapped in main() function

--- pythontutorials/books/AutomateTheBoringStuff/Ch12/Projects/P2_blankRowInserter.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch12/Projects/P2_blankRowInserter.py
@@ -1,31 +1,41 @@
-# Create a program blankRowInserter.py that takes two integers and a filename
-# string as command line arguments. Let’s call the first integer N and the second
-# integer M. Starting at row N, the program should insert M blank rows into the
-# spreadsheet.
+"""Blank row inserter
 
-import sys
-import openpyxl
+Create a program blankRowInserter.py that takes two integers and a filename
+string as command line arguments. Let’s call the first integer N and the second
+integer M. Starting at row N, the program should insert M blank rows into the
+spreadsheet.
 
-# Get arguments from commandline
-N = int(''.join(sys.argv[1]))
-M = int(''.join(sys.argv[2]))
-file = ''.join(sys.argv[3])
+"""
 
-# Open workbooks
-readwb = openpyxl.load_workbook(file)
-readsheet = readwb.active
 
-writewb = openpyxl.Workbook()
-writesheet = writewb.active
+def main():
+    import sys
+    import openpyxl
 
-# Read readwb and transpose into writewb
-for rowNum in range(1, readsheet.max_row + 1):
-    for colNum in range(1, readsheet.max_column + 1):
-            if rowNum < N:
-                writesheet.cell(row=rowNum, column=colNum).value = readsheet.cell(row=rowNum, column=colNum).value
-            else:
-                # Insert blank lines
-                writesheet.cell(row=(rowNum + M), column=colNum).value = readsheet.cell(row=rowNum, column=colNum).value
+    # Get arguments from commandline
+    N = int(''.join(sys.argv[1]))
+    M = int(''.join(sys.argv[2]))
+    file = ''.join(sys.argv[3])
 
-# Save workbook
-writewb.save("updated" + file)
+    # Open workbooks
+    readwb = openpyxl.load_workbook(file)
+    readsheet = readwb.active
+
+    writewb = openpyxl.Workbook()
+    writesheet = writewb.active
+
+    # Read readwb and transpose into writewb
+    for rowNum in range(1, readsheet.max_row + 1):
+        for colNum in range(1, readsheet.max_column + 1):
+                if rowNum < N:
+                    writesheet.cell(row=rowNum, column=colNum).value = readsheet.cell(row=rowNum, column=colNum).value
+                else:
+                    # Insert blank lines
+                    writesheet.cell(row=(rowNum + M), column=colNum).value = readsheet.cell(row=rowNum, column=colNum).value
+
+    # Save workbook
+    writewb.save("updated" + file)
+
+
+if __name__ == '__main__':
+    main()
