24706778d9d8f2907450460d356ebfe5f1588196
JoseALermaIII <jose@JoseALerma.com>
2018-12-28 03:54:40 -0600

    Update P1_readCSV.py added docstring and wrapped in main function

--- pythontutorials/books/AutomateTheBoringStuff/Ch14/P1_readCSV.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch14/P1_readCSV.py
@@ -1,21 +1,34 @@
-# This program uses the csv module to manipulate .csv files
+"""Read CSV
 
-import csv
+This program uses :py:mod:`csv` to read .csv files.
 
-# Reader Objects
-exampleFile = open("example.csv")
-exampleReader = csv.reader(exampleFile)
-exampleData = list(exampleReader)
-print(exampleData)
+Note:
+    Uses provided ``example.csv`` file.
 
-print(exampleData[0][0])
-print(exampleData[0][1])
-print(exampleData[0][2])
-print(exampleData[1][1])
-print(exampleData[6][1])
+"""
 
-# Reading Data from Reader Objects in a for Loop
-exampleFile = open("example.csv")
-exampleReader = csv.reader(exampleFile)
-for row in exampleReader:
-    print("Row #" + str(exampleReader.line_num) + ' ' + str(row))
+
+def main():
+    import csv
+
+    # Reader Objects
+    exampleFile = open("example.csv")
+    exampleReader = csv.reader(exampleFile)
+    exampleData = list(exampleReader)
+    print(exampleData)
+
+    print(exampleData[0][0])
+    print(exampleData[0][1])
+    print(exampleData[0][2])
+    print(exampleData[1][1])
+    print(exampleData[6][1])
+
+    # Reading Data from Reader Objects in a for Loop
+    exampleFile = open("example.csv")
+    exampleReader = csv.reader(exampleFile)
+    for row in exampleReader:
+        print("Row #" + str(exampleReader.line_num) + ' ' + str(row))
+
+
+if __name__ == '__main__':
+    main()
