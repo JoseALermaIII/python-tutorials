ffc0d986843dfd7dc8b5babcdce6561206127daf
JoseALermaIII <jose@JoseALerma.com>
2018-12-28 01:52:55 -0600

    Update P3_cellInverter.py added note to docstring

--- pythontutorials/books/AutomateTheBoringStuff/Ch12/Projects/P3_cellInverter.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch12/Projects/P3_cellInverter.py
@@ -4,6 +4,9 @@ Write a program to invert the row and column of the cells in the spreadsheet.
 For example, the value at row 5, column 3 will be at row 3, column 5
 (and vice versa). This should be done for all cells in the spreadsheet.
 
+Note:
+    Gets full file path from commandline arguments.
+
 """
 
 
