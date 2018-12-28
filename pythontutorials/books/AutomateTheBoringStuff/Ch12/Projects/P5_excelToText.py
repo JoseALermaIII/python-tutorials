3a29c41aefdc4d1486d1ba445536209229c12d16
JoseALermaIII <jose@JoseALerma.com>
2018-12-28 01:56:40 -0600

    Update P5_excelToText.py added note to docstring

--- pythontutorials/books/AutomateTheBoringStuff/Ch12/Projects/P5_excelToText.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch12/Projects/P5_excelToText.py
@@ -4,6 +4,9 @@ Write a program that performs the tasks of the previous program in reverse
 order: The program should open a spreadsheet and write the cells of column A
 into one text file, the cells of column B into another text file, and so on.
 
+Note:
+    Default output folder is ``./p5files/``.
+
 """
 
 
