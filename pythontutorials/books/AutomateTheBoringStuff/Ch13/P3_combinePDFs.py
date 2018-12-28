2bf4aacbc2a43305506d2d16cef97c6c89c30ee9
JoseALermaIII <jose@JoseALerma.com>
2018-12-28 02:57:59 -0600

    Update P2_combinePDF.py added module reference in docstring

--- pythontutorials/books/AutomateTheBoringStuff/Ch13/P3_combinePDFs.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch13/P3_combinePDFs.py
@@ -5,7 +5,7 @@ Combines all the PDFs in the current working directory into a single PDF.
 
 Note:
     * Example PDFs can be downloaded from http://nostarch.com/automatestuff/
-    * Book uses PyPDF2; I'm an overachiever that uses PyPDF4
+    * Book uses :py:mod:`PyPDF2`; I'm an overachiever that uses PyPDF4
 
 """
 
