5d5b59bde655fbeb2d07bd5539c2ff9b29879d1d
JoseALermaIII <jose@JoseALerma.com>
2018-12-28 03:56:04 -0600

    Update P1_writeCSV.py added docstring and wrapped in main function

--- pythontutorials/books/AutomateTheBoringStuff/Ch14/P2_writeCSV.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch14/P2_writeCSV.py
@@ -1,19 +1,32 @@
-# This program uses the csv module to manipulate .csv files
+"""Write CSV
 
-import csv
+This program uses :py:mod:`csv` to write .csv files.
 
-# Writer Objects
-outputFile = open("output.csv", "w", newline='')
-outputWriter = csv.writer(outputFile)
-print(outputWriter.writerow(['spam', 'eggs', 'bacon', 'ham']))
-print(outputWriter.writerow(['Hello, world!', 'eggs', 'bacon', 'ham']))
-print(outputWriter.writerow([1, 2, 3.141592, 4]))
-outputFile.close()
+Note:
+    Creates 'output.csv' and 'example.tsv' files.
 
-# Delimiter and lineterminator Keyword Arguments
-csvFile = open("example.tsv", 'w', newline='')
-csvWriter = csv.writer(csvFile, delimiter='\t', lineterminator='\n\n')
-print(csvWriter.writerow(['apples', 'oranges', 'grapes']))
-print(csvWriter.writerow(['eggs', 'bacon', 'ham']))
-print(csvWriter.writerow(['spam', 'spam', 'spam', 'spam', 'spam', 'spam']))
-csvFile.close()
+"""
+
+
+def main():
+    import csv
+
+    # Writer Objects
+    outputFile = open("output.csv", "w", newline='')
+    outputWriter = csv.writer(outputFile)
+    print(outputWriter.writerow(['spam', 'eggs', 'bacon', 'ham']))
+    print(outputWriter.writerow(['Hello, world!', 'eggs', 'bacon', 'ham']))
+    print(outputWriter.writerow([1, 2, 3.141592, 4]))
+    outputFile.close()
+
+    # Delimiter and lineterminator Keyword Arguments
+    csvFile = open("example.tsv", 'w', newline='')
+    csvWriter = csv.writer(csvFile, delimiter='\t', lineterminator='\n\n')
+    print(csvWriter.writerow(['apples', 'oranges', 'grapes']))
+    print(csvWriter.writerow(['eggs', 'bacon', 'ham']))
+    print(csvWriter.writerow(['spam', 'spam', 'spam', 'spam', 'spam', 'spam']))
+    csvFile.close()
+
+
+if __name__ == '__main__':
+    main()
