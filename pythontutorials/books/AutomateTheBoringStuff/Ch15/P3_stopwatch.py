7af73d0955f74c8c8518030aa23645190b55b2f7
JoseALermaIII <jose@JoseALerma.com>
2018-12-28 04:49:10 -0600

    Update P3_stopwatch.py added docstring and wrapped in main function

--- pythontutorials/books/AutomateTheBoringStuff/Ch15/P3_stopwatch.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch15/P3_stopwatch.py
@@ -1,26 +1,36 @@
 #! python3
-# P3_stopwatch.py - A simple stopwatch program.
+"""Stopwatch
 
-import time
+A simple stopwatch program with lap times.
 
-# Display the program's instructions
-print("Press ENTER to begin. Afterwards, press ENTER to 'click' the stopwatch. "
-      "Press CTRL-C to quit.")
-input()  # Press Enter to begin
-print('Started.')
-startTime = time.time()  # Get the first lap's start time
-lastTime = startTime
-lapNum = 1
+"""
 
-# Start tracking the lap times.
-try:
-    while True:
-        input()
-        lapTime = round(time.time() - lastTime, 2)
-        totalTime = round(time.time() - startTime, 2)
-        print("Lap #%s: %s (%s)" % (lapNum, totalTime, lapTime), end='')
-        lapNum += 1
-        lastTime = time.time()  # reset the last lap time
-except KeyboardInterrupt:
-    # Handle the CTRL-C exception to keep its error message from displaying.
-    print("\nDone.")
+
+def main():
+    import time
+
+    # Display the program's instructions
+    print("Press ENTER to begin. Afterwards, press ENTER to 'click' the stopwatch. "
+          "Press CTRL-C to quit.")
+    input()  # Press Enter to begin
+    print('Started.')
+    startTime = time.time()  # Get the first lap's start time
+    lastTime = startTime
+    lapNum = 1
+
+    # Start tracking the lap times.
+    try:
+        while True:
+            input()
+            lapTime = round(time.time() - lastTime, 2)
+            totalTime = round(time.time() - startTime, 2)
+            print("Lap #%s: %s (%s)" % (lapNum, totalTime, lapTime), end='')
+            lapNum += 1
+            lastTime = time.time()  # reset the last lap time
+    except KeyboardInterrupt:
+        # Handle the CTRL-C exception to keep its error message from displaying.
+        print("\nDone.")
+
+
+if __name__ == '__main__':
+    main()
