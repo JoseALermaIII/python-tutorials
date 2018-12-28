b45d630384a9b42db757004ab666fd560a0455ea
JoseALermaIII <jose@JoseALerma.com>
2018-12-28 05:08:25 -0600

    Update P8_popenFunction.py added docstring and wrapped in main function

--- pythontutorials/books/AutomateTheBoringStuff/Ch15/P8_popenFunction.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch15/P8_popenFunction.py
@@ -1,23 +1,33 @@
-# This program uses the subprocess module's Popen function to launch programs
+"""Popen function
 
-import subprocess
+This program uses :func:`subprocess.Popen` to launch programs.
 
-# Launching Other Programs from Python
-calcProc = subprocess.Popen('/usr/bin/gnome-calculator')
-print(calcProc.poll() is None)
-print(calcProc.wait())
-print(calcProc.poll())
+"""
 
-# Passing Command Line Arguments to Popen()
-subprocess.Popen(['/usr/bin/gedit', '/home/jose/Documents/Notepad.py'])  # Using Ubuntu 18.04
 
-# Running Other Python Scripts
-subprocess.Popen(['/usr/bin/python3',
-                  '/home/jose/PycharmProjects/python-tutorials/pythontutorials/books/'
-                  'AutomateTheBoringStuff/Ch01/P2_hello.py']).communicate()
+def main():
+    import subprocess
+    
+    # Launching Other Programs from Python
+    calcProc = subprocess.Popen('/usr/bin/gnome-calculator')
+    print(calcProc.poll() is None)
+    print(calcProc.wait())
+    print(calcProc.poll())
+    
+    # Passing Command Line Arguments to Popen()
+    subprocess.Popen(['/usr/bin/gedit', '/home/jose/Documents/Notepad.py'])  # Using Ubuntu 18.04
+    
+    # Running Other Python Scripts
+    subprocess.Popen(['/usr/bin/python3',
+                      '/home/jose/PycharmProjects/python-tutorials/pythontutorials/books/'
+                      'AutomateTheBoringStuff/Ch01/P2_hello.py']).communicate()
+    
+    # Opening Files with Default Applications
+    fileObj = open('hello.txt', 'w')
+    fileObj.write('Hello world!')
+    fileObj.close()
+    subprocess.Popen(['see', 'hello.txt'])
 
-# Opening Files with Default Applications
-fileObj = open('hello.txt', 'w')
-fileObj.write('Hello world!')
-fileObj.close()
-subprocess.Popen(['see', 'hello.txt'])
+
+if __name__ == '__main__':
+    main()
