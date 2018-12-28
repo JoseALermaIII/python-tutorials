181a880ea4a8e18f9cbe45a9976f0853e983db1f
JoseALermaIII <jose@JoseALerma.com>
2018-12-28 04:00:35 -0600

    Update P4_readWriteJSON.py added docstring and wrapped in main function

--- pythontutorials/books/AutomateTheBoringStuff/Ch14/P4_readWriteJSON.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch14/P4_readWriteJSON.py
@@ -1,15 +1,25 @@
-# This program uses the json module to manipulate JSON data
+"""Read/write JSON
 
-import json
+This program uses :py:mod:`json` to manipulate JSON data.
 
-# Reading JSON with the loads() Function
-stringOfJsonData = '{"name": "Zophie", "isCat": true, "miceCaught": 0,' \
-                   '"felineIQ": null}'
-jsonDataAsPythonValue = json.loads(stringOfJsonData)
-print(jsonDataAsPythonValue)
+"""
 
-# Writing JSON with the dumps() Function
-pythonValue = {'isCat': True, 'miceCaught': 0, 'name': 'Zophie',
-               'felineIQ': None}
-stringOfJsonData = json.dumps(pythonValue)
-print(stringOfJsonData)
+
+def main():
+    import json
+
+    # Reading JSON with the loads() Function
+    stringOfJsonData = '{"name": "Zophie", "isCat": true, "miceCaught": 0,' \
+                       '"felineIQ": null}'
+    jsonDataAsPythonValue = json.loads(stringOfJsonData)
+    print(jsonDataAsPythonValue)
+
+    # Writing JSON with the dumps() Function
+    pythonValue = {'isCat': True, 'miceCaught': 0, 'name': 'Zophie',
+                   'felineIQ': None}
+    stringOfJsonData = json.dumps(pythonValue)
+    print(stringOfJsonData)
+
+
+if __name__ == '__main__':
+    main()
