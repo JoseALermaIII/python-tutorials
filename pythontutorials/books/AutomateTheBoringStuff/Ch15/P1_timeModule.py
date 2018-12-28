1371c84b647654c68536423667a26dd12a8058aa
JoseALermaIII <jose@JoseALerma.com>
2018-12-28 04:31:31 -0600

    Update P1_timeModule.py added docstring and wrapped in main function

--- pythontutorials/books/AutomateTheBoringStuff/Ch15/P1_timeModule.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch15/P1_timeModule.py
@@ -1,24 +1,36 @@
-# This program uses the time module to reference the Unix epoch as a timestamp.
+"""Time module
 
-# The time.time() Function
-import time
-print(time.time())
+This program uses :py:mod:`time` to reference the `Unix epoch`_ as a timestamp.
 
-# The time.sleep() Function
-for i in range(3):
-    print('Tick')
-    time.sleep(1)
-    print('Tock')
-    time.sleep(1)
+.. _Unix epoch:
+    https://en.wikipedia.org/wiki/Unix_time
+"""
 
-time.sleep(5)
 
-for i in range(30):  # 30 second timer that can be CTRL-C'd from within IDLE
-    time.sleep(1)
+def main():
+    # The time.time() Function
+    import time
+    print(time.time())
 
-# Rounding Numbers
-now = time.time()
-print(now)
-print(round(now, 2))
-print(round(now, 4))
-print(round(now))
+    # The time.sleep() Function
+    for i in range(3):
+        print('Tick')
+        time.sleep(1)
+        print('Tock')
+        time.sleep(1)
+
+    time.sleep(5)
+
+    for i in range(30):  # 30 second timer that can be CTRL-C'd from within IDLE
+        time.sleep(1)
+
+    # Rounding Numbers
+    now = time.time()
+    print(now)
+    print(round(now, 2))
+    print(round(now, 4))
+    print(round(now))
+
+
+if __name__ == '__main__':
+    main()
