614c2233752b6416743fb35dd0a4296b13030c46
JoseALermaIII <jose@JoseALerma.com>
2018-12-29 17:48:23 -0600

    Update P1_sendingEmail.py added docstring and wrapped in main function

--- pythontutorials/books/AutomateTheBoringStuff/Ch16/P1_sendingEmail.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch16/P1_sendingEmail.py
@@ -1,30 +1,40 @@
-# This program uses the smtplib module to send emails
-#
-# Note:
-# - smtp_info file has each item on separate line
-# - email address used is specially created for this chapter
-# - use input() for password to prevent storing in unencrypted file
+"""Sending email
 
-# Connecting to an SMTP Server
-import smtplib
+This program uses :py:mod:`smtplib` to send emails.
 
-with open('smtp_info') as config:
-    email, password, server, port = config.read().splitlines()
+Notes:
+    * ``smtp_info`` file has each item on a separate line.
+    * Email address used is specially created for this chapter.
+    * Use :func:`input` for password to prevent storing in unencrypted file.
 
-smtp_obj = smtplib.SMTP_SSL(server, port)  # Using port 465
-print(type(smtp_obj))
+"""
 
-# Sending the SMTP "Hello" Message
-print(smtp_obj.ehlo())
 
-# Logging in to the SMTP Server
-print(smtp_obj.login(email, password))
+def main():
+    # Connecting to an SMTP Server
+    import smtplib
 
-# Sending an Email
-unsent = smtp_obj.sendmail(email, 'contact.me@JoseALerma.com',
-                           'Subject: Bot test.\nDear Myself,\nAlways remember that with great '
-                           'power comes great responsibility.\nRegards,\nYou')
-print(unsent.keys())
+    with open('smtp_info') as config:
+        email, password, server, port = config.read().splitlines()
 
-# Disconnecting from the SMTP Server
-print(smtp_obj.quit())
+    smtp_obj = smtplib.SMTP_SSL(server, port)  # Using port 465
+    print(type(smtp_obj))
+
+    # Sending the SMTP "Hello" Message
+    print(smtp_obj.ehlo())
+
+    # Logging in to the SMTP Server
+    print(smtp_obj.login(email, password))
+
+    # Sending an Email
+    unsent = smtp_obj.sendmail(email, 'contact.me@JoseALerma.com',
+                               'Subject: Bot test.\nDear Myself,\nAlways remember that with great '
+                               'power comes great responsibility.\nRegards,\nYou')
+    print(unsent.keys())
+
+    # Disconnecting from the SMTP Server
+    print(smtp_obj.quit())
+
+
+if __name__ == '__main__':
+    main()
