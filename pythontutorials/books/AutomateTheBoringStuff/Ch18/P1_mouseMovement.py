ae3680a98d02243c6f58973923b7aff7e16890cc
JoseALermaIII <jose@JoseALerma.com>
2018-12-29 23:21:30 -0600

    Update P1_mouseMovement.py added docstring and wrapped in main function

--- pythontutorials/books/AutomateTheBoringStuff/Ch18/P1_mouseMovement.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch18/P1_mouseMovement.py
@@ -1,28 +1,38 @@
-# This program uses the PyAutoGUI module to control mouse movement
+"""Mouse movement
 
-import pyautogui
+This program uses :py:mod:`PyAutoGUI` to control mouse movement.
 
-# Pauses and Fail-Safes
-pyautogui.PAUSE = 1  # pause 1 second for every pyautogui function call
-pyautogui.FAILSAFE = True  # cause FailSafeException by moving mouse cursor to upper-left corner
+"""
 
-# Moving the Mouse
-print(pyautogui.size())
-width, height = pyautogui.size()
 
-for i in range(10):
-    pyautogui.moveTo(100, 100, duration=0.25)
-    pyautogui.moveTo(200, 100, duration=0.25)
-    pyautogui.moveTo(200, 200, duration=0.25)
-    pyautogui.moveTo(100, 200, duration=0.25)
+def main():
+    import pyautogui
 
-for i in range(10):
-    pyautogui.moveRel(100, 0, duration=0.25)
-    pyautogui.moveRel(0, 100, duration=0.25)
-    pyautogui.moveRel(-100, 0, duration=0.25)
-    pyautogui.moveRel(0, -100, duration=0.25)
+    # Pauses and Fail-Safes
+    pyautogui.PAUSE = 1  # pause 1 second for every pyautogui function call
+    pyautogui.FAILSAFE = True  # cause FailSafeException by moving mouse cursor to upper-left corner
 
-# Getting the Mouse Position
-print(pyautogui.position())
-print(pyautogui.position())
-print(pyautogui.position())
+    # Moving the Mouse
+    print(pyautogui.size())
+    width, height = pyautogui.size()
+
+    for i in range(10):
+        pyautogui.moveTo(100, 100, duration=0.25)
+        pyautogui.moveTo(200, 100, duration=0.25)
+        pyautogui.moveTo(200, 200, duration=0.25)
+        pyautogui.moveTo(100, 200, duration=0.25)
+
+    for i in range(10):
+        pyautogui.moveRel(100, 0, duration=0.25)
+        pyautogui.moveRel(0, 100, duration=0.25)
+        pyautogui.moveRel(-100, 0, duration=0.25)
+        pyautogui.moveRel(0, -100, duration=0.25)
+
+    # Getting the Mouse Position
+    print(pyautogui.position())
+    print(pyautogui.position())
+    print(pyautogui.position())
+
+
+if __name__ == '__main__':
+    main()
