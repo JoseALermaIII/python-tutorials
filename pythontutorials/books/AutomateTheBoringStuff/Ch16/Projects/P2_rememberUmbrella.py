b57bda95fb12d8ef2139409f5824902fbe3d0c17
JoseALermaIII <jose@JoseALerma.com>
2018-12-29 21:12:11 -0600

    Update P2_rememberUmbrella.py added docstrings

--- pythontutorials/books/AutomateTheBoringStuff/Ch16/Projects/P2_rememberUmbrella.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch16/Projects/P2_rememberUmbrella.py
@@ -1,12 +1,28 @@
-# Use the requests module to scrape data from http://weather.gov/.
-# Write a program that runs just before you wake up in the morning and checks
-# whether it’s raining that day. If so, have the program text you a reminder to pack
-# an umbrella before leaving the house.
+"""Remember umbrella
+
+Use :py:mod:`requests` to scrape data from http://weather.gov/.
+
+Write a program that runs just before you wake up in the morning and checks
+whether it’s raining that day. If so, have the program text you a reminder to pack
+an umbrella before leaving the house.
+
+"""
 
 import requests, bs4, datetime
 
 
-def get_weather(url_arg):
+def get_weather(url_arg: str) -> str:
+    """Get weather
+
+    Uses :py:mod:`requests` to download given weather page url, then uses :py:mod:`bs4` to get
+    the current weather data text.
+
+    Args:
+        url_arg: String containing url to specified city's http://weather.gov/ weather page.
+
+    Returns:
+        String with current weather data text.
+    """
     # Download url_arg and soupify
     res = requests.get(url_arg)
     res.raise_for_status()
@@ -18,17 +34,41 @@ def get_weather(url_arg):
     return weather_element[0].getText()
 
 
-def remember_umbrella(weather_arg):
+def remember_umbrella(weather_arg: str) -> bool:
+    """Remember umbrella
+
+    Checks current weather data text from :meth:`get_weather` for keywords indicating rain.
+
+    Args:
+        weather_arg: String containing current weather text of specified city.
+
+    Returns:
+        True if any of the rain keywords are found, False otherwise.
+
+    """
     # Check weather_arg for rain
     tokens = ['rain', 't-storms']
+    """list: Strings of keywords that indicate rain."""
+
+    weather_arg = weather_arg.lower()  # To match tokens' case
 
     for token in tokens:
-        if token in weather_arg.lower():
+        if token in weather_arg:
             return True
     return False
 
 
-def check_time(time_arg):
+def check_time(time_arg: datetime.time) -> bool:
+    """Check time
+
+    Checks if given time is after current time as given by :meth:`datetime.datetime.now`.
+
+    Args:
+        time_arg: :class:`datetime.time` object to compare with current time.
+
+    Returns:
+        True if given time is after current time.
+    """
     # Check for time_arg
     time_now = datetime.datetime.now().time()
 
