e75dae9c7e1aa74f3150283ae5bf477c697f670f
JoseALermaIII <jose@JoseALerma.com>
2018-12-29 23:22:25 -0600

    Update P2_mouseNow.py added docstring and wrapped in main function

--- pythontutorials/books/AutomateTheBoringStuff/Ch18/P2_mouseNow.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch18/P2_mouseNow.py
@@ -1,14 +1,25 @@
 #! python3
-# P2_mouseNow.py - Displays the mouse cursor's current position.
-import pyautogui
+"""Mouse now
 
-print('Press Ctrl-C to quit.')
-try:
-    while True:
-        # Get and print the mouse coordinates.
-        x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-        print(positionStr, end='')
-        print('\b' * len(positionStr), end='', flush=True)
-except KeyboardInterrupt:
-    print('\nDone.')
+Displays the mouse cursor's current position in terminal.
+
+"""
+
+
+def main():
+    import pyautogui
+    
+    print('Press Ctrl-C to quit.')
+    try:
+        while True:
+            # Get and print the mouse coordinates.
+            x, y = pyautogui.position()
+            positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+            print(positionStr, end='')
+            print('\b' * len(positionStr), end='', flush=True)
+    except KeyboardInterrupt:
+        print('\nDone.')
+
+
+if __name__ == '__main__':
+    main()
