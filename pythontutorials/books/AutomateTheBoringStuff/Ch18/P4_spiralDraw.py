2b4b2f19e7da7744dad3e67cdeab8b2f84ff5e26
JoseALermaIII <jose@JoseALerma.com>
2018-12-29 23:23:26 -0600

    Update P4_spiralDraw.py added docstring and wrapped in main function

--- pythontutorials/books/AutomateTheBoringStuff/Ch18/P4_spiralDraw.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch18/P4_spiralDraw.py
@@ -1,14 +1,24 @@
 #! python3
-# P4_spiralDraw.py - this program uses PyAutoGui to draw a spiral pattern
+"""Spiral draw
 
-import pyautogui, time
-time.sleep(5)
-pyautogui.click()  # click to put drawing program in focus
-distance = 200
-while distance > 0:
-    pyautogui.dragRel(distance, 0, duration=0.2)  # move right
-    distance = distance - 5
-    pyautogui.dragRel(0, distance, duration=0.2)  # move down
-    pyautogui.dragRel(-distance, 0, duration=0.2)  # move left
-    distance = distance - 5
-    pyautogui.dragRel(0, -distance, duration=0.2)  # move up
+Program uses :py:mod:`PyAutoGUI` to draw a spiral pattern in an opened drawing program.
+
+"""
+
+
+def main():
+    import pyautogui, time
+    time.sleep(5)
+    pyautogui.click()  # click to put drawing program in focus
+    distance = 200
+    while distance > 0:
+        pyautogui.dragRel(distance, 0, duration=0.2)  # move right
+        distance = distance - 5
+        pyautogui.dragRel(0, distance, duration=0.2)  # move down
+        pyautogui.dragRel(-distance, 0, duration=0.2)  # move left
+        distance = distance - 5
+        pyautogui.dragRel(0, -distance, duration=0.2)  # move up
+
+
+if __name__ == '__main__':
+    main()
