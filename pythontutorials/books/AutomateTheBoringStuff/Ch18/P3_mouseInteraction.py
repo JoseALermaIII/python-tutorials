3a9ff39e1d6849856116cfefb9bdfd39d3e4c5ef
JoseALermaIII <jose@JoseALerma.com>
2018-12-29 23:22:56 -0600

    Update P3_mouseInteraction.py added docstring and wrapped in main function

--- pythontutorials/books/AutomateTheBoringStuff/Ch18/P3_mouseInteraction.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch18/P3_mouseInteraction.py
@@ -1,29 +1,39 @@
-# This program uses the PyAutoGUI module to control mouse interaction
+"""Mouse interaction
 
-import pyautogui, time
+This program uses :py:mod:`PyAutoGUI` to control mouse interaction.
 
-# Clicking the Mouse
-# No delay, so don't blink
-pyautogui.click(100, 150, button='left')
-pyautogui.click(10, 5)
-pyautogui.click(200, 250, button='right')
-pyautogui.rightClick(20, 25)
-pyautogui.click(300, 350, button='middle')
-pyautogui.middleClick(30, 35)
+"""
 
-pyautogui.mouseDown()  # pushes left button down at current location
-pyautogui.mouseUp()  # releases left button at current location
 
-pyautogui.doubleClick()  # double click left button
+def main():
+    import pyautogui, time
 
-# Scrolling the Mouse
-pyautogui.scroll(200)  # Units vary depending on OS and application
+    # Clicking the Mouse
+    # No delay, so don't blink
+    pyautogui.click(100, 150, button='left')
+    pyautogui.click(10, 5)
+    pyautogui.click(200, 250, button='right')
+    pyautogui.rightClick(20, 25)
+    pyautogui.click(300, 350, button='middle')
+    pyautogui.middleClick(30, 35)
 
-import pyperclip  # imports should be at top of file
-numbers = ''
-for i in range(200):
-    numbers = numbers + str(i) + '\n'
-pyperclip.copy(numbers)  # 200 lines of numbers
+    pyautogui.mouseDown()  # pushes left button down at current location
+    pyautogui.mouseUp()  # releases left button at current location
 
-time.sleep(10)  # time to paste the lines in a text editor
-pyautogui.scroll(100)
+    pyautogui.doubleClick()  # double click left button
+
+    # Scrolling the Mouse
+    pyautogui.scroll(200)  # Units vary depending on OS and application
+
+    import pyperclip  # imports should be at top of file
+    numbers = ''
+    for i in range(200):
+        numbers = numbers + str(i) + '\n'
+    pyperclip.copy(numbers)  # 200 lines of numbers
+
+    time.sleep(10)  # time to paste the lines in a text editor
+    pyautogui.scroll(100)
+
+
+if __name__ == '__main__':
+    main()
