f247e3d31b25908a46cb64754166fca2a421edd9
JoseALermaIII <jose@JoseALerma.com>
2018-12-29 23:27:54 -0600

    Update P1_lookingBusy.py added docstring

--- pythontutorials/books/AutomateTheBoringStuff/Ch18/Projects/P1_lookingBusy.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch18/Projects/P1_lookingBusy.py
@@ -1,6 +1,10 @@
-# Write a script to nudge your mouse cursor slightly every ten seconds.
-# The nudge should be small enough so that it won’t get in the way if
-# you do happen to need to use your computer while the script is running.
+"""Looking busy
+
+Write a script to nudge your mouse cursor slightly every ten seconds.
+The nudge should be small enough so that it won’t get in the way if
+you do happen to need to use your computer while the script is running.
+
+"""
 
 import pyautogui, time
 
