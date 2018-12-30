2e89f6001747a794d96da5b2596f8d5cd38ca4b9
JoseALermaIII <jose@JoseALerma.com>
2018-12-29 22:16:58 -0600

    Update P1_imageFundamentals.py added docstring and wrapped in main function

--- pythontutorials/books/AutomateTheBoringStuff/Ch17/P1_imageFundamentals.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch17/P1_imageFundamentals.py
@@ -1,9 +1,19 @@
-# This program uses the pillow module to represent RGBA values
+"""Image fundamentals
 
-# Colors and RGBA Values
-from PIL import ImageColor
-print(ImageColor.getcolor('red', 'RGBA'))
-print(ImageColor.getcolor('RED', 'RGBA'))
-print(ImageColor.getcolor('Black', 'RGBA'))
-print(ImageColor.getcolor('chocolate', 'RGBA'))
-print(ImageColor.getcolor('CornflowerBlue', 'RGBA'))
+This program uses :meth:`PIL.ImageColor.getcolor` to convert color names to RGBA values.
+
+"""
+
+
+def main():
+    # Colors and RGBA Values
+    from PIL import ImageColor
+    print(ImageColor.getcolor('red', 'RGBA'))
+    print(ImageColor.getcolor('RED', 'RGBA'))
+    print(ImageColor.getcolor('Black', 'RGBA'))
+    print(ImageColor.getcolor('chocolate', 'RGBA'))
+    print(ImageColor.getcolor('CornflowerBlue', 'RGBA'))
+
+
+if __name__ == '__main__':
+    main()
