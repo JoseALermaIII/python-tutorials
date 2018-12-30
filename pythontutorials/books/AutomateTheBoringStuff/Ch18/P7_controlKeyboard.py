a340e7a7f4b0b04dc4c1e9f8d1617338df6b549d
JoseALermaIII <jose@JoseALerma.com>
2018-12-29 23:24:52 -0600

    Update P7_controlKeyboard.py added docstrings and wrapped in main function

--- pythontutorials/books/AutomateTheBoringStuff/Ch18/P7_controlKeyboard.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch18/P7_controlKeyboard.py
@@ -1,37 +1,53 @@
-# This program uses the PyAutoGUI module to manipulate keyboard input
+"""Control Keyboard
 
-import pyautogui
-# Sending a String from the Keyboard
-pyautogui.click(100, 100)  # put file editor in focus
-pyautogui.typewrite('Hello world!')
-pyautogui.typewrite('!dlrow olleH', 0.25)
+This program uses :py:mod:`PyAutoGUI` to manipulate keyboard input.
 
-# Key Names
-pyautogui.typewrite(['enter', 'a', 'b', 'left', 'left', 'X', 'Y'])
+"""
 
-# Press and Release Keyboard Keys
-pyautogui.press('end')
-pyautogui.press('enter')
-pyautogui.keyDown('shift')
-pyautogui.press('4')
-pyautogui.keyUp('shift')
-
-# Hotkey Combinations
-pyautogui.keyDown('ctrl')
-pyautogui.keyDown('c')
-pyautogui.keyUp('c')
-pyautogui.keyUp('ctrl')
-
-pyautogui.hotkey('ctrl', 'c')
-
-import time  # imports should be at the top of the file
+import pyautogui, time
 
 
-def commentAfterDelay():
+def commentAfterDelay() -> None:
+    """Comment after delay
+
+    Automatically types then comments out a line in IDLE after waiting two seconds.
+
+    Returns:
+        None. Executes keyboard commands.
+        
+    """
     pyautogui.click(100, 100)  # focus IDLE file editor
     pyautogui.typewrite('In IDLE, Alt-3 comments out a line.')
     time.sleep(2)
     pyautogui.hotkey('alt', '3')
 
 
-commentAfterDelay()
+def main():
+    # Sending a String from the Keyboard
+    pyautogui.click(100, 100)  # put file editor in focus
+    pyautogui.typewrite('Hello world!')
+    pyautogui.typewrite('!dlrow olleH', 0.25)
+
+    # Key Names
+    pyautogui.typewrite(['enter', 'a', 'b', 'left', 'left', 'X', 'Y'])
+
+    # Press and Release Keyboard Keys
+    pyautogui.press('end')
+    pyautogui.press('enter')
+    pyautogui.keyDown('shift')
+    pyautogui.press('4')
+    pyautogui.keyUp('shift')
+
+    # Hotkey Combinations
+    pyautogui.keyDown('ctrl')
+    pyautogui.keyDown('c')
+    pyautogui.keyUp('c')
+    pyautogui.keyUp('ctrl')
+
+    pyautogui.hotkey('ctrl', 'c')
+
+    commentAfterDelay()
+
+
+if __name__ == '__main__':
+    main()
