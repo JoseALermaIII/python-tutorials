580f693089bbd1a914b11c2de144edfb43abcd8a
JoseALermaIII <jose@JoseALerma.com>
2018-12-29 20:26:39 -0600

    Update P5_textMyself added docstrings

--- pythontutorials/books/AutomateTheBoringStuff/Ch16/P5_textMyself.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch16/P5_textMyself.py
@@ -1,6 +1,13 @@
 #! python3
-# P5_textMyself.py - Defines the textmyself() function that texts a message
-# passed to it as a string.
+"""P5_textMyself.py
+
+Defines :func:`textmyself` that texts a string message passed to it.
+
+Note:
+    * ``twilio_info`` file has each item on a separate line.
+    * Use :func:`input` to prevent storing sensitive info in unencrypted file.
+
+"""
 
 from twilio.rest import Client
 
@@ -10,7 +17,18 @@ with open(filepath) as config:
     accountSID, authToken, twilioNumber, myNumber = config.read().splitlines()
 
 
-def textmyself(message):
+def textmyself(message: str) -> None:
+    """Text myself
+
+    Sends given message as SMS to twilio account specified in ``./twilio_info``.
+
+    Args:
+        message: String containing message to be sent.
+
+    Returns:
+        None. Message is sent to twilio phone number.
+
+    """
     twilioCli = Client(accountSID, authToken)
     twilioCli.messages.create(body=message, from_=twilioNumber, to=myNumber)
     return None
