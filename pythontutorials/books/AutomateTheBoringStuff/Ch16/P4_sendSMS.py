f8337718c45a5822d99cc6e263c7a686e5681c34
JoseALermaIII <jose@JoseALerma.com>
2018-12-29 18:05:19 -0600

    Update P4_sendSMS.py added docstring and wrapped in main function

--- pythontutorials/books/AutomateTheBoringStuff/Ch16/P4_sendSMS.py
+++ pythontutorials/books/AutomateTheBoringStuff/Ch16/P4_sendSMS.py
@@ -1,27 +1,40 @@
-# This program uses the twilio module to send SMS messages to a phone number
-#
-# Note:
-# - twilio_info file has each item on separate line
-# - use input() to prevent storing sensitive info in unencrypted file
+"""Send SMS
 
-# Sending Text Messages
-from twilio.rest import Client
+This program uses `twilio`_ to send SMS messages to a phone number.
 
-with open('twilio_info') as config:
-    accountSID, authToken, myTwilioNumber, myCellPhone = config.read().splitlines()
+Note:
+    * ``twilio_info`` file has each item on a separate line.
+    * Use :func:`input` to prevent storing sensitive info in unencrypted file.
 
-twilioCli = Client(accountSID, authToken)
-message = twilioCli.messages.create(body='Mr. Watson - Come here - I want to see you.',
-                                    from_=myTwilioNumber, to=myCellPhone)
+.. _twilio:
+    https://www.twilio.com/docs/sms
 
-print(message.to)
-print(message.from_)
-print(message.body)
-print(message.status)
-print(message.date_created)
-print(message.date_sent is None)
+"""
 
-print(message.sid)
-updatedMessage = twilioCli.messages(message.sid).fetch()
-print(updatedMessage.status)
-print(updatedMessage.date_sent)
+
+def main():
+    # Sending Text Messages
+    from twilio.rest import Client
+
+    with open('twilio_info') as config:
+        accountSID, authToken, myTwilioNumber, myCellPhone = config.read().splitlines()
+
+    twilioCli = Client(accountSID, authToken)
+    message = twilioCli.messages.create(body='Mr. Watson - Come here - I want to see you.',
+                                        from_=myTwilioNumber, to=myCellPhone)
+
+    print(message.to)
+    print(message.from_)
+    print(message.body)
+    print(message.status)
+    print(message.date_created)
+    print(message.date_sent is None)
+
+    print(message.sid)
+    updatedMessage = twilioCli.messages(message.sid).fetch()
+    print(updatedMessage.status)
+    print(updatedMessage.date_sent)
+
+
+if __name__ == '__main__':
+    main()
